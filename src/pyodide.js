eab618ec2f3c10183a72d118fb77cbefbeab8c2b
Michael Droettboom <mdboom@gmail.com>
2018-03-30 10:35:28 -0400

    Simplify pyodide.js

--- src/pyodide.js
+++ src/pyodide.js
@@ -15,43 +15,10 @@ var pyodide = {}
             }
         }
 
-        var memoryInitializer = 'pyodide.asm.html.mem';
-        if (typeof pyodide['locateFile'] === 'function') {
-            memoryInitializer = pyodide['locateFile'](memoryInitializer);
-        } else if (pyodide['memoryInitializerPrefixURL']) {
-            memoryInitializer = pyodide['memoryInitializerPrefixURL'] + memoryInitializer;
-        }
-        pyodide['memoryInitializerRequestURL'] = memoryInitializer;
-        var meminitXHR = pyodide['memoryInitializerRequest'] = new XMLHttpRequest();
-        meminitXHR.open('GET', memoryInitializer, true);
-        meminitXHR.responseType = 'arraybuffer';
-        meminitXHR.send(null);
-
         var script = document.createElement('script');
         script.src = "pyodide.asm.js";
         document.body.appendChild(script);
 
     };
     wasmXHR.send(null);
-
-    if (window.iodide !== undefined) {
-        iodide.addLanguage({
-            name: 'py',
-            displayName: 'Python',
-            keybinding: 'p',
-            evaluate: code => pyodide.runPython(code),
-        });
-
-        iodide.addOutputHandler({
-            shouldHandle: value => (
-                value.$$ !== undefined &&
-                    value.$$.ptrType.name === 'Py*'
-            ),
-
-            render: value => (
-                '<span><span role="img" aria-label="py">🐍</span>' +
-                    pyodide.repr(value).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;') +
-                    '</span>'),
-        });
-    }
 }
