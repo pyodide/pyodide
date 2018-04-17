2a1a5b722ec045185cdf274aaf2d1e16834c58cf
Michael Droettboom <mdboom@gmail.com>
2018-04-17 10:43:26 -0400

    Use promise for initialization. Use postRun hook to resolve.

--- src/pyodide.js
+++ src/pyodide.js
@@ -1,7 +1,6 @@
-
-{
+var languagePluginLoader = new Promise((resolve, reject) => {
     let baseURL = "{{DEPLOY}}";
-    let wasmURL = baseURL + 'pyodide.asm.wasm?x=' + Date.now();
+    let wasmURL = `${baseURL}pyodide.asm.wasm?x=${Date.now()}`;
     let wasmXHR = new XMLHttpRequest();
     wasmXHR.open('GET', wasmURL, true);
     wasmXHR.responseType = 'arraybuffer';
@@ -11,14 +10,21 @@
         if (wasmXHR.status === 200 || wasmXHR.status === 0) {
             Module.wasmBinary = wasmXHR.response;
         } else {
-            alert("Couldn't download the pyodide.asm.wasm binary.  Response was " + wasmXHR.status);
+            alert(`Couldn't download the pyodide.asm.wasm binary.  Response was ${wasmXHR.status}`);
+            reject();
         }
 
         Module.baseURL = baseURL;
-        var script = document.createElement('script');
-        script.onload = function() { window.pyodide = pyodide(Module); };
-        script.src = baseURL + "pyodide.asm.js";
+        Module.postRun = () => {
+            resolve();
+        }
+        let script = document.createElement('script');
+        script.src = `${baseURL}pyodide.asm.js`;
+        script.onload = () => {
+            window.pyodide = pyodide(Module);
+        };
         document.body.appendChild(script);
     };
     wasmXHR.send(null);
-}
+});
+languagePluginLoader
