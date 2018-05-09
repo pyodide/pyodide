75d6ea20e9f9f95c4b97d822bf664f6dec37fb1b
Michael Droettboom <mdboom@gmail.com>
2018-05-09 14:42:29 -0400

    Fix pandas test

--- test/test_pandas.py
+++ test/test_pandas.py
@@ -1,6 +1,6 @@
 def test_pandas(selenium):
     selenium.load_package("pandas")
-    assert len(selenium.run("import pandas\ndir(pandas)")) == 6
+    assert len(selenium.run("import pandas\ndir(pandas)")) == 179
 
 
 def test_extra_import(selenium):
