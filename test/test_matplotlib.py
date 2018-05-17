14db9e693c307296697a933eee7b93ae71897ccf
Michael Droettboom <mdboom@gmail.com>
2018-05-17 14:30:52 -0400

    Fix matplotlib test

--- test/test_matplotlib.py
+++ test/test_matplotlib.py
@@ -1,9 +1,9 @@
 def test_matplotlib(selenium):
     selenium.load_package("matplotlib")
+    selenium.run("from matplotlib import pyplot as plt")
+    selenium.run("x = plt.plot([1,2,3])")
     img = selenium.run(
-        "from matplotlib import pyplot as plt\n"
-        "plt.plot([1,2,3])\n"
         "plt.gcf()._repr_html_().src"
     )
     assert img.startswith('data:image/png;base64,')
-    assert len(img) == 42
+    assert len(img) == 26766
