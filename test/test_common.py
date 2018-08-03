076e9bb083e20231a8a99f361721b85aa7b2e345
Roman Yurchak <rth.yurchak@gmail.com>
2018-08-03 15:43:40 +0300

    Address review comments

--- test/test_common.py
+++ test/test_common.py
@@ -12,9 +12,19 @@ import common  # noqa
 
 
 def registered_packages():
+    """Returns a list of registred package names"""
     packages = [name for name in os.listdir(PKG_DIR)
                 if (PKG_DIR / name).is_dir()]
     return packages
+
+
+def registered_packages_meta():
+    """Returns a dictionary with the contents of `meta.yaml`
+    for each registed package
+    """
+    packages = registered_packages
+    return {name: common.parse_package(PKG_DIR / name / 'meta.yaml')
+            for name in packages}
 
 
 @pytest.mark.parametrize('name', registered_packages())
@@ -22,9 +32,7 @@ def test_meta(selenium, name):
     # check that we can parse the meta.yaml
     meta = common.parse_package(PKG_DIR / name / 'meta.yaml')
 
-    if 'test' in meta:
-        if 'imports' in meta['test']:
-            # check imports
-            for import_name in meta['test']['imports']:
-                selenium.load_package(import_name)
-                selenium.run('import %s' % import_name)
+    # check imports
+    for import_name in meta.get('test', {}).get('imports', []):
+        selenium.load_package(import_name)
+        selenium.run('import %s' % import_name)
