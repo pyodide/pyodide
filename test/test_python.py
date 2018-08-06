6c8c599dce13667bd02cfb569efa4022cee56e61
Michael Droettboom <mdboom@gmail.com>
2018-08-06 14:29:19 -0400

    Automatically retry failed tests

--- test/test_python.py
+++ test/test_python.py
@@ -280,6 +280,7 @@ def test_open_url(selenium):
         "pyodide.open_url('test_data.txt').read()\n") == 'HELLO\n'
 
 
+@pytest.mark.flaky(reruns=2)
 def test_run_core_python_test(python_test, selenium):
     selenium.load_package('test')
     try:
