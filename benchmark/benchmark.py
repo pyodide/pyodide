6d76f25bd70973b3b314dfe530a1e1001472cb14
Michael Droettboom <mdboom@gmail.com>
2018-10-08 13:24:25 -0400

    Make `make benchmark` work again

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -6,6 +6,8 @@ import sys
 
 sys.path.insert(
     0, str((Path(__file__).resolve().parents[1] / 'test')))
+sys.path.insert(
+    0, str((Path(__file__).resolve().parents[1])))
 
 import conftest  # noqa: E402
 
