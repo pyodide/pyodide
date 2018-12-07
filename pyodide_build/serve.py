369ce905c1a0243bf88e8f84814e2dcf6ab0dc5e
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-12-07 22:51:56 +0530

    Adhere to flake8

--- pyodide_build/serve.py
+++ pyodide_build/serve.py
@@ -8,22 +8,30 @@ import pathlib
 TEST_PATH = pathlib.Path(__file__).parents[0].resolve()
 BUILD_PATH = TEST_PATH / '..' / 'build'
 
+
 class Handler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         super().end_headers()
 
+
 Handler.extensions_map['.wasm'] = 'application/wasm'
 
+
 def make_parser(parser):
-    parser.description = ('Start a server with the supplied build_dir and port.')
-    parser.add_argument('--build_dir', action='store', type=str, default=BUILD_PATH, help='set the build directory')
-    parser.add_argument('--port', action='store', type=int, default=8000, help='set the PORT number')
+    parser.description = ('Start a server with the supplied '
+                          'build_dir and port.')
+    parser.add_argument('--build_dir', action='store', type=str,
+                        default=BUILD_PATH, help='set the build directory')
+    parser.add_argument('--port', action='store', type=int,
+                        default=8000, help='set the PORT number')
     return parser
+
 
 def server(port):
     httpd = socketserver.TCPServer(('', port), Handler)
     return httpd
+
 
 def main(args):
     build_dir = args.build_dir
@@ -37,6 +45,7 @@ def main(args):
         print("\n...shutting down http server")
         httpd.shutdown()
         sys.exit()
+
 
 if __name__ == "__main__":
     parser = make_parser(argparse.ArgumentParser())
