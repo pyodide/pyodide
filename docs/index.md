c3c2f10bc56f08e9392dd4a9540c7008b2d1e46e
Michael Droettboom <mdboom@gmail.com>
2019-05-03 13:43:31 -0400

    Loading wheels from PyPI (#147)
    * Add micropip
    
    * Remove CORS proxy for metadata
    
    * Update docs
    
    * Support loading a mixture of built-in and PyPI packages

--- docs/index.md
+++ docs/index.md
@@ -5,6 +5,7 @@
 - [Using Pyodide directly from Javascript](using_pyodide_from_javascript.md)
 - [Using Pyodide from a web worker](using_pyodide_from_webworker.md)
 - [Using Pyodide from Iodide](using_pyodide_from_iodide.md)
+- [Installing packages from PyPI](pypi.md)
 
 ## Developing Pyodide
 
---
+++ test/test_micropip.py
@@ -0,0 +1,24 @@
+import time
+
+
+def test_install_simple(selenium_standalone):
+    selenium_standalone.run("import os")
+    selenium_standalone.load_package("micropip")
+    selenium_standalone.run("import micropip")
+    selenium_standalone.run("micropip.install('snowballstemmer')")
+
+    for i in range(10):
+        if selenium_standalone.run(
+                "os.path.exists"
+                "('/lib/python3.6/site-packages/snowballstemmer')"
+        ):
+            break
+        else:
+            time.sleep(1)
+
+    selenium_standalone.run("import snowballstemmer")
+    selenium_standalone.run("stemmer = snowballstemmer.stemmer('english')")
+    assert selenium_standalone.run(
+        "stemmer.stemWords('go going goes gone'.split())") == [
+            'go', 'go', 'goe', 'gone'
+        ]
