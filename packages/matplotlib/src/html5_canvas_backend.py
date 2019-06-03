c809871946f4da34ed1a6b02e6b2e752d193d0e6
Madhur Tandon <madhur16053@iiitd.ac.in>
2019-06-03 18:57:14 +0530

    Refactor wasm_backend.py into 3 components (#450)
    * Separate out browser_backend.py from wasm_backend.py
    
    * wasm_backend.py now contains Agg specific code
    
    * html5_canvas_backend.py now contains HTML5 Canvas specific code
    
    * Make the backends available to matplotlib
    
    * Use pass instead of raising an error
    
    * Make files flake8 compliant

--- packages/matplotlib/src/html5_canvas_backend.py
+++ packages/matplotlib/src/html5_canvas_backend.py
@@ -1,7 +1,68 @@
 import numpy as np
-from matplotlib.backend_bases import GraphicsContextBase, RendererBase
+from matplotlib.backends.browser_backend import \
+    FigureCanvasWasm, NavigationToolbar2Wasm
+from matplotlib.backend_bases import (
+    GraphicsContextBase, RendererBase,
+    FigureManagerBase, _Backend
+)
+from matplotlib import interactive
 
-_capstyle_d = {'projecting' : 'square', 'butt' : 'butt', 'round': 'round'}
+from js import document
+
+import base64
+import io
+
+_capstyle_d = {'projecting': 'square', 'butt': 'butt', 'round': 'round'}
+
+interactive(True)
+
+
+class FigureCanvasHTMLCanvas(FigureCanvasWasm):
+
+    def __init__(self, *args, **kwargs):
+        FigureCanvasWasm.__init__(self, *args, **kwargs)
+
+    def draw(self):
+        # Render the figure using custom renderer
+        self._idle_scheduled = True
+        orig_dpi = self.figure.dpi
+        if self._ratio != 1:
+            self.figure.dpi *= self._ratio
+        try:
+            width, height = self.get_width_height()
+            canvas = self.get_element('canvas')
+            if canvas is None:
+                return
+            ctx = canvas.getContext("2d")
+            renderer = RendererHTMLCanvas(ctx, width, height, dpi=72)
+            self.figure.draw(renderer)
+        finally:
+            self.figure.dpi = orig_dpi
+            self._idle_scheduled = False
+
+
+class NavigationToolbar2HTMLCanvas(NavigationToolbar2Wasm):
+    """
+    Is a copy of what Agg backend uses, needs to change!
+    """
+    def download(self, format, mimetype):
+        # Creates a temporary `a` element with a URL containing the image
+        # content, and then virtually clicks it. Kind of magical, but it
+        # works...
+        element = document.createElement('a')
+        data = io.BytesIO()
+        try:
+            self.canvas.figure.savefig(data, format=format)
+        except Exception:
+            raise
+        element.setAttribute('href', 'data:{};base64,{}'.format(
+            mimetype, base64.b64encode(data.getvalue()).decode('ascii')))
+        element.setAttribute('download', 'plot.{}'.format(format))
+        element.style.display = 'none'
+        document.body.appendChild(element)
+        element.click()
+        document.body.removeChild(element)
+
 
 class GraphicsContextHTMLCanvas(GraphicsContextBase):
 
@@ -27,7 +88,8 @@ class GraphicsContextHTMLCanvas(GraphicsContextBase):
             self.renderer.ctx.setLineDash([])
         else:
             dl = np.asarray(dash_list)
-            self.renderer.ctx.setLineDash(list(self.renderer.points_to_pixels(dl)))
+            dl = list(self.renderer.points_to_pixels(dl))
+            self.renderer.ctx.setLineDash(dl)
 
     def set_joinstyle(self, js):
         if js in ['miter', 'round', 'bevel']:
@@ -39,6 +101,7 @@ class GraphicsContextHTMLCanvas(GraphicsContextBase):
     def set_linewidth(self, w):
         self._linewidth = float(w)
         self.renderer.ctx.lineWidth = self.renderer.points_to_pixels(float(w))
+
 
 class RendererHTMLCanvas(RendererBase):
 
@@ -56,3 +119,31 @@ class RendererHTMLCanvas(RendererBase):
 
     def points_to_pixels(self, points):
         return (points / 72.0) * self.dpi
+
+
+class FigureManagerHTMLCanvas(FigureManagerBase):
+
+    def __init__(self, canvas, num):
+        super().__init__(canvas, num)
+        self.set_window_title("Figure %d" % num)
+        self.toolbar = NavigationToolbar2HTMLCanvas(canvas)
+
+    def show(self):
+        self.canvas.show()
+
+    def resize(self, w, h):
+        pass
+
+    def set_window_title(self, title):
+        self.canvas.set_window_title(title)
+
+
+@_Backend.export
+class _BackendHTMLCanvas(_Backend):
+    FigureCanvas = FigureCanvasHTMLCanvas
+    FigureManager = FigureManagerHTMLCanvas
+
+    @staticmethod
+    def show():
+        from matplotlib import pyplot as plt
+        plt.gcf().canvas.show()
