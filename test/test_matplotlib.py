8b61272f5331d75919fec01adb4f91fea04c0841
Madhur Tandon <madhur16053@iiitd.ac.in>
2019-06-12 02:30:43 +0530

    Switch back to XMLHttpRequest, make comparison in callback

--- test/test_matplotlib.py
+++ test/test_matplotlib.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.support.wait import WebDriverWait
+
+
 def test_matplotlib(selenium_standalone):
     selenium = selenium_standalone
     selenium.load_package("matplotlib")
@@ -33,10 +36,11 @@ def test_pdf(selenium):
 def test_rendering(selenium):
     selenium.load_package("matplotlib")
     selenium.run("""
-    from js import Request, window
+    from js import XMLHttpRequest, window
     from matplotlib import pyplot as plt
+    from matplotlib import _png
     import numpy as np
-    import pyodide, io
+    import io
     t = np.arange(0.0, 2.0, 0.01)
     s = 1 + np.sin(2 * np.pi * t)
     plt.plot(t, s, linewidth=1.0, marker=11)
@@ -46,11 +50,27 @@ def test_rendering(selenium):
     canvas_data = plt.gcf().canvas.get_pixel_data()
     """)
     selenium.run("""
-    def func(response):
-        return response.blob()
+    def _get_url_async(url, cb):
+        req = XMLHttpRequest.new()
+        req.open('GET', url, True)
+        req.responseType = 'arraybuffer'
+
+        def callback(e):
+            if req.readyState == 4:
+                ref_data = cb(io.BytesIO(req.response))
+                window.result = np.all(canvas_data == ref_data)
+
+        req.onreadystatechange = callback
+        req.send(None)
     """)
-    selenium.run("""
-    req = Request.new('test/canvas.png')
-    resp = window.fetch(req).then(func)
-    ref_data = io.BytesIO(resp)
-    """)
+    selenium.run("_get_url_async('test/canvas.png', _png.read_png_int)")
+    wait = WebDriverWait(selenium.driver, timeout=150)
+    wait.until(ResultLoaded())
+    res = selenium.run("window.result")
+    assert res is True
+
+
+class ResultLoaded:
+    def __call__(self, driver):
+        inited = driver.execute_script("return window.result")
+        return bool(inited)
