5a37d99a18614573a4bc73c9349ad576b9294ef7
Madhur Tandon <madhur16053@iiitd.ac.in>
2019-06-11 19:25:46 +0530

    Switch to XMLHttpRequest

--- test/test_matplotlib.py
+++ test/test_matplotlib.py
@@ -32,25 +32,31 @@ def test_pdf(selenium):
 
 def test_rendering(selenium):
     selenium.load_package("matplotlib")
-    selenium.run("from matplotlib import pyplot as plt")
-    selenium.run("import numpy as np")
-    selenium.run("import pyodide, io, base64")
-    selenium.run("data = io.BytesIO()")
-    selenium.run("t = np.arange(0.0, 2.0, 0.01)")
-    selenium.run("s = 1 + np.sin(2 * np.pi * t)")
-    selenium.run("plt.plot(t, s, linewidth=1.0, marker=11)")
-    selenium.run("plt.plot(t, t)")
-    selenium.run("plt.grid(True)")
-    selenium.run("plt.show()")
-    # png_button = selenium.driver.find_element_by_xpath("//button[contains(text(), 'png')]")
-    # png_button.click()
-    # selenium.run("canvas_data = plt.gcf().canvas.get_pixel_data()")
-    # selenium.run("ref_data = pyodide.open_url('test/canvas.png').getvalue()")
-    # ref_data = selenium.run("io.open(ref_data, encoding='rb', buffering = 0)")
-    # assert ref_data == 1
-    selenium.run("plt.gcf().canvas.print_png(data)")
-    y = selenium.run("base64.b64encode(data.getvalue()).decode('utf-8')")
-    selenium.run("ref_data = pyodide.open_url('test/canvas.png').getvalue()")
-    z = selenium.run("base64.b64encode(io.BytesIO(ref_data.encode('utf-8')).getvalue()).decode('utf-8')")
-    # x = selenium.run("base64.b64encode(data.getvalue()).decode('ascii') == ref_data")
-    assert y == z
+    selenium.run("""
+    from js import XMLHttpRequest
+    from matplotlib import pyplot as plt
+    import numpy as np
+    import pyodide, io
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
+    plt.plot(t, s, linewidth=1.0, marker=11)
+    plt.plot(t, t)
+    plt.grid(True)
+    plt.show()
+    canvas_data = plt.gcf().canvas.get_pixel_data()
+    """)
+    selenium.run("""
+    def _get_url_async(url):
+        req = XMLHttpRequest.new()
+        req.open('GET', url, True)
+        req.responseType = 'blob'
+        req.send(None)
+        if req.readyState == 4:
+            return req.response
+        else:
+            return 'nay'
+
+    ref_data = _get_url_async('test/canvas.png')
+    """)
+    x = selenium.run("ref_data")
+    assert x == 1
