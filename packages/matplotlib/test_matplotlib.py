a377a10c85130b436103cbd7b159991fe81afbc9
Madhur Tandon <madhur16053@iiitd.ac.in>
2019-07-09 13:50:25 -0500

    Testing Infrastructure (#459)
    * Experimental Test
    
    * Set Up the reference data to be loaded from Pyodide
    
    * Fix Integration with Iodide
    
    * Experiment with different comparison methods
    
    * Switch to XMLHttpRequest
    
    * Switch to fetch API
    
    * Switch back to XMLHttpRequest, make comparison in callback
    
    * Fix TimeOutException
    
    * Check out mean of absolute deviations
    
    * Explicitly set DPI Ratio if inside tests
    
    * Add the ability to save images from Web-Server
    
    * Re-write the test
    
    * Adhere to flake8
    
    * Make the testing driver use its respective image
    
    * wrapping it up
    
    * Deviation is 0 for same machine and same conditions
    
    * print deviation
    
    * Use plt.figure()
    
    * Add reference images
    
    * Add comments and move get_canvas_data() inside test_matplotlib.py

--- packages/matplotlib/test_matplotlib.py
+++ packages/matplotlib/test_matplotlib.py
@@ -1,4 +1,21 @@
 import pytest
+import os
+import pathlib
+from selenium.webdriver.support.wait import WebDriverWait
+
+TEST_PATH = pathlib.Path(__file__).parents[0].resolve()
+
+
+def get_canvas_data(selenium):
+    import base64
+    canvas_tag_property = "//canvas[starts-with(@id, 'matplotlib')]"
+    canvas_element = selenium.driver.find_element_by_xpath(canvas_tag_property)
+    img_script = "return arguments[0].toDataURL('image/png').substring(21)"
+    canvas_base64 = selenium.driver.execute_script(img_script, canvas_element)
+    canvas_png = base64.b64decode(canvas_base64)
+    with open(TEST_PATH /
+              r"canvas-{0}.png".format(selenium.browser), 'wb') as f:
+        f.write(canvas_png)
 
 
 @pytest.mark.skip_refcount_check
@@ -69,3 +86,41 @@ def test_font_manager(selenium):
         """
     )
     assert selenium.run("fontlist_built == fontlist_vendor")
+
+
+def test_rendering(selenium):
+    selenium.load_package("matplotlib")
+    selenium.run("""
+    from js import window
+    window.testing = True
+    from matplotlib import pyplot as plt
+    import numpy as np
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
+    plt.figure()
+    plt.plot(t, s, linewidth=1.0, marker=11)
+    plt.plot(t, t)
+    plt.grid(True)
+    plt.show()
+    """)
+
+    # If we don't have a reference image, write one to disk
+    if not os.path.isfile('test/canvas-{0}.png'.format(selenium.browser)):
+        get_canvas_data(selenium)
+
+    selenium.run("""
+    url = 'test/canvas-{0}.png'
+    threshold = 0
+    plt.gcf().canvas.compare_reference_image(url, threshold)
+    """.format(selenium.browser))
+
+    wait = WebDriverWait(selenium.driver, timeout=70)
+    wait.until(ResultLoaded())
+    assert selenium.run("window.deviation") == 0
+    assert selenium.run("window.result") is True
+
+
+class ResultLoaded:
+    def __call__(self, driver):
+        inited = driver.execute_script("return window.result")
+        return inited is not None
