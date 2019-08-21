069550c245634dc9a44a0783bc30c3b0ee58793c
Madhur Tandon <madhur16053@iiitd.ac.in>
2019-08-22 01:41:35 +0530

    Fix Stroke Bug for 0 linewidth (#508)
    * Fix Stroke for linewidth 0 bug
    
    * Reduce number of circles in transparency test
    
    * Add updated test images
    
    * Minor fixes
    
    * Update Reference Images

--- packages/matplotlib/test_matplotlib.py
+++ packages/matplotlib/test_matplotlib.py
@@ -430,7 +430,7 @@ def test_transparency(selenium_standalone):
 
     fig, ax = plt.subplots()
     for color in ['tab:blue', 'tab:orange', 'tab:green']:
-        n = 750
+        n = 100
         x, y = np.random.rand(2, n)
         scale = 200.0 * np.random.rand(n)
         ax.scatter(x, y, c=color, s=scale, label=color,
