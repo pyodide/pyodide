73fd7687d04acc80527e023bf31b9b9aa83aebf3
Michael Greminger <michael.greminger@gmail.com>
2020-08-01 15:27:32 -0500

    Remove antlr4 and parse_latex test

--- packages/sympy/test_sympy.py
+++ packages/sympy/test_sympy.py
@@ -10,16 +10,3 @@ def test_sympy(selenium):
         c.subs({a:3, b:4}) == 5
     """
     )
-
-
-def test_parse_latex(selenium):
-    selenium.load_package("antlr4-python3-runtime")
-    assert selenium.run(
-        r"""
-        from sympy.parsing.latex import parse_latex
-
-        z = parse_latex(r"\frac{4}{y}+\sqrt{x^2+y^2}")
-
-        z.subs({'y':4, 'x':3}) == 6
-    """
-    )
