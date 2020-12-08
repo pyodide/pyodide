ff7ec612c5810cdf9eb327dd6ecdd7d7460db86e
Dexter Chua <dalcde@users.noreply.github.com>
2020-12-08 17:22:57 +0800

    BLD Remove --host argument in pyodide_build (#836)

--- pyodide_build/buildpkg.py
+++ pyodide_build/buildpkg.py
@@ -11,6 +11,7 @@ import os
 from pathlib import Path
 import shutil
 import subprocess
+import sys
 from urllib import request
 from datetime import datetime
 from typing import Any, Dict
@@ -143,7 +144,7 @@ def compile(path: Path, srcpath: Path, pkg: Dict[str, Any], args):
     try:
         subprocess.run(
             [
-                str(Path(args.host) / "bin" / "python3"),
+                sys.executable,
                 "-m",
                 "pyodide_build",
                 "pywasmcross",
@@ -151,8 +152,6 @@ def compile(path: Path, srcpath: Path, pkg: Dict[str, Any], args):
                 args.cflags + " " + pkg.get("build", {}).get("cflags", ""),
                 "--ldflags",
                 args.ldflags + " " + pkg.get("build", {}).get("ldflags", ""),
-                "--host",
-                args.host,
                 "--target",
                 args.target,
                 "--install-dir",
@@ -288,13 +287,6 @@ def make_parser(parser: argparse.ArgumentParser):
         nargs="?",
         default=common.DEFAULTLDFLAGS,
         help="Extra linking flags",
-    )
-    parser.add_argument(
-        "--host",
-        type=str,
-        nargs="?",
-        default=common.HOSTPYTHON,
-        help="The path to the host Python installation",
     )
     parser.add_argument(
         "--target",
--- pyodide_build/common.py
+++ pyodide_build/common.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 from typing import Optional, Set
-import sys
 
 ROOTDIR = Path(__file__).parents[1].resolve() / "tools"
-HOSTPYTHON = sys.prefix
 TARGETPYTHON = ROOTDIR / ".." / "cpython" / "installs" / "python-3.8.2"
 DEFAULTCFLAGS = ""
 # fmt: off
