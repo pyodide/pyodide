d1b2576a44125ff91f009a7e6bc666f2141860f5
Hood <hood@mit.edu>
2020-12-17 00:19:09 -0800

    Added failing test: what happens when we await a random JsProxy that is not a promise?

--- src/tests/test_await_jsproxy.py
+++ src/tests/test_await_jsproxy.py
@@ -1,25 +1,30 @@
 import pytest
 
-def test_await_jsproxy(selenium):
-    selenium.run(
-        """
-        from asyncio import AbstractEventLoop
-        from functools import partial
-        class WebLoop(AbstractEventLoop):
-            def call_soon(self, coro, arg=None):
-                try:
-                    x = coro.send(arg)
-                    x = x.then(partial(self.call_soon, coro))
-                    x.catch(partial(self.fail,coro))
-                except StopIteration as result:
-                    pass
-
-            def fail(self, coro,arg=None):
+startup = """
+    from asyncio import AbstractEventLoop
+    from functools import partial
+    class WebLoop(AbstractEventLoop):
+        def call_soon(self, coro, arg=None):
+            try:
+                x = coro.send(arg)
+                x = x.then(partial(self.call_soon, coro))
+                x.catch(partial(self.fail,coro))
+            except StopIteration as result:
                 pass
 
-        import asyncio
-        loop = WebLoop()
-        asyncio.set_event_loop(loop)
+        def fail(self, coro,arg=None):
+            pass
+
+    import asyncio
+    loop = WebLoop()
+    asyncio.set_event_loop(loop)
+"""
+
+
+def test_await_jsproxy(selenium):
+    selenium.run(startup)
+    selenium.run(
+        """
         def prom(res,rej):
             global resolve
             resolve = res
@@ -30,5 +35,18 @@ def test_await_jsproxy(selenium):
             # TODO: How to assert that x = 10?
             print(x)
         resolve(10)
+        loop.call_soon(temp())
+        """
+    )
+
+def test_await_nonpromise(selenium):
+    selenium.run(startup)
+    selenium.run(
+        """
+        from js import Math
+        async def temp():
+            x = await Math
+            print(x)
+        loop.call_soon(temp())
         """
     )
