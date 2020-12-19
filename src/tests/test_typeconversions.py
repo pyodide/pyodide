277eb32263e07181cbdf758009e89c87795d7ef1
Hood <hood@mit.edu>
2020-12-19 10:39:17 -0800

    Removed limitation from docs and added test showing it works correctly.

---
+++ src/tests/test_typeconversions.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium.common.exceptions import WebDriverException
+
+
+def test_del_jsproxy(selenium):
+    selenium.run_js(
+        """
+        class Point {
+            constructor(x, y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        window.point = new Point(42, 43)
+        """
+    )
+    selenium.run(
+        """
+        from js import point
+        assert point.y == 43
+        del point.y
+        """
+    )
+    msg = "AttributeError: y"
+    with pytest.raises(WebDriverException, match=msg):
+        selenium.run("point.y")
+    assert selenium.run_js("return point.y;") is None
