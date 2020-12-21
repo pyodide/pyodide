7007ca082ef1c3d5f171433777d295714c6aad3a
Dexter Chua <dec41@srcf.net>
2020-12-21 17:25:48 +0800

    Add tests for dyncall_so patch

--- emsdk/tests/test_dyncall.py
+++ emsdk/tests/test_dyncall.py
@@ -8,10 +8,33 @@ def test_emulate_function(tmpdir):
             f.write(
                 """\
 #include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
+#include <assert.h>
+
+// This can be any function that has a signature not found in main.
+__attribute__ ((noinline)) int indirect_function(int a, float b, int c, double d) {
+  return a;
+}
+
+typedef int (*type_iifid) (int, float, int, double);
 
 void foo() {
-  puts("hello from library");
-}"""
+  // Hack to force inclusion of malloc
+  volatile int x = (int) malloc(1);
+  free((void *) x);
+
+  type_iifid fp = &indirect_function;
+
+  jmp_buf buf;
+  int i = setjmp(buf);
+
+  printf("%d\\n", i);
+  assert(fp(i, 0, 0, 0) == i);
+
+  if (i == 0) longjmp(buf, 1);
+}
+"""
             )
         with open("main.c", "w") as f:
             f.write(common.MAIN_C)
@@ -49,4 +72,4 @@ void foo() {
         out = subprocess.run(
             ["node", "a.out.js"], capture_output=True, check=True, env=common.env
         )
-        assert out.stdout == b"hello from main\nhello from library\n"
+        assert out.stdout == b"hello from main\n0\n4\n"
