1be899605672d28acd76930a67579efcfada6990
Dexter Chua <dec41@srcf.net>
2020-12-21 10:19:12 +0800

    Lint

--- emsdk/tests/common.py
+++ emsdk/tests/common.py
@@ -3,14 +3,16 @@ import os
 
 EMSDK = Path(__file__).resolve().parents[1] / "emsdk"
 
-path = [str(EMSDK / 'node' / '12.18.1_64bit' / 'bin'),
-        str(EMSDK / 'binaryen' / 'bin'),
-        str(EMSDK / 'fastcomp' / 'emscripten')]
+path = [
+    str(EMSDK / "node" / "12.18.1_64bit" / "bin"),
+    str(EMSDK / "binaryen" / "bin"),
+    str(EMSDK / "fastcomp" / "emscripten"),
+]
 
 env = {
-    'PATH': ":".join(path) + ":" + os.environ["PATH"],
-    'EMSDK': str(EMSDK),
-    'EM_CONFIG': str(EMSDK / ".emscripten"),
-    'EM_CACHE': str(EMSDK / ".emscripten_cache"),
-    'BINARYEN_ROOT': str(EMSDK / "binaryen"),
+    "PATH": ":".join(path) + ":" + os.environ["PATH"],
+    "EMSDK": str(EMSDK),
+    "EM_CONFIG": str(EMSDK / ".emscripten"),
+    "EM_CACHE": str(EMSDK / ".emscripten_cache"),
+    "BINARYEN_ROOT": str(EMSDK / "binaryen"),
 }
