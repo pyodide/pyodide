11648085c83093be678af2ff4a5eff6caafc24a1
Hood <hood@mit.edu>
2020-12-23 12:22:30 -0800

    Added tests for py2js_deep

--- src/tests/test_py2js_variants.py
+++ src/tests/test_py2js_variants.py
@@ -1,11 +1,55 @@
 def my_setup(selenium):
     selenium.run_js(
         """
+        window.py2js_deep = pyodide.TestEntrypoints.py2js_deep;
         window.py2js_shallow = pyodide.TestEntrypoints.py2js_shallow;
         window.py2js_minimal = pyodide.TestEntrypoints.py2js_minimal;
         window.isPyProxy = pyodide.TestEntrypoints.isPyProxy;
         """
     )
+
+
+def test_py2js_deep(selenium):
+    my_setup(selenium)
+    selenium.run("a = [1, 2, 3]")
+    assert selenium.run_js(
+        """
+        res = py2js_deep("a");
+        return (res instanceof window.Array) && JSON.stringify(res) === "[1,2,3]";
+        """
+    )
+    selenium.run("a = (1, 2, 3)")
+    assert selenium.run_js(
+        """
+        res = py2js_deep("a");
+        return (res instanceof window.Array) && JSON.stringify(res) === "[1,2,3]";
+        """
+    )
+    selenium.run("a = [(1,2), (3,4), [5, 6], { 2 : 3,  4 : 9}]")
+    assert selenium.run_js(
+        """
+        a3 = py2js_deep("a");
+        result = true;
+        result &= a3 instanceof window.Array;
+        result &= JSON.stringify(a3) === `[[1,2],[3,4],[5,6],{"2":3,"4":9}]`;
+        return result;
+        """
+    )
+    selenium.run("a = (1, (2, (3, [4, 5])))")
+    assert selenium.run_js(
+        """
+        a4 = py2js_deep("a");
+        result = true;
+        result &= a4 instanceof window.Array;
+        result &= a4[1] instanceof window.Array;
+        result &= a4[1][1] instanceof window.Array;
+        result &= a4[1][1][1] instanceof window.Array;
+        result &= JSON.stringify(a4) === "[1,[2,[3,[4,5]]]]";
+        return result
+        """
+    )
+    selenium.run("a = {1, 2, 3}")
+    # TODO: should convert to a javascript Set.
 
 
 def test_py2js_shallow(selenium):
@@ -51,7 +95,7 @@ def test_py2js_shallow(selenium):
         """
     )
     selenium.run("a = {1, 2, 3}")
-    # TODO: should convert a5 to a javascript Set.
+    # TODO: should convert to a javascript Set.
 
 
 def test_py2js_minimal(selenium):
