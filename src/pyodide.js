1044a625723eab123876365e6cda6c15d006b085
Hood <hood@mit.edu>
2020-12-31 17:26:55 -0800

    Some more fine tuning.

--- src/pyodide.js
+++ src/pyodide.js
@@ -7,21 +7,27 @@
    *  Module.locateFile :
    *     We set this once at the start to load pyodide core, and once each time
    *     loadPackage is called. In loadPackage we use the toFetch package
-   * metadata to decide where to grab the package from, so we use a closure for
-   * this. That closure is provided by the function "getFileLocator"
+   *     metadata to decide where to grab the package from, so we use a closure
+   * for this. That closure is provided by the function "getFileLocator"
    *
    *  Module.dependencyCallbacks :
    *     gets called at both start and end of package loading
    *     (why didn't emscripten give us two distict callbacks for these two
-   * distinct things?) We handle this by setting it always to the same value and
-   * counting up the number of times it is called. When it hits certain
-   * interesting points, we resolve a promise.
+   *     distinct things?) We handle this by setting it always to the same value
+   * and counting up the number of times it is called. When it hits certain
+   *     interesting points, we resolve a promise.
    *
-   *  Both of these global variables mean we cannot load multiple groups of
+   * Both of these global variables mean we cannot load multiple groups of
    * files at once or else we won't be able to control where they come from or
    * what to do when they are done loading. `loadPackage` uses a "promise chain"
    * to make sure that only one instance can run at a time.
    */
+
+  // Note: PYODIDE_BASE_URL is an environment variable replaced in
+  // in this template in the Makefile. It's recommended to always set
+  // languagePluginUrl in any case.
+  let baseURL = globalThis.languagePluginUrl || '{{ PYODIDE_BASE_URL }}';
+  baseURL = baseURL.substr(0, baseURL.lastIndexOf('/')) + '/';
 
   ////////////////////////////////////////////////////////////
   // Loading Pyodide
@@ -30,7 +36,6 @@
     noAudioDecoding : true,
     noWasmDecoding : true,
     preloadedWasm : {},
-    monitorRunDependencies,
     checkABI : function(ABI_number) {
       if (ABI_number !== parseInt('{{ PYODIDE_PACKAGE_ABI }}')) {
         var ABI_mismatch_exception =
@@ -85,15 +90,23 @@
     }
   }
 
-  // Note: PYODIDE_BASE_URL is an environment variable replaced in
-  // in this template in the Makefile. It's recommended to always set
-  // languagePluginUrl in any case.
-  let baseURL = globalThis.languagePluginUrl || '{{ PYODIDE_BASE_URL }}';
-  baseURL = baseURL.substr(0, baseURL.lastIndexOf('/')) + '/';
-  // This creates closures for the Module level callback Module.locateFile.
-  // When inside loadPackage, locateFile needs the toFetch metadata to
-  // decide where to fetch the package from.
-  // Thus we need to update Module.locateFile every time we load a package.
+  function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
+
+  ////////////////////////////////////////////////////////////
+  // Managing module-global emscripten callbacks:
+  //   * Module.locateFile.
+  //   * Module.monitorRunDependencies
+  //
+
+  //   First Module.locateFile.
+  //   getFileLocator creates closures for the Module level callback
+  //   Module.locateFile. When inside loadPackage, locateFile needs the toFetch
+  //   metadata to decide where to fetch the package from. Thus we need to
+  //   update Module.locateFile from getFileLocator every time we run
+  //   loadPackage.
+
   function getFileLocator(toFetch = {}) {
     return (path) => {
       // handle packages loaded from custom URLs
@@ -107,55 +120,146 @@
       return baseURL + path;
     };
   }
-
   Module.locateFile = getFileLocator();
 
-  let dependencyIndex = 0;
-  let resolvedDependencies = 0;
-  dependencyCallbacks = new Map();
-  // Resolve promise when num_dependencies many wasm dependencies are finished
-  // loaded monitorRunDependencies is called twice per package load So we
-  // resolve when monitorRunDependencies has been called 2*num_dependencies many
-  // times.
-  function getRunDependencyPromise(num_dependencies = 1) {
-    return new Promise((resolve, reject) => {
-      dependencyIndex += 2 * num_dependencies;
-      dependencyCallbacks.set(dependencyIndex, {resolve, reject});
-    });
-  }
+  //  Now Module.monitorRunDependencies. Module.monitorRunDependencies is called
+  //  twice per script included, once at start and once at finish. Each time
+  //  it's called we increment a counter. getRunDependencyPromise returns
+  //  promises that we resolve when this counter hits various even integers.
 
-  function runDependencyLoadFailed() {
-    // If the package_uri fails to load, call monitorRunDependencies twice
+  class _ScriptLoadPromiseManager {
+    constructor() {
+      this.expectedDependencies = 0;
+      this.resolvedDependencies = 0;
+      this.dependencyCallbacks = new Map();
+      Module.monitorRunDependencies = this.monitorRunDependencies.bind(this);
+    }
+
+    // This is the value we will use for the Emscripten Module global callback
+    // Module.monitorRunDependencies.
+    // Module.monitorRunDependencies is called by emscripten at the beginning
+    // and the end of each package being loaded.
+    monitorRunDependencies() {
+      this.resolvedDependencies++;
+      let promise_handles =
+          this.dependencyCallbacks.get(this.resolvedDependencies);
+      if (promise_handles) {
+        promise_handles.resolve();
+        this.dependencyCallbacks.delete(this.resolvedDependencies);
+      }
+    }
+
+    // Returns a promise that resolves when num_dependencies many wasm
+    // dependencies are finished. monitorRunDependencies is called twice per
+    // package load, so we resolve when monitorRunDependencies has been called
+    // 2*num_dependencies many times.
+    getPromise(num_dependencies = 1) {
+      return new Promise((resolve, reject) => {
+        this.expectedDependencies += 2 * num_dependencies;
+        this.dependencyCallbacks.set(this.expectedDependencies,
+                                     {resolve, reject});
+      });
+    }
+
+    // If a single script fails to load, call monitorRunDependencies twice
     // (so resolvedDependencies will equal dependencyIndex at the end and
     // we can finish loading)
-    monitorRunDependencies();
-    monitorRunDependencies();
-  }
-
-  // Call this to bail out when the window error handler catches an error.
-  // We don't know what's gone wrong so we just assume it's really bad.
-  function clearRunDependencyCallbacks(err) {
-    for (let promise_handles of dependencyCallbacks.values()) {
-      promise_handles.reject(err);
+    scriptLoadFailed() {
+      this.monitorRunDependencies();
+      // Make sure we don't have a parity error
+      if (this.resolvedDependencies % 2 === 1) {
+        this.monitorRunDependencies();
+      }
     }
-    dependencyCallbacks = new Map();
-  }
 
-  // This is the value we will use for the Emscripten Module global callback
-  // Module.monitorRunDependencies.
-  // Module.monitorRunDependencies is called by emscripten at the beginning and
-  // the end of each package being loaded.
-  function monitorRunDependencies() {
-    resolvedDependencies++;
-    let promise_handles = dependencyCallbacks.get(resolvedDependencies);
-    if (promise_handles) {
-      promise_handles.resolve();
-      dependencyCallbacks.delete(resolvedDependencies);
+    // Reject any remaining dependency callbacks with error "err"
+    // and resets the dependency ScriptLoadPromiseManager state.
+    clearRunDependencyCallbacks(err) {
+      for (let promise_handles of this.dependencyCallbacks.values()) {
+        promise_handles.reject(err);
+      }
+      this.dependencyCallbacks = new Map();
+      this.expectedDependencies = 0;
+      this.resolvedDependencies = 0;
     }
   }
+  let ScriptLoadPromiseManager = new _ScriptLoadPromiseManager();
 
   ////////////////////////////////////////////////////////////
   // Package loading
+  let loadedPackages = {};
+  let loadPackagePromise = Promise.resolve();
+
+  async function loadPackage(names, messageCallback, errorCallback) {
+    /* We want to make sure that only one loadPackage invocation runs at any
+     * given time, so this creates a "chain" of promises. */
+    await loadPackagePromise;
+    loadPackagePromise = _loadPackage(names, messageCallback, errorCallback);
+    return loadPackagePromise;
+  };
+
+  async function _loadPackage(names, messageCallback, errorCallback) {
+    let result;
+    try {
+      result = await __loadPackage(names, messageCallback, errorCallback);
+    } catch (e) {
+      ScriptLoadPromiseManager.clearCallbacks(e);
+      throw e;
+    }
+    let err = new Error(
+        "Internal loadPackage error: unexpected unresolved runDependency callbacks.");
+    ScriptLoadPromiseManager.clearCallbacks(err);
+    return result;
+  }
+
+  async function __loadPackage(names, messageCallback, errorCallback) {
+    // clang-format off
+    messageCallback ||= () => {};
+    errorCallback ||= () => {};
+    // clang-format on
+    let _messageCallback = (msg) => {
+      console.log(msg);
+      messageCallback(msg);
+    };
+    let _errorCallback = (errMsg) => {
+      console.error(errMsg);
+      errorCallback(errMsg);
+    };
+    let toFetch = chaseDependencies(names, _messageCallback, _errorCallback);
+    let numDependenciesToFetch = Object.keys(toFetch).length;
+    Module.locateFile = getFileLocator(toFetch);
+
+    if (numDependenciesToFetch === 0) {
+      return 'No new packages to load';
+    }
+
+    // Set global window handler so we can cancel package loading if an error
+    // occurs
+    // TODO: be more specific about which errors are related to pyodide.
+    globalThis.addEventListener('error', windowErrorHandler);
+    let packageList = Array.from(Object.keys(toFetch));
+    messageCallback(`Fetching ${packageList.join(', ')}`)
+    let fetchPackagePromise =
+        fetchPackages(toFetch, _messageCallback, _errorCallback);
+
+    let loadPromise =
+        ScriptLoadPromiseManager.getPromise(numDependenciesToFetch);
+    // Async breakpoint so that the script loading jobs started up by
+    // fetchPackagePromise get a chance to make their requests.
+    await sleep(0);
+    // We have to invalidate Python's import caches, or it won't
+    // see the new files. This is done here so it happens in parallel
+    // with the fetching over the network.
+    Module.runPython('import importlib as _importlib\n' +
+                     '_importlib.invalidate_caches()\n');
+
+    let fetched = await fetchPackagePromise;
+
+    await loadPromise;
+    globalThis.removeEventListener('error', windowErrorHandler);
+    let packagesLoadedMessage = await installPackages(fetched);
+    return packagesLoadedMessage;
+  };
 
   // Regexp for validating package name and URI
   const package_name_regexp_inner = '[a-z0-9_][a-z0-9_\-]*'
@@ -237,23 +341,12 @@
     return toFetch;
   }
 
-  let loadedPackages = {};
-  let loadPackagePromise = Promise.resolve();
-
-  async function loadPackage(names, messageCallback, errorCallback) {
-    /* We want to make sure that only one loadPackage invocation runs at any
-     * given time, so this creates a "chain" of promises. */
-    await loadPackagePromise;
-    loadPackagePromise = _loadPackage(names, messageCallback, errorCallback);
-    return loadPackagePromise;
-  };
-
   // A handler for any exceptions that are thrown in the process of
   // loading a package
   // TODO: attaching a global error handler for this seems like a really bad
   // idea. What are we trying to catch? Can't we be a bit more specific?
   function windowErrorHandler(err) {
-    clearRunDependencyCallbacks(err);
+    ScriptLoadPromiseManager.clearCallbacks(err);
     globalThis.removeEventListener('error', windowErrorHandler);
     // Set up a new Promise chain, since this one failed
     loadPackagePromise = Promise.resolve();
@@ -279,7 +372,7 @@
         fetched[pkg] = package_uri;
       } catch {
         errorCallback(`Couldn't load package from URL ${scriptSrc}`);
-        runDependencyLoadFailed();
+        ScriptLoadPromiseManager.scriptLoadFailed();
       }
     }
 
@@ -290,7 +383,7 @@
     return fetched;
   }
 
-  // Put the data into Module.loadedPackages and
+  // Put the package uri into Module.loadedPackages and call preloadWasm().
   async function installPackages(fetched) {
     for (let [pkg, package_uri] of Object.entries(fetched)) {
       Module.loadedPackages[pkg] = package_uri;
@@ -307,85 +400,6 @@
     await preloadWasm();
     return resolveMsg;
   }
-
-  async function _loadPackage(names, messageCallback, errorCallback) {
-    // clang-format off
-    messageCallback ||= () => {};
-    errorCallback ||= () => {};
-    // clang-format on
-    let _messageCallback = (msg) => {
-      console.log(msg);
-      messageCallback(msg);
-    };
-    let _errorCallback = (errMsg) => {
-      console.error(errMsg);
-      errorCallback(errMsg);
-    };
-
-    let toFetch = chaseDependencies(names, messageCallback, errorCallback);
-    let numDependenciesToFetch = Object.keys(toFetch).length;
-    Module.locateFile = getFileLocator(toFetch);
-
-    if (numDependenciesToFetch === 0) {
-      return 'No new packages to load';
-    }
-
-    // monitorRunDependencies is called at the beginning and the end of each
-    // package being loaded. We know we are done when it has been called
-    // exactly "numDependenciesToFetch * 2" times.
-    let runDependencyPromise = getRunDependencyPromise(numDependenciesToFetch);
-    // Set global window handler so we can cancel package loading if an error
-    // occurs
-    // TODO: be more specific about which errors are related to pyodide.
-    globalThis.addEventListener('error', windowErrorHandler);
-    let packageList = Array.from(Object.keys(toFetch));
-    messageCallback(`Fetching ${packageList.join(', ')}`)
-    let fetchPackagePromise =
-        fetchPackages(toFetch, messageCallback, errorCallback);
-
-    // We have to invalidate Python's import caches, or it won't
-    // see the new files. This is done here so it happens in parallel
-    // with the fetching over the network.
-    Module.runPython('import importlib as _importlib\n' +
-                     '_importlib.invalidate_caches()\n');
-
-    let fetched = await fetchPackagePromise;
-
-    await runDependencyPromise;
-    globalThis.removeEventListener('error', windowErrorHandler);
-    let packagesLoadedMessage = await installPackages(fetched);
-    return packagesLoadedMessage;
-  };
-
-  ////////////////////////////////////////////////////////////
-  // Fix Python recursion limit
-  function fixRecursionLimit(pyodide) {
-    // The Javascript/Wasm call stack may be too small to handle the default
-    // Python call stack limit of 1000 frames. This is generally the case on
-    // Chrom(ium), but not on Firefox. Here, we determine the Javascript call
-    // stack depth available, and then divide by 50 (determined heuristically)
-    // to set the maximum Python call stack depth.
-
-    let depth = 0;
-    function recurse() {
-      depth += 1;
-      recurse();
-    }
-    try {
-      recurse();
-    } catch (err) {
-      ;
-    }
-
-    let recursionLimit = depth / 50;
-    if (recursionLimit > 1000) {
-      recursionLimit = 1000;
-    }
-    pyodide.runPython(`
-      import sys
-      sys.setrecursionlimit(int(${recursionLimit}))
-    `);
-  };
 
   // clang-format off
   async function preloadWasm() {
@@ -432,6 +446,36 @@
   }
   // clang-format on
 
+  ////////////////////////////////////////////////////////////
+  // Fix Python recursion limit
+  function fixRecursionLimit(pyodide) {
+    // The Javascript/Wasm call stack may be too small to handle the default
+    // Python call stack limit of 1000 frames. This is generally the case on
+    // Chrom(ium), but not on Firefox. Here, we determine the Javascript call
+    // stack depth available, and then divide by 50 (determined heuristically)
+    // to set the maximum Python call stack depth.
+
+    let depth = 0;
+    function recurse() {
+      depth += 1;
+      recurse();
+    }
+    try {
+      recurse();
+    } catch (err) {
+      ;
+    }
+
+    let recursionLimit = depth / 50;
+    if (recursionLimit > 1000) {
+      recursionLimit = 1000;
+    }
+    pyodide.runPython(`
+      import sys
+      sys.setrecursionlimit(int(${recursionLimit}))
+    `);
+  };
+
   async function main() {
     let postRunPromise = new Promise(resolve => Module.postRun = resolve);
     async function postRun() {
@@ -447,15 +491,17 @@
       globalThis.pyodide._module.packages = json;
     }
 
-    // dataScriptSrc must be loaded before scriptSrc.
     async function loadPyodideCore() {
-      let runDependencyPromise = getRunDependencyPromise(2);
+      let numScriptsToLoad = 2;
+      let runDependencyPromise =
+          ScriptLoadPromiseManager.getPromise(numScriptsToLoad);
       const dataScriptSrc = `${baseURL}pyodide.asm.data.js`;
       const scriptSrc = `${baseURL}pyodide.asm.js`;
-      // The emscripten module needs to be at this location for the core
+      // The emscripten module needs to be at globalThis.Module for the core
       // filesystem to install itself. Once that's complete, it will be replaced
       // by the call to `makePublicAPI` with a more limited public API.
       globalThis.Module = Module;
+      // dataScriptSrc must be loaded before scriptSrc.
       await loadScript(dataScriptSrc);
       await loadScript(scriptSrc);
       globalThis.pyodide = pyodide(Module);
@@ -464,7 +510,10 @@
       return runDependencyPromise;
     }
 
-    await Promise.all([ postRun(), runDependencyPromise ]);
+    await Promise.all([ postRun(), loadPyodideCore() ]);
+    let err = new Error(
+        "Internal loadPackage error: unexpected unresolved runDependency callbacks.");
+    ScriptLoadPromiseManager.clearCallbacks(err);
     delete globalThis.Module;
   };
 
