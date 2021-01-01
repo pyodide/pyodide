cd7962beb1b20b2b5bcfd87aa7756eb92d8e2376
Hood <hood@mit.edu>
2021-01-01 08:12:26 -0800

    Added extra ._module to runPythonDebug path

--- src/tests/test_python.py
+++ src/tests/test_python.py
@@ -187,7 +187,7 @@ def test_completions(selenium):
 
 
 def test_run_python_debug(selenium):
-    assert selenium.run_js("return pyodide.runPythonDebug('1+1');") == 2
+    assert selenium.run_js("return pyodide._module.runPythonDebug('1+1');") == 2
     assert selenium.run_js(
-        "return pyodide.runPythonDebug('[x*x + 1 for x in range(4)]');"
+        "return pyodide._module.runPythonDebug('[x*x + 1 for x in range(4)]');"
     ) == [0, 2, 10]
