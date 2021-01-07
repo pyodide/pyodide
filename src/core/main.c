bb2227c4678bb26193768e81fbee7637ae76378c
Hood <hood@mit.edu>
2021-01-07 09:12:06 -0800

    Fix compile errors

--- src/core/main.c
+++ src/core/main.c
@@ -37,11 +37,11 @@ static int
 version_info_init()
 {
   PyObject* pyodide = PyImport_ImportModule("pyodide");
-  PyObject* pyodide_version = _PyAttr_GetId(pyodide, &PyId___version__);
-  char* pyodide_version_utf8 = PyUnicode_AsUTF8(version);
+  PyObject* pyodide_version = _PyObject_GetAttrId(pyodide, &PyId___version__);
+  const char* pyodide_version_utf8 = PyUnicode_AsUTF8(pyodide_version);
   PyObject* sys = PyImport_ImportModule("sys");
-  PyObject* python_version = _PyAttr_GetId(sys, &PyId___version__);
-  char* python_version_utf8 = PyUnicode_AsUTF8(version);
+  PyObject* python_version = _PyObject_GetAttrId(sys, &PyId___version__);
+  const char* python_version_utf8 = PyUnicode_AsUTF8(python_version);
 
   EM_ASM(
     {
