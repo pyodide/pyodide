8229e4c27815b22ffed892f04497e8a655a4fa59
Romain Casati <romain.casati@basthon.fr>
2021-01-08 14:30:12 +0100

    True std streams redirection in InteractiveConsole.

--- src/pyodide-py/pyodide/console.py
+++ src/pyodide-py/pyodide/console.py
@@ -1,22 +1,110 @@
-from typing import Optional
+from typing import Optional, Callable
 import code
 import io
 import sys
 
 
+__all__ = ["InteractiveConsole"]
+
+
+class _StdStream(io.TextIOWrapper):
+    """
+    Custom std stream to retdirect sys.stdout/stderr in InteractiveConsole.
+
+    Parmeters
+    ---------
+    flush_callback
+        Function to call at each flush.
+    """
+
+    def __init__(self, flush_callback: Callable[[str], None]):
+        buffer = io.BufferedWriter(_CallbackBuffer(flush_callback))
+        super().__init__(buffer, line_buffering=True)  # type: ignore
+
+
+class _CallbackBuffer(io.RawIOBase):
+    """
+    Internal _StdStream buffer that triggers flush callback.
+
+    Parmeters
+    ---------
+    flush_callback
+        Function to call at each flush.
+    """
+
+    def __init__(self, flush_callback: Callable[[str], None]):
+        self._flush_callback = flush_callback
+
+    def writable(self):
+        return True
+
+    def seekable(self):
+        return False
+
+    def isatty(self):
+        return True
+
+    def write(self, data):
+        self._flush_callback(data.tobytes().decode())
+        return len(data)
+
+
 class InteractiveConsole(code.InteractiveConsole):
-    """
+    """Interactive Pyodide console
+
     Base implementation for an interactive console that manages
-    stdout/stderr. It does not actually run any code.
+    stdout/stderr. Their value will be restored at destruction or
+    at `restore_stdstreams` call.
 
-    A subclass may overload runcode and access std streams via
-    sys.stdout.getvalue() and sys.stderr.getvalue(). Remember to
-    start calling parent's runcode.
+    `self.stdout_callback` and `self.stderr_callback` can be overloaded.
+
+    Parameters
+    ----------
+    stdout_callback
+        Function to call at each `sys.stdout` flush.
+    stderr_callback
+        Function to call at each `sys.stderr` flush.
     """
 
-    def __init__(self, locals: Optional[dict] = None):
+    def __init__(
+        self,
+        locals: Optional[dict] = None,
+        stdout_callback: Optional[Callable[[str], None]] = None,
+        stderr_callback: Optional[Callable[[str], None]] = None,
+    ):
         super().__init__(locals)
+        self.stdout_callback = stdout_callback
+        self.stderr_callback = stderr_callback
+        self.redirect_stdstreams()
 
-    def runcode(self, code):
-        sys.stdout = io.StringIO()
-        sys.stderr = io.StringIO()
+    def redirect_stdstreams(self):
+        """ Toggle stdout/stderr redirections. """
+
+        # we use meta callbacks to allow self.std{out,err}_callback
+        # overloading.
+        # we check callback against None at each call since it can be
+        # changed dynamically.
+
+        def meta_stdout_callback(*args):
+            if self.stdout_callback is not None:
+                return self.stdout_callback(*args)
+
+        def meta_stderr_callback(*args):
+            if self.stderr_callback is not None:
+                return self.stderr_callback(*args)
+
+        # for later restore
+        self._old_stdout = sys.stdout
+        self._old_stderr = sys.stderr
+
+        sys.stdout = _StdStream(meta_stdout_callback)
+        sys.stderr = _StdStream(meta_stderr_callback)
+
+    def restore_stdstreams(self):
+        """Restore stdout/stderr to the value it was before
+        the creation of the object."""
+        sys.stdout = self._old_stdout
+        sys.stderr = self._old_stderr
+
+    def __del__(self):
+        self.restore_stdstreams()
