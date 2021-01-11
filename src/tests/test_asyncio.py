9b51a589e40455662cc0653aca8d1bf244c6abff
Hood <hood@mit.edu>
2021-01-10 16:49:05 -0800

    Add new eval_code_async tests

--- src/tests/test_asyncio.py
+++ src/tests/test_asyncio.py
@@ -127,3 +127,137 @@ def test_await_error(selenium):
             r2 = c.send(r1.result())
             """
         )
+
+
+from pyodide._base import eval_code_async
+
+
+def test_eval_code_async_simple():
+    c = eval_code_async("1+92")
+    with pytest.raises(StopIteration, match="93"):
+        c.send(None)
+
+
+import asyncio
+
+
+def test_eval_code_async_loop():
+    async def slow_identity(i):
+        await asyncio.sleep(0.1)
+        return i
+
+    c = eval_code_async(
+        """
+        tot = 0
+        for i in range(10):
+            tot += await slow_identity(i)
+        tot
+        """,
+        globals=globals(),
+        locals=locals(),
+    )
+    fut = asyncio.ensure_future(c)
+    asyncio.get_event_loop().run_until_complete(fut)
+    assert fut.result() == 45
+
+
+def test_eval_code_await_jsproxy(selenium):
+    selenium.run(ASYNCIO_EVENT_LOOP_STARTUP)
+    selenium.run(
+        """
+        def prom(res,rej):
+            global resolve
+            resolve = res
+        from js import Promise
+        p = Promise.new(prom)
+        from pyodide._base import eval_code_async
+        c = eval_code_async(
+            '''
+            x = await p
+            x + 7
+            ''',
+            globals=globals()
+        )
+        resolve(10)
+        r = c.send(None)
+        """
+    )
+    time.sleep(0.01)
+    msg = "StopIteration: 17"
+    with pytest.raises(selenium.JavascriptException, match=msg):
+        selenium.run(
+            """
+            c.send(r.result())
+            """
+        )
+
+
+def test_eval_code_await_fetch(selenium):
+    selenium.run(ASYNCIO_EVENT_LOOP_STARTUP)
+    selenium.run(
+        """
+        from js import fetch, window
+        fetch = fetch.bind(window)
+        from pyodide._base import eval_code_async
+        c = eval_code_async(
+            '''
+            response = await fetch("console.html")
+            await response.text()
+            ''',
+            globals=globals()
+        )
+        r1 = c.send(None)
+        """
+    )
+    time.sleep(0.1)
+    selenium.run(
+        """
+        r2 = c.send(r1.result())
+        """
+    )
+    time.sleep(0.1)
+    msg = "StopIteration: <!doctype html>"
+    with pytest.raises(selenium.JavascriptException, match=msg):
+        selenium.run(
+            """
+            c.send(r2.result())
+            """
+        )
+
+
+def test_eval_code_await_error(selenium):
+    selenium.run_js(
+        """
+        async function async_js_raises(){
+            console.log("Hello there???");
+            throw new Error("This is an error message!");
+        }
+        window.async_js_raises = async_js_raises;
+        function js_raises(){
+            throw new Error("This is an error message!");
+        }
+        window.js_raises = js_raises;
+        """
+    )
+    selenium.run(ASYNCIO_EVENT_LOOP_STARTUP)
+    selenium.run(
+        """
+        from js import async_js_raises, js_raises
+        from pyodide._base import eval_code_async
+        c = eval_code_async(
+            '''
+            await async_js_raises()
+            ''',
+            globals=globals()
+        )
+        r1 = c.send(None)
+        """
+    )
+    msg = "This is an error message!"
+    with pytest.raises(selenium.JavascriptException, match=msg):
+        # Wait for event loop to go around for chome
+        selenium.run(
+            """
+            r2 = c.send(r1.result())
+            """
+        )
