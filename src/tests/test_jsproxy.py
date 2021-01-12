709118c0291f66d5bf736e885e37d75ab209df2c
Hood <hood@mit.edu>
2021-01-11 20:30:33 -0800

    Added tests

--- src/tests/test_jsproxy.py
+++ src/tests/test_jsproxy.py
@@ -314,29 +314,18 @@ def test_await_error(selenium):
 
 
 @run_in_pyodide
-def test_window_invocation(selenium):
-    """ Make sure js.setTimeout etc don't yeild illegal invocation errors. """
-    selenium.run(
-        """
-        import js
-        def temp():
-            print("okay?")
-        js.setTimeout(temp, 100)
-        js.fetch("example.com")
-        """
-    )
+def test_import_invocation():
+    import js
+
+    def temp():
+        print("okay?")
+
+    js.setTimeout(temp, 100)
+    js.fetch("packages.json")
 
 
 @run_in_pyodide
-def test_import_bind(selenium):
-    selenium.run(
-        """
-        from js import Promise
-        Promise.new
-        from js import fetch
-        fetch("example.com")
-        from js import window
-        # window.Promise.new # broken without PR #1124
-        window.fetch("example.com")
-        """
-    )
+def test_import_bind():
+    from js import fetch
+
+    fetch("packages.json")
