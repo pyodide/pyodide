5e237e23af1cc906d2f37c9d0eceb30d8aaa34ea
Dexter Chua <dalcde@users.noreply.github.com>
2021-01-13 01:02:11 +0800

    MAINT Tidy up webworker.js (#1127)

--- src/webworker.js
+++ src/webworker.js
@@ -1,25 +1,24 @@
 self.languagePluginUrl = '{{ PYODIDE_BASE_URL }}'
 importScripts('./pyodide.js')
 
-var onmessage = function(e) { // eslint-disable-line no-unused-vars
-  languagePluginLoader.then(() => {
-    const data = e.data;
-    const keys = Object.keys(data);
-    for (let key of keys) {
-      if (key !== 'python') {
-        // Keys other than python must be arguments for the python script.
-        // Set them on self, so that `from js import key` works.
-        self[key] = data[key];
-      }
+onmessage = async function(e) {
+  await languagePluginLoader;
+  const data = e.data;
+  for (let key of Object.keys(data)) {
+    if (key !== 'python') {
+      // Keys other than python must be arguments for the python script.
+      // Set them on self, so that `from js import key` works.
+      self[key] = data[key];
     }
+  }
 
-    self.pyodide.runPythonAsync(data.python, () => {})
-        .then((results) => { self.postMessage({results}); })
-        .catch((err) => {
-          // if you prefer messages with the error
-          self.postMessage({error : err.message});
-          // if you prefer onerror events
-          // setTimeout(() => { throw err; });
-        });
-  });
+  try {
+    self.postMessage(
+        {results : await self.pyodide.runPythonAsync(data.python)});
+  } catch (e) {
+    // if you prefer messages with the error
+    self.postMessage({error : e.message});
+    // if you prefer onerror events
+    // setTimeout(() => { throw err; });
+  }
 }
