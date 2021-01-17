ec7db5ff6a6abc519b6d76203ef304a5404e7966
Hood <hood@mit.edu>
2021-01-16 17:12:32 -0800

    JsImporter ==> JsFinder

--- src/pyodide-py/pyodide/__init__.py
+++ src/pyodide-py/pyodide/__init__.py
@@ -1,14 +1,14 @@
 from ._base import open_url, eval_code, find_imports, as_nested_list
 from ._core import JsException  # type: ignore
-from ._importhooks import JsImporter
+from ._importhooks import JsFinder
 
 import platform
 import sys
 
-jsimporter = JsImporter()
-register_js_module = jsimporter.register_js_module
-unregister_js_module = jsimporter.unregister_js_module
-sys.meta_path.append(jsimporter)  # type: ignore
+jsfinder = JsFinder()
+register_js_module = jsfinder.register_js_module
+unregister_js_module = jsfinder.unregister_js_module
+sys.meta_path.append(jsfinder)  # type: ignore
 
 
 __version__ = "0.16.1"
