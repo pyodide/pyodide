53d4741fe23cf9d6cdc58bd6a730f32494fd8fe9
Hood <hood@mit.edu>
2021-01-29 14:09:48 -0800

    Fix compile error

--- src/core/jsproxy.c
+++ src/core/jsproxy.c
@@ -178,7 +178,7 @@ JsProxy_RichCompare(PyObject* a, PyObject* b, int op)
 }
 
 static PyObject*
-JsProxy_GetIter(PyObject* o, PyObject* _args)
+JsProxy_GetIter(PyObject* o)
 {
   JsProxy* self = (JsProxy*)o;
 
@@ -389,10 +389,6 @@ static PyNumberMethods JsProxy_NumberMethods = {
 };
 
 static PyMethodDef JsProxy_Methods[] = {
-  { "__iter__",
-    (PyCFunction)JsProxy_GetIter,
-    METH_NOARGS,
-    "Get an iterator over the object" },
   { "__await__", (PyCFunction)JsProxy_Await, METH_NOARGS, ""},
   { "__dir__",
     (PyCFunction)JsProxy_Dir,
