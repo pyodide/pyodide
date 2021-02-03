8eaef2f148c4db93c81f0c8268c692e28d2a6236
Hood Chatham <roberthoodchatham@gmail.com>
2021-02-03 15:27:31 -0800

    Update src/pyodide-py/pyodide/__init__.py
    Co-authored-by: Roman Yurchak <rth.yurchak@gmail.com>

--- src/pyodide-py/pyodide/__init__.py
+++ src/pyodide-py/pyodide/__init__.py
@@ -1,7 +1,7 @@
 from ._base import open_url, eval_code, find_imports, as_nested_list
 from ._core import JsException  # type: ignore
 from ._importhooks import JsFinder
-from .simple_web_loop import SimpleWebLoopPolicy
+from .web_loop import WebLoopPolicy
 import asyncio
 import sys
 import platform
