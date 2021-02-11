f7083cc3fccfe0bc22b0bd5e575d8b979dfb1884
Hood <hood@mit.edu>
2021-02-10 18:59:46 -0800

    lint

--- packages/numpy/test_numpy.py
+++ packages/numpy/test_numpy.py
@@ -2,13 +2,9 @@ def test_numpy(selenium):
     selenium.load_package("numpy")
     selenium.run("import numpy")
     selenium.run("x = numpy.ones((32, 64))")
-    assert selenium.run_js(
-        "return pyodide.pyimport('x').toJs().length == 32"
-    )
+    assert selenium.run_js("return pyodide.pyimport('x').toJs().length == 32")
     for i in range(32):
-        assert selenium.run_js(
-            f"return pyodide.pyimport('x').toJs()[{i}].length == 64"
-        )
+        assert selenium.run_js(f"return pyodide.pyimport('x').toJs()[{i}].length == 64")
         for j in range(64):
             assert selenium.run_js(
                 f"return pyodide.pyimport('x').toJs()[{i}][{j}] == 1"
@@ -112,18 +108,9 @@ def test_python2js_numpy_dtype(selenium_standalone):
     assert selenium.run("np.array([True, False])") == [True, False]
 
     selenium.run("x = np.array([['string1', 'string2'], ['string3', 'string4']])")
-    assert (
-        selenium.run_js("return pyodide.pyimport('x').toJs().length")
-        == 2
-    )
-    assert (
-        selenium.run_js("return pyodide.pyimport('x').toJs()[0][0]")
-        == "string1"
-    )
-    assert (
-        selenium.run_js("return pyodide.pyimport('x').toJs()[1][1]")
-        == "string4"
-    )
+    assert selenium.run_js("return pyodide.pyimport('x').toJs().length") == 2
+    assert selenium.run_js("return pyodide.pyimport('x').toJs()[0][0]") == "string1"
+    assert selenium.run_js("return pyodide.pyimport('x').toJs()[1][1]") == "string4"
 
 
 def test_py2js_buffer_clear_error_flag(selenium):
