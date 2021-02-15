1dbd8ceecaf8b22cf20d7986e363a55b98d5082b
Hood <hood@mit.edu>
2021-02-14 18:22:35 -0800

    Update test

--- src/tests/test_pyproxy.py
+++ src/tests/test_pyproxy.py
@@ -220,13 +220,12 @@ def test_pyproxy_mixins(selenium):
         let name_proxy = {noimpls, awaitable, iterable, iterator, awaititerable, awaititerator};
         let result = {};
         for(let [name, x] of Object.entries(name_proxy)){
-            let impls = { 
-                "then" : x.then !== undefined,
-                "catch" : x.catch !== undefined,
-                "finally_" : x.finally !== undefined,
-                "iterable" : x[Symbol.iterator] !== undefined,
-                "iterator" : x.next !== undefined
+            let impls = {};
+            for(let field of ["then", "catch", "finally", "iterable", "iterator]){
+                impls[field] = field in x;
             }
+            impls["finally_"] = impls["finally"];
+            delete impls["finally"];
             result[name] = impls;
         }
         return result;
