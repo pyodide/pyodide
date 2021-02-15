45def834d240dc4e6fc4a54eb2b74d1716cf5b8c
Hood <hood@mit.edu>
2021-02-14 19:02:39 -0800

    Improve error handling and reference counting in to_py

--- src/core/js2python.c
+++ src/core/js2python.c
@@ -155,7 +155,10 @@ EM_JS_NUM(errcode, js2python_init, (), {
     return 0;
   };
 
-  Module.__js2python_convertList = function(obj, map, depth)
+  class TempError extends Error
+  {};
+
+  Module.__js2python_convertList = function(obj, cache, depth)
   {
     let list = _PyList_New(obj.length);
     // clang-format off
@@ -163,141 +166,197 @@ EM_JS_NUM(errcode, js2python_init, (), {
       // clang-format on
       return 0;
     }
-    map.set(obj, list);
-    for (let i = 0; i < obj.length; i++) {
-      let entryid = Module.hiwire.new_value(obj[i]);
-      let item = Module.__js2python_convert(entryid, map, depth);
-      Module.hiwire.decref(entryid);
-      // clang-format off
-      if (item === 0) {
-        // clang-format on
-        _Py_DecRef(list);
-        return 0;
+    let entryid = 0;
+    let item = 0;
+    try {
+      cache.set(obj, list);
+      for (let i = 0; i < obj.length; i++) {
+        entryid = Module.hiwire.new_value(obj[i]);
+        item = Module.__js2python_convert(entryid, cache, depth);
+        // clang-format off
+        if (item === 0) {
+          // clang-format on
+          throw new TempError();
+        }
+        // clang-format off
+        // PyList_SetItem steals a reference to item no matter what
+        _Py_IncRef(item);
+        if (_PyList_SetItem(list, i, item) === -1) {
+          // clang-format on
+          throw new TempError();
+        }
+        Module.hiwire.decref(entryid);
+        entryid = 0;
+        _Py_DecRef(item);
+        item = 0;
       }
-      // PyList_SetItem steals a reference to item no matter what
-      let errcode = _PyList_SetItem(list, i, item);
-      // clang-format off
-      if (errcode === -1) {
-        // clang-format on
-        _Py_DecRef(list);
+    } catch (e) {
+      Module.hiwire.decref(entryid);
+      _Py_DecRef(item);
+      _Py_DecRef(list);
+      if (e instanceof TempError) {
         return 0;
+      } else if (_PyErr_Occurred()) {
+        return 0;
+      } else {
+        throw e;
       }
     }
+
     return list;
   };
 
-  Module.__js2python_convertMap = function(obj, entries, map, depth)
+  Module.__js2python_convertMap = function(obj, entries, cache, depth)
   {
-    if (map.has(1) && map.has(true)) {
-      throw new Error("Cannot faithfully convert Map into Python since it " +
-                      "contains both 1 and true as keys.");
-    }
-    if (map.has(0) && map.has(false)) {
-      throw new Error("Cannot faithfully convert Map into Python since it " +
-                      "contains both 0 and false as keys.");
-    }
     let dict = _PyDict_New();
     // clang-format off
     if (dict === 0) {
       // clang-format on
       return 0;
     }
-    map.set(obj, dict);
-    for (let[key_js, value_js] of entries) {
-      let key_id = Module.hiwire.new_value(key_js);
-      let key_py = Module.__js2python_convert(key_id, map, depth);
-      Module.hiwire.decref(key_id);
-      // clang-format off
-      if (key_py === 0) {
-        // clang-format on
-        _Py_DecRef(dict);
-        return 0;
-      }
+    let key_py = 0;
+    let value_id = 0;
+    let value_py = 0;
+    try {
+      cache.set(obj, dict);
+      for (let[key_js, value_js] of entries) {
+        key_py = Module.__js2python_convertImmutable(key_js);
+        // clang-format off
+        if (key_py === 0) {
+          // clang-format on
+          if (_PyErr_Occurred()) {
+            throw new TempError();
+          } else {
+            let key_type =
+              (key_js.constructor && key_js.constructor.name) || typeof(key_js);
+            // clang-format off
+            throw new Error(`Cannot use key of type ${key_type} as a key to a Python dict`);
+            // clang-format on
+          }
+        }
+        value_id = Module.hiwire.new_value(value_js);
+        value_py = Module.__js2python_convert(value_id, cache, depth);
+        // clang-format off
+        if (value_py === 0) {
+          // clang-format on
+          throw new TempError();
+        }
 
-      let value_id = Module.hiwire.new_value(value_js);
-      let value_py = Module.__js2python_convert(value_id, map, depth);
-      Module.hiwire.decref(value_id);
-      // clang-format off
-      if (value_py === 0) {
-        // clang-format on
-        _Py_DecRef(dict);
-        return 0;
+        // clang-format off
+        if (_PyDict_SetItem(dict, key_py, value_py) === -1) {
+          // clang-format on
+          throw new TempError();
+        }
+        _Py_DecRef(key_py);
+        key_py = 0;
+        Module.hiwire.decref(value_id);
+        value_id = 0;
+        _Py_DecRef(value_py);
+        value_py = 0;
       }
-
-      let errcode = _PyDict_SetItem(dict, key_py, value_py);
+    } catch (e) {
       _Py_DecRef(key_py);
+      Module.hiwire.decref(value_id);
       _Py_DecRef(value_py);
-      // clang-format off
-      if (errcode === -1) {
-        // clang-format on
-        _Py_DecRef(dict);
+      _Py_DecRef(dict);
+      if (e instanceof TempError) {
         return 0;
+      } else if (_PyErr_Occurred()) {
+        return 0;
+      } else {
+        throw e;
       }
     }
     return dict;
   };
 
-  Module.__js2python_convertSet = function(obj, map, depth)
+  Module.__js2python_convertSet = function(obj, cache, depth)
   {
-    if (set.has(1) && set.has(true)) {
-      throw new Error("Cannot faithfully convert Set into Python since it " +
-                      "contains both 1 and true as keys.");
-    }
-    if (set.has(0) && set.has(false)) {
-      throw new Error("Cannot faithfully convert Set into Python since it " +
-                      "contains both 0 and false as keys.");
-    }
     let set = _PySet_New(0);
     // clang-format off
     if (set === 0) {
       // clang-format on
       return 0;
     }
-    map.set(obj, set);
-    for (let key_js of obj) {
-      let key_id = Module.hiwire.new_value(key_js);
-      let key_py = Module.__js2python_convert(key_id, map, depth);
-      Module.hiwire.decref(key_id);
-      // clang-format off
-      if (key_py === 0) {
-        // clang-format on
-        _Py_DecRef(set);
-        return 0;
+    let key_py = 0;
+    try {
+      cache.set(obj, set);
+      for (let key_js of obj) {
+        key_py = Module.__js2python_convertImmutable(key_js);
+        // clang-format off
+        if (key_py === 0) {
+          // clang-format on
+          if (_PyErr_Occurred()) {
+            throw new TempError();
+          } else {
+            let key_type =
+              (key_js.constructor && key_js.constructor.name) || typeof(key_js);
+            // clang-format off
+            throw new Error(`Cannot use key of type ${key_type} as a key to a Python set`);
+            // clang-format on
+          }
+        }
+        let errcode = _PySet_Add(set, key_py);
+        // clang-format off
+        if (errcode === -1) {
+          // clang-format on
+          throw new TempError();
+        }
+        _Py_DecRef(key_py);
+        key_py = 0;
       }
-      let errcode = _PySet_Add(set, key_py);
+    } catch (e) {
       _Py_DecRef(key_py);
-      // clang-format off
-      if (errcode === -1) {
-        // clang-format on
-        _Py_DecRef(set);
+      _Py_DecRef(set);
+      if (e instanceof TempError) {
         return 0;
+      } else if (_PyErr_Occurred()) {
+        return 0;
+      } else {
+        throw e;
       }
     }
     return set;
   };
 
-  Module.__js2python_convertOther = function(id, value, map, depth)
+  function checkBoolIntCollision(obj, ty)
+  {
+    if (obj.has(1) && obj.has(true)) {
+      throw new Error(`Cannot faithfully convert ${
+                        ty } into Python since it ` +
+                      "contains both 1 and true as keys.");
+    }
+    if (obj.has(0) && obj.has(false)) {
+      throw new Error(`Cannot faithfully convert ${
+                        ty } into Python since it ` +
+                      "contains both 0 and false as keys.");
+    }
+  }
+
+  Module.__js2python_convertOther = function(id, value, cache, depth)
   {
     let toStringTag = Object.prototype.toString.call(value);
     // clang-format off
     if (Array.isArray(value) || value === "[object HTMLCollection]" ||
                                            value === "[object NodeList]") {
-      return Module.__js2python_convertList(value, map, depth);
+      return Module.__js2python_convertList(value, cache, depth);
     }
     if (toStringTag === "[object Map]" || value instanceof Map) {
-      return Module.__js2python_convertMap(value, value.entries(), map, depth);
+      checkBoolIntCollision(value, "Map");
+      return Module.__js2python_convertMap(value, value.entries(), cache, depth);
     }
     if (toStringTag === "[object Set]" || value instanceof Set) {
-      return Module.__js2python_convertSet(value, map, depth);
+      checkBoolIntCollision(value, "Set");
+      return Module.__js2python_convertSet(value, cache, depth);
     }
     if (toStringTag === "[object Object]" && (value.constructor === undefined || value.constructor.name === "Object")) {
-      return Module.__js2python_convertMap(value, Object.entries(value), map, depth);
+      return Module.__js2python_convertMap(value, Object.entries(value), cache, depth);
     }
     // clang-format on
     return _JsProxy_create(id);
   };
 
-  Module.__js2python_convert = function(id, map, depth)
+  Module.__js2python_convert = function(id, cache, depth)
   {
     let value = Module.hiwire.get_value(id);
     let result = Module.__js2python_convertImmutable(value);
@@ -308,12 +367,12 @@ EM_JS_NUM(errcode, js2python_init, (), {
     if (depth === 0) {
       return _JsProxy_create(id);
     }
-    result = map.get(value);
+    result = cache.get(value);
     if (result !== undefined) {
       return result;
     }
     // clang-format on
-    return Module.__js2python_convertOther(id, value, map, depth - 1);
+    return Module.__js2python_convertOther(id, value, cache, depth - 1);
   };
 
   return 0;
