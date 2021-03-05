e3647a8fbe6dab6f9f9d93f8bc936add66b721ca
pmp-p <mail.peny@free.fr>
2021-03-05 17:06:58 +0100

    incremental package building

--- build-system-3.8.sh
+++ build-system-3.8.sh
@@ -2,52 +2,34 @@
 reset
 
 # running in pyodide git tree from CI
-if [ -d /home/runner/work ]
+if [ -d /home/runner/work/pyodide ]
 then
     patch -p1 < build-system-3.8.diff
     mkdir -p /home/runner/work/pyodide/pyodide/packages/.artifacts/lib/python
+else
+    mkdir -p packages/.artifacts/lib/python
 fi
 
 
 # trouble with cross compilation or circular deps on a failed package
 # most likely pkg that rely on egg decompression or include path redirection
-echo "FAILED=swiglpk,optlang,mne,scipy,pywavelets,\
-scikit-learn,scikit-image,\
-astropy,\
-"
-
-TESTING="statsmodels,"
-
-# kiwisolver need cppyy unpacking
-
-EGGUNPACK="kiwisolver,"
-
-CIRCULAR="\
-pandas,setuptools,glpk,traits,nlopt,\
-Jinja2,jedi,autograd,matplotlib,cloudpickle,docutils,\
-networkx,lxml,pytest,mpmath,nltk,bleach,yt,\
-zarr,cytoolz,python-sat,biopython,\
-"
-
-
-# clean packages
-
-MINIMAL="micropip,distlib,parso,pytz,python-dateutil,six,zlib,future,"
-
-# regex failed once
-
-FULL="${MINIMAL},regex,Pygments,attrs,numpy,pillow,webencodings,\
-html5lib,imageio,joblib,libiconv,libxml,libxslt,xlrd,asciitree,beautifulsoup4,\
-packaging,cssselect,patsy,pluggy,msgpack,MarkupSafe,more-itertools,pyparsing,decorator,\
-CLAPACK,pyodide-interrupts,toolz,uncertainties,atomicwrites,\
-numcodecs,nose,cycler,soupsieve,sympy,freesasa,\
-"
 
 # TODO incremental build of those
-# ${MINIMAL}${EGGUNPACK}${CIRCULAR}${TESTING}py
+# regex failed once
+PACKAGES="\
+ kiwisolver\
+ pandas setuptools glpk traits nlopt Jinja2 jedi autograd matplotlib cloudpickle docutils\
+ networkx lxml pytest mpmath nltk bleach yt zarr cytoolz python-sat biopython\
+ regex Pygments attrs numpy pillow webencodings\
+ html5lib imageio joblib libiconv libxml libxslt xlrd asciitree beautifulsoup4\
+ packaging cssselect patsy pluggy msgpack MarkupSafe more-itertools pyparsing decorator\
+ CLAPACK pyodide-interrupts toolz uncertainties atomicwrites\
+ numcodecs nose cycler soupsieve sympy freesasa\
+ statsmodels\
+ swiglpk optlang mne scipy pywavelets scikit-learn scikit-image astropy\
+"
 
 
-export PYODIDE_PACKAGES="micropip,${MINIMAL}py"
 
 export HOSTPYTHON=$(command -v python3.8)
 export PYTHON_FOR_BUILD=$HOSTPYTHON
@@ -59,6 +41,30 @@ ln -sf $HOSTPYTHON ./bin/python3.8
 
 echo $PYTHON_FOR_BUILD > bin/PYTHON_FOR_BUILD
 
-PATH=$(pwd)/bin:$PATH make
+export PATH=$(pwd)/bin:$PATH
+
+# clean packages
+PYODIDE_PACKAGES="micropip,distlib,parso,pytz,python-dateutil,six,zlib,future,py" make
 
 
+unset PYODIDE_PACKAGES
+
+> FAILURES
+
+for package in $PACKAGES
+do
+    echo "building $package ..."
+    if PYODIDE_PACKAGES="$package" make 2>&1 >/dev/null
+    then
+        echo "$package -> ok"
+    else
+        echo "$package" >> FAILURES
+        echo "$package :  FAILED !"
+        echo
+    fi
+done
+
+echo
+echo
+
+cat FAILURES
