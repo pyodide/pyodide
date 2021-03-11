1e4fa3d9d59fd353237b7b114786f2c094fe7bdf
Hood <hood@mit.edu>
2021-03-11 14:29:27 -0800

    Move error handling code from python2js to error_handling

--- src/core/python2js.c
+++ src/core/python2js.c
@@ -9,141 +9,11 @@
 
 #include "python2js_buffer.h"
 
-static PyObject* tbmod = NULL;
-
-_Py_IDENTIFIER(format_exception);
-_Py_IDENTIFIER(last_type);
-_Py_IDENTIFIER(last_value);
-_Py_IDENTIFIER(last_traceback);
-
 static JsRef
 _python2js_unicode(PyObject* x);
 
 static inline JsRef
 _python2js_immutable(PyObject* x);
-
-EM_JS_REF(JsRef, new_error, (const char* msg, JsRef pyproxy), {
-  return Module.hiwire.new_value(new Module.PythonError(
-    UTF8ToString(msg), Module.hiwire.get_value(pyproxy)));
-});
-
-static int
-fetch_and_normalize_exception(PyObject** type,
-                              PyObject** value,
-                              PyObject** traceback)
-{
-  PyErr_Fetch(type, value, traceback);
-  PyErr_NormalizeException(type, value, traceback);
-  if (*type == NULL || *type == Py_None || *value == NULL ||
-      *value == Py_None) {
-    Py_CLEAR(*type);
-    Py_CLEAR(*value);
-    PyErr_SetString(PyExc_TypeError, "No exception type or value");
-    FAIL();
-  }
-
-  if (*traceback == NULL) {
-    *traceback = Py_None;
-    Py_INCREF(*traceback);
-  }
-  PyException_SetTraceback(*value, *traceback);
-  return 0;
-
-finally:
-  return -1;
-}
-
-static PyObject*
-format_exception_traceback(PyObject* type, PyObject* value, PyObject* traceback)
-{
-  PyObject* pylines = NULL;
-  PyObject* empty = NULL;
-  PyObject* result = NULL;
-
-  pylines = _PyObject_CallMethodIdObjArgs(
-    tbmod, &PyId_format_exception, type, value, traceback, NULL);
-  FAIL_IF_NULL(pylines);
-  empty = PyUnicode_New(0, 0);
-  FAIL_IF_NULL(empty);
-  result = PyUnicode_Join(empty, pylines);
-  FAIL_IF_NULL(result);
-
-finally:
-  Py_CLEAR(pylines);
-  Py_CLEAR(empty);
-  return result;
-}
-
-JsRef
-wrap_exception(bool attach_python_error)
-{
-  bool success = false;
-  PyObject* type = NULL;
-  PyObject* value = NULL;
-  PyObject* traceback = NULL;
-  PyObject* pystr;
-  JsRef pyexc_proxy = NULL;
-  JsRef jserror = NULL;
-
-  FAIL_IF_MINUS_ONE(fetch_and_normalize_exception(&type, &value, &traceback));
-  pystr = format_exception_traceback(type, value, traceback);
-  FAIL_IF_NULL(pystr);
-  const char* pystr_utf8 = PyUnicode_AsUTF8(pystr);
-  FAIL_IF_NULL(pystr_utf8);
-
-  if (attach_python_error) {
-    pyexc_proxy = pyproxy_new(value);
-  } else {
-    pyexc_proxy = Js_undefined;
-  }
-  jserror = new_error(pystr_utf8, pyexc_proxy);
-
-  success = true;
-finally:
-  // Log an appropriate warning.
-  if (success) {
-    EM_ASM(
-      {
-        let msg = Module.hiwire.get_value($0).message;
-        console.warn("Python exception:\n" + msg + "\n");
-      },
-      jserror);
-  } else {
-    PySys_WriteStderr("Error occurred while formatting traceback:\n");
-    PyErr_Print();
-    if (type != NULL) {
-      PySys_WriteStderr("\nOriginal exception was:\n");
-      PyErr_Display(type, value, traceback);
-    }
-  }
-
-  if (success) {
-    _PySys_SetObjectId(&PyId_last_type, type);
-    _PySys_SetObjectId(&PyId_last_value, value);
-    _PySys_SetObjectId(&PyId_last_traceback, traceback);
-  }
-  Py_CLEAR(type);
-  Py_CLEAR(value);
-  Py_CLEAR(traceback);
-  Py_CLEAR(pystr);
-  hiwire_CLEAR(pyexc_proxy);
-  if (!success) {
-    hiwire_CLEAR(jserror);
-  }
-  return jserror;
-}
-
-void _Py_NO_RETURN
-pythonexc2js()
-{
-  JsRef jserror = wrap_exception(false);
-  if (jserror == NULL) {
-    jserror =
-      new_error("Error occurred while formatting traceback", Js_undefined);
-  }
-  // hiwire_throw_error steals jserror
-  hiwire_throw_error(jserror);
-}
 
 int
 _python2js_add_to_cache(PyObject* cache, PyObject* pyparent, JsRef jsparent);
@@ -566,31 +436,5 @@ python2js_with_depth(PyObject* x, int depth)
 int
 python2js_init()
 {
-  bool success = false;
-  EM_ASM({
-    class PythonError extends Error
-    {
-      constructor(message, pythonError)
-      {
-        super(message);
-        this.name = this.constructor.name;
-        this.pythonError = pythonError;
-      }
-
-      clear()
-      {
-        if (this.pythonError) {
-          this.pythonError.destroy();
-          delete this.pythonError;
-        }
-      }
-    };
-    Module.PythonError = PythonError;
-  });
-
-  tbmod = PyImport_ImportModule("traceback");
-  FAIL_IF_NULL(tbmod);
-  success = true;
-finally:
-  return success ? 0 : -1;
+  return 0;
 }
