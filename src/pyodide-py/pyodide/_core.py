cd7ca5ad4142b8e2f4a0eb4b6bba34e23e1e72fe
Hood <hood@mit.edu>
2021-03-17 12:49:44 -0700

    lint

--- src/pyodide-py/pyodide/_core.py
+++ src/pyodide-py/pyodide/_core.py
@@ -4,7 +4,6 @@ if platform.system() == "Emscripten":
     from _pyodide_core import (
         JsProxy,
         JsException,
-        JsBuffer,
         create_proxy,
         create_once_proxy,
     )
