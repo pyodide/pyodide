00fcd848a275fc0e2953e441e5aa0d4cadebd744
Hood <hood@mit.edu>
2021-03-17 13:10:50 -0700

    Use create_once_proxy in console.py

--- src/pyodide-py/pyodide/console.py
+++ src/pyodide-py/pyodide/console.py
@@ -219,6 +219,7 @@ class InteractiveConsole(code.InteractiveConsole):
         function sets the promise `self.run_complete`.
         If you need to wait for the end of the computation,
         you should await for it."""
+        from pyodide import create_once_proxy
         parent_runcode = super().runcode
         source = "\n".join(self.buffer)
 
@@ -233,9 +234,9 @@ class InteractiveConsole(code.InteractiveConsole):
                     self.flush_all()
                 return _dummy_promise
 
-            return _load_packages_from_imports(source).then(run)
+            return _load_packages_from_imports(source).then(create_once_proxy(run))
 
-        self.run_complete = self.run_complete.then(load_packages_and_run)
+        self.run_complete = self.run_complete.then(create_once_proxy(load_packages_and_run))
 
     def __del__(self):
         self.restore_stdstreams()
