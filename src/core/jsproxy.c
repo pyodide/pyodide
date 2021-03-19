3f1064cc7aafaca21c579b1342af8756657573be
Hood <hood@mit.edu>
2021-03-19 09:37:52 -0700

    Fix catch, update docstrings

--- src/core/jsproxy.c
+++ src/core/jsproxy.c
@@ -666,7 +666,11 @@ finally:
 }
 
 /**
- * Overload for `then` for JsProxies with a `then` method.
+ * Overload for `then` for JsProxies with a `then` method. Of course without
+ * this overload, the call would just fall through to the normal `then`
+ * function. The advantage of this overload is that it automatically releases
+ * the references to the onfulfilled and onrejected callbacks, which is quite
+ * hard to do otherwise.
  */
 PyObject*
 JsProxy_then(JsProxy* self, PyObject* args, PyObject* kwds)
@@ -714,9 +718,11 @@ JsProxy_catch(JsProxy* self, PyObject* onrejected)
   JsRef result_promise = NULL;
   PyObject* result = NULL;
 
-  proxy = create_once_callback(onrejected);
-  FAIL_IF_NULL(proxy);
-  result_promise = hiwire_call_member_va(self->js, "catch", proxy, NULL);
+  // We have to use create_promise_handles so that the handler gets released
+  // even if the promise resolves successfully.
+  promise_handles = create_promise_handles(NULL, onrejected);
+  FAIL_IF_NULL(promise_handles);
+  result_promise = hiwire_call_member(self->js, "then", promise_handles);
   if (result_promise == NULL) {
     Py_DECREF(onrejected);
     FAIL();
@@ -730,7 +736,11 @@ finally:
 }
 
 /**
- * Overload for `finally` for JsProxies with a `then` method.
+ * Overload for `finally` for JsProxies with a `then` method. This isn't
+ * strictly necessary since one could get the same effect by just calling
+ * create_once_callback on the argument, but it'd be bad to have `then` and
+ * `catch` handle freeing the handler automatically but require something extra
+ * to use `finally`.
  */
 PyObject*
 JsProxy_finally(JsProxy* self, PyObject* onfinally)
@@ -739,6 +749,7 @@ JsProxy_finally(JsProxy* self, PyObject* onfinally)
   JsRef result_promise = NULL;
   PyObject* result = NULL;
 
+  // Finally method is called no matter what so we can use `create_once_callback`.
   proxy = create_once_callback(onfinally);
   FAIL_IF_NULL(proxy);
   result_promise = hiwire_call_member_va(self->js, "finally", proxy, NULL);
