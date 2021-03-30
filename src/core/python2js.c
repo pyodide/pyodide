pyodide/pyodide

commit ff706d33964de9941a26c42271f5db4c3da1bfd8

MAINT Improve core error handling: narrow types of exceptions we catch (#1385)

src/core/python2js.c
@@ -228,19 +228,24 @@ finally:
 }
 
 /**
- * Return x if x is not NULL.
+ * if x is NULL, fail
+ * if x is Js_novalue, do nothing
+ * in any other case, return x
  */
-#define RETURN_IF_SUCCEEDS(x)                                                  \
+#define RETURN_IF_HAS_VALUE(x)                                                 \
   do {                                                                         \
     JsRef _fresh_result = x;                                                   \
-    if (_fresh_result != NULL) {                                               \
+    FAIL_IF_NULL(_fresh_result);                                               \
+    if (_fresh_result != Js_novalue) {                                         \
       return _fresh_result;                                                    \
     }                                                                          \
   } while (0)
 
 /**
  * Convert x if x is an immutable python type for which there exists an
- * equivalent immutable Javascript type. Otherwise return NULL.
+ * equivalent immutable Javascript type. Otherwise return Js_novalue.
+ *
+ * Return type would be Option<JsRef>
  */
 static inline JsRef
 _python2js_immutable(PyObject* x)
@@ -258,12 +263,14 @@ _python2js_immutable(PyObject* x)
   } else if (PyUnicode_Check(x)) {
     return _python2js_unicode(x);
   }
-  return NULL;
+  return Js_novalue;
 }
 
 /**
  * If x is a wrapper around a Javascript object, unwrap the Javascript object
- * and return it. Otherwise, return NULL.
+ * and return it. Otherwise, return Js_novalue.
+ *
+ * Return type would be Option<JsRef>
  */
 static inline JsRef
 _python2js_proxy(PyObject* x)
@@ -273,7 +280,7 @@ _python2js_proxy(PyObject* x)
   } else if (JsException_Check(x)) {
     return JsException_AsJs(x);
   }
-  return NULL;
+  return Js_novalue;
 }
 
 /**
@@ -283,11 +290,8 @@ _python2js_proxy(PyObject* x)
 static JsRef
 _python2js_deep(PyObject* x, PyObject* cache, int depth)
 {
-  RETURN_IF_SUCCEEDS(_python2js_immutable(x));
-  FAIL_IF_ERR_OCCURRED();
-  RETURN_IF_SUCCEEDS(_python2js_proxy(x));
-  FAIL_IF_ERR_OCCURRED();
-
+  RETURN_IF_HAS_VALUE(_python2js_immutable(x));
+  RETURN_IF_HAS_VALUE(_python2js_proxy(x));
   if (PyList_Check(x) || PyTuple_Check(x)) {
     return _python2js_sequence(x, cache, depth);
   }
@@ -297,8 +301,9 @@ _python2js_deep(PyObject* x, PyObject* cache, int depth)
   if (PySet_Check(x)) {
     return _python2js_set(x, cache, depth);
   }
-  RETURN_IF_SUCCEEDS(_python2js_buffer(x));
-  PyErr_Clear();
+  if (PyObject_CheckBuffer(x)) {
+    return _python2js_buffer(x);
+  }
   return pyproxy_new(x);
 finally:
   return NULL;
@@ -379,11 +384,9 @@ finally:
 JsRef
 python2js(PyObject* x)
 {
-  RETURN_IF_SUCCEEDS(_python2js_immutable(x));
-  FAIL_IF_ERR_OCCURRED();
-  RETURN_IF_SUCCEEDS(_python2js_proxy(x));
-  FAIL_IF_ERR_OCCURRED();
-  RETURN_IF_SUCCEEDS(pyproxy_new(x));
+  RETURN_IF_HAS_VALUE(_python2js_immutable(x));
+  RETURN_IF_HAS_VALUE(_python2js_proxy(x));
+  RETURN_IF_HAS_VALUE(pyproxy_new(x));
 finally:
   if (PyErr_Occurred()) {
     if (!PyErr_ExceptionMatches(conversion_error)) {
@@ -418,7 +421,7 @@ python2js_with_depth(PyObject* x, int depth)
     hiwire_decref(obj);
   }
   Py_DECREF(cache);
-  if (result == NULL) {
+  if (result == NULL || result == Js_novalue) {
     if (PyErr_Occurred()) {
       if (!PyErr_ExceptionMatches(conversion_error)) {
         _PyErr_FormatFromCause(conversion_error,

src/core/hiwire.c
@@ -14,6 +14,9 @@ const JsRef Js_true = ((JsRef)(4));
 const JsRef Js_false = ((JsRef)(6));
 const JsRef Js_null = ((JsRef)(8));
 
+// For when the return value would be Option<JsRef>
+const JsRef Js_novalue = ((JsRef)(1000));
+
 JsRef
 hiwire_bool(bool boolean)
 {

src/core/hiwire.h
@@ -44,6 +44,9 @@ extern const JsRef Js_true;
 extern const JsRef Js_false;
 extern const JsRef Js_null;
 
+// For when the return value would be Option<JsRef>
+extern const JsRef Js_novalue;
+
 #define hiwire_CLEAR(x)                                                        \
   do {                                                                         \
     hiwire_decref(x);                                                          \

src/core/jsproxy.c
@@ -131,7 +131,7 @@ static PyObject*
 JsProxy_GetAttr(PyObject* self, PyObject* attr)
 {
   PyObject* result = PyObject_GenericGetAttr(self, attr);
-  if (result != NULL) {
+  if (result != NULL || !PyErr_ExceptionMatches(PyExc_AttributeError)) {
     return result;
   }
   PyErr_Clear();

src/core/pyproxy.c
@@ -279,7 +279,10 @@ _pyproxy_getitem(PyObject* pyobj, JsRef idkey)
 
   success = true;
 finally:
-  PyErr_Clear();
+  if (!success && (PyErr_ExceptionMatches(PyExc_KeyError) ||
+                   PyErr_ExceptionMatches(PyExc_IndexError))) {
+    PyErr_Clear();
+  }
   Py_CLEAR(pykey);
   Py_CLEAR(pyresult);
   if (!success) {

src/core/pyproxy.js
@@ -156,10 +156,16 @@ TEMP_EMJS_HELPER(() => {0, /* Magic, see comment */
      * @returns The Javascript object resulting from the conversion.
      */
     toJs(depth = -1) {
-      let idresult = _python2js_with_depth(_getPtr(this), depth);
-      let result = Module.hiwire.get_value(idresult);
-      Module.hiwire.decref(idresult);
-      return result;
+      let idresult;
+      try {
+        idresult = _python2js_with_depth(_getPtr(this), depth);
+      } catch (e) {
+        Module.fatal_error(e);
+      }
+      if (idresult === 0) {
+        _pythonexc2js();
+      }
+      return Module.hiwire.pop_value(idresult);
     }
     apply(jsthis, jsargs) {
       return Module.callPyObject(_getPtr(this), ...jsargs);

src/core/python2js_buffer.c
@@ -460,7 +460,6 @@ _python2js_buffer(PyObject* x)
 {
   PyObject* memoryview = PyMemoryView_FromObject(x);
   if (memoryview == NULL) {
-    PyErr_Clear();
     return NULL;
   }
 

commit fa42f3aae7bae8aeb1f3c58db039310b9ac8c3f9

Fix a bug introduced into python2js by #1385

src/core/python2js.c
@@ -260,6 +260,13 @@ _python2js_immutable(PyObject* x)
     return _python2js_long(x);
   } else if (PyFloat_Check(x)) {
     return _python2js_float(x);
+  } else if (PyNumber_Check(x)) {
+    JsRef result = _python2js_float(x);
+    if (result == NULL) {
+      PyErr_Clear();
+      return Js_novalue;
+    }
+    return result;
   } else if (PyUnicode_Check(x)) {
     return _python2js_unicode(x);
   }
@@ -394,8 +401,7 @@ finally:
                              "Conversion from python to javascript failed");
     }
   } else {
-    PyErr_SetString(internal_error,
-                    "Internal error occurred in python2js_with_depth");
+    PyErr_SetString(internal_error, "Internal error occurred in python2js");
   }
   return NULL;
 }
@@ -422,6 +428,7 @@ python2js_with_depth(PyObject* x, int depth)
   }
   Py_DECREF(cache);
   if (result == NULL || result == Js_novalue) {
+    result = NULL;
     if (PyErr_Occurred()) {
       if (!PyErr_ExceptionMatches(conversion_error)) {
         _PyErr_FormatFromCause(conversion_error,