8c78a15ff9cd24d58605f1f3aed109826ae9f656
Hood <hood@mit.edu>
2021-03-31 17:47:51 -0700

    Split off change to pyproxy.js

--- src/core/pyproxy.js
+++ src/core/pyproxy.js
@@ -716,10 +716,10 @@ TEMP_EMJS_HELPER(() => {0, /* Magic, see comment */
      *    "f32", or "f64,
      * @returns PyBuffer
      */
-    getBuffer : function(type) {
+    getBuffer : function(type = "u8") {
       let ArrayType = undefined;
       if (type) {
-        ArrayType = type_to_array_map.get(type);
+        let ArrayType = type_to_array_map.get(type);
         if (ArrayType === undefined) {
           throw new Error(`Unknown type ${type}`);
         }
