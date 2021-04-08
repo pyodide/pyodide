1ce9e698840086293448e656ab183abe6473f158
Hood <hood@mit.edu>
2021-04-08 15:54:28 -0700

    Add docstrings

--- src/core/python2js_buffer.c
+++ src/core/python2js_buffer.c
@@ -16,6 +16,11 @@
 // standard Javascript numbers (python2js_buffer_recursive)
 
 // clang-format off
+/**
+ * A simple helper function that puts the arguments into a Javascript object
+ * (for readability) and looks up the conversion function, then calls into
+ * python2js_buffer_recursive.
+ */
 EM_JS_REF(JsRef, _python2js_buffer_inner, (
   void* buf,
   Py_ssize_t itemsize,
@@ -40,6 +45,13 @@ EM_JS_REF(JsRef, _python2js_buffer_inner, (
 });
 // clang-format on
 
+/**
+ * Convert a buffer. To get the data out of the Py_buffer without relying on the
+ * exact memory layout of Py_buffer, we need to do this in C. After pulling the
+ * data out we call into the EM_JS helper _python2js_buffer_inner, which sets up
+ * the base case for the recursion and then calls the main js function
+ * _python2js_buffer_recursive (defined in python2js_buffer.js).
+ */
 JsRef
 _python2js_buffer(PyObject* x)
 {
