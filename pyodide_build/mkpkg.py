7507dcee49bc3ee5e18361a5603578ab3be5b091
Hood <hood@mit.edu>
2021-04-13 13:43:07 -0700

    Revert changes from #1465

--- pyodide_build/mkpkg.py
+++ pyodide_build/mkpkg.py
@@ -5,19 +5,13 @@ import json
 import os
 import shutil
 import urllib.request
-import urllib.error
 import sys
 from pathlib import Path
-from typing import Dict, Any, Optional
-import warnings
+from typing import Dict, Tuple, Any, Optional
 
 from .io import parse_package_config
 
 PACKAGES_ROOT = Path(__file__).parent.parent / "packages"
-
-
-class MkpkgFailedException(Exception):
-    pass
 
 
 def _extract_sdist(pypi_metadata: Dict[str, Any]) -> Dict:
@@ -33,7 +27,7 @@ def _extract_sdist(pypi_metadata: Dict[str, Any]) -> Dict:
         if entry["filename"].endswith(sdist_extensions):
             return entry
 
-    raise MkpkgFailedException(
+    raise Exception(
         "No sdist URL found for package %s (%s)"
         % (
             pypi_metadata["info"].get("name"),
@@ -42,20 +36,17 @@ def _extract_sdist(pypi_metadata: Dict[str, Any]) -> Dict:
     )
 
 
-def _get_metadata(package: str, version: Optional[str] = None) -> Dict:
+def _get_metadata(package: str, version: Optional[str] = None) -> Tuple[Dict, Dict]:
     """Download metadata for a package from PyPi"""
     version = ("/" + version) if version is not None else ""
     url = f"https://pypi.org/pypi/{package}{version}/json"
 
-    try:
-        with urllib.request.urlopen(url) as fd:
-            pypi_metadata = json.load(fd)
-    except urllib.error.HTTPError as e:
-        raise MkpkgFailedException(
-            f"Failed to load metadata for {package}{version} from https://pypi.org/pypi/{package}{version}/json: {e}"
-        )
+    with urllib.request.urlopen(url) as fd:
+        pypi_metadata = json.load(fd)
 
-    return pypi_metadata
+    sdist_metadata = _extract_sdist(pypi_metadata)
+
+    return sdist_metadata, pypi_metadata
 
 
 def make_package(package: str, version: Optional[str] = None):
@@ -65,9 +56,7 @@ def make_package(package: str, version: Optional[str] = None):
     """
     import yaml
 
-    pypi_metadata = _get_metadata(package, version)
-    sdist_metadata = _extract_sdist(pypi_metadata)
-
+    sdist_metadata, pypi_metadata = _get_metadata(package, version)
     url = sdist_metadata["url"]
     sha256 = sdist_metadata["digests"]["sha256"]
     version = pypi_metadata["info"]["version"]
@@ -84,35 +73,8 @@ def make_package(package: str, version: Optional[str] = None):
         yaml.dump(yaml_content, fd, default_flow_style=False)
 
 
-class bcolors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
-
-
-def abort(msg):
-    print(bcolors.FAIL + msg + bcolors.ENDC)
-    sys.exit(1)
-
-
-def warn(msg):
-    warnings.warn(bcolors.WARNING + msg + bcolors.ENDC)
-
-
-def success(msg):
-    print(bcolors.OKBLUE + msg + bcolors.ENDC)
-
-
-def update_package(package: str, update_patched: bool):
-    from ruamel.yaml import YAML
-
-    yaml = YAML()
+def update_package(package: str):
+    import yaml
 
     meta_path = PACKAGES_ROOT / package / "meta.yaml"
     yaml_content = parse_package_config(meta_path)
@@ -121,45 +83,37 @@ def update_package(package: str, update_patched: bool):
         print(f"Skipping: {package} is a local package!")
         sys.exit(0)
 
-    build_info = yaml_content.get("build", {})
-    if build_info.get("library", False) or build_info.get("sharedlibrary", False):
-        print(f"Skipping: {package} is a library!")
-        sys.exit(0)
+    if set(yaml_content.keys()).difference(
+        ("package", "source", "test", "requirements")
+    ):
+        print(
+            f"{package}: Only pure-python packages can be updated using this script. "
+            f"Aborting."
+        )
+        sys.exit(1)
 
-    pypi_metadata = _get_metadata(package)
+    sdist_metadata, pypi_metadata = _get_metadata(package)
     pypi_ver = pypi_metadata["info"]["version"]
     local_ver = yaml_content["package"]["version"]
     if pypi_ver <= local_ver:
         print(f"{package} already up to date. Local: {local_ver} PyPi: {pypi_ver}")
         sys.exit(0)
-
-    print(f"{package} is out of date: {local_ver} <= {pypi_ver}.")
-    if set(yaml_content.keys()).difference(
-        ("package", "source", "test", "requirements")
-    ):
-        abort(
-            f"{package}: Only pure python packages can be updated using this script. "
-            f"Aborting."
-        )
+    print(f"Updating {package} from {local_ver} to {pypi_ver}.")
 
     if "patches" in yaml_content["source"]:
-        if update_patched:
-            warn(
-                f"Pyodide applies patches to {package}. Update the "
-                "patches (if needed) to avoid build failing."
-            )
-        else:
-            abort(f"Pyodide applies patches to {package}. Skipping update.")
+        import warnings
 
-    sdist_metadata = _extract_sdist(pypi_metadata)
+        warnings.warn(
+            f"Pyodide applies patches to {package}. Update the "
+            "patches (if needed) to avoid build failing."
+        )
 
     yaml_content["source"]["url"] = sdist_metadata["url"]
     yaml_content["source"].pop("md5", None)
     yaml_content["source"]["sha256"] = sdist_metadata["digests"]["sha256"]
     yaml_content["package"]["version"] = pypi_metadata["info"]["version"]
     with open(PACKAGES_ROOT / package / "meta.yaml", "w") as fd:
-        yaml.dump(yaml_content, fd)
-    success(f"Updated {package} from {local_ver} to {pypi_ver}.")
+        yaml.dump(yaml_content, fd, default_flow_style=False)
 
 
 def make_parser(parser):
@@ -169,11 +123,6 @@ for most pure Python packages, but will have to be edited for more
 complex things.""".strip()
     parser.add_argument("package", type=str, nargs=1, help="The package name on PyPI")
     parser.add_argument("--update", action="store_true", help="Update existing package")
-    parser.add_argument(
-        "--update-if-not-patched",
-        action="store_true",
-        help="Update existing package if it has no patches",
-    )
     parser.add_argument(
         "--version",
         type=str,
@@ -185,17 +134,11 @@ complex things.""".strip()
 
 
 def main(args):
-    try:
-        package = args.package[0]
-        if args.update:
-            update_package(package, update_patched=True)
-            return
-        if args.update_if_not_patched:
-            update_package(package, update_patched=False)
-            return
-        make_package(package, args.version)
-    except MkpkgFailedException as e:
-        abort(e.args[0])
+    package = args.package[0]
+    if args.update:
+        update_package(package)
+        return
+    make_package(package, args.version)
 
 
 if __name__ == "__main__":
