98cce7de5f84ac9879aefce00be98ede054c716b
Roman Yurchak <rth.yurchak@gmail.com>
2021-05-01 21:35:03 +0200

    Lint

--- src/pyodide.js
+++ src/pyodide.js
@@ -838,7 +838,7 @@ def temp(Module):
   Module.registerJsModule("pyodide_js", pyodide);
   globalThis.pyodide = pyodide;
   if (config.fullStdLib !== false) {
-    await pyodide.loadPackage([ 'distutils' ]);
+    await pyodide.loadPackage(["distutils"]);
   }
 
   return pyodide;
