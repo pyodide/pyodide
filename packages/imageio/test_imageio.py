635d6878922957542d8f55b535ce8234b441df03
Hood <hood@mit.edu>
2021-06-15 00:03:45 -0700

    Put numpy dependent packages back in

---
+++ packages/imageio/test_imageio.py
@@ -0,0 +1,14 @@
+from pyodide_build.testing import run_in_pyodide
+
+
+@run_in_pyodide(standalone=True, packages=["numpy", "imageio"])
+def test_imageio():
+    import numpy as np
+    import imageio
+
+    filename = "/tmp/foo.tif"
+    image_in = np.random.randint(0, 65535, size=(100, 36), dtype=np.uint16)
+    imageio.imwrite(filename, image_in)
+    image_out = imageio.imread(filename)
+    assert image_out.shape == (100, 36)
+    np.testing.assert_equal(image_in, image_out)
---
+++ packages/networkx/test_networkx.py
@@ -0,0 +1,13 @@
+from pyodide_build.testing import run_in_pyodide
+
+
+@run_in_pyodide(standalone=True, packages=["networkx"])
+def test_networkx_basicgraph():
+    import networkx as nx
+
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3])
+    G.add_edges_from([(1, 2), (1, 3)])
+
+    assert G.number_of_nodes() == 3
+    assert G.number_of_edges() == 2
