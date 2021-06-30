08e92b89f38cad397bda4d1b44cea4d0c69d3d93
Hood <hood@mit.edu>
2021-06-29 18:27:03 -0700

    Fix hiwire_int_from_digits

--- src/core/hiwire.c
+++ src/core/hiwire.c
@@ -234,8 +234,7 @@ EM_JS_REF(JsRef, hiwire_int, (int val), {
 EM_JS_REF(JsRef, hiwire_int_from_digits, (const int* digits, size_t ndigits), {
   let result = BigInt(0);
   for (let i = 0; i < ndigits; i++) {
-    result <<= BigInt(32);
-    result += BigInt(DEREF_U32(digits, i));
+    result += BigInt(DEREF_U32(digits, i)) << BigInt(32 * i);
   }
   if (-Number.MAX_SAFE_INTEGER < result && result < Number.MAX_SAFE_INTEGER) {
     result = Number(result);
