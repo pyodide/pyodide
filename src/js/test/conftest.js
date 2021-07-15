5e1d175a4afb51512f5f9e09bfd6896760bf4f93
Roman Yurchak <rth.yurchak@gmail.com>
2021-07-15 13:38:18 +0200

    Repeat timeout value in conftest.js

--- src/js/test/conftest.js
+++ src/js/test/conftest.js
@@ -2,6 +2,8 @@ import { loadPyodide } from "../pyodide.js";
 import path from "path";
 globalThis.path = path;
 
-before(async () => {
+// arrow functions don't bind their own `this`, so we use a regular function.
+before(async function () {
+  this.timeout(20000);
   globalThis.pyodide = await loadPyodide({ indexURL: "../../build/" });
 });
