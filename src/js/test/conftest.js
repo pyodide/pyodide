3bd30c363cb70189b86d73a252fbd16984949b89
Roman Yurchak <rth.yurchak@gmail.com>
2021-07-15 14:01:30 +0200

    Add mocha config to package.json

--- src/js/test/conftest.js
+++ src/js/test/conftest.js
@@ -2,8 +2,6 @@ import { loadPyodide } from "../pyodide.js";
 import path from "path";
 globalThis.path = path;
 
-// arrow functions don't bind their own `this`, so we use a regular function.
-before(async function () {
-  this.timeout(20000);
+before(async () => {
   globalThis.pyodide = await loadPyodide({ indexURL: "../../build/" });
 });
