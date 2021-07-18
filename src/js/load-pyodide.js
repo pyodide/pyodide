352676a5d7b38408bc42deba5216032be9b26bf9
Hood <hood@mit.edu>
2021-07-18 08:12:00 -0700

    Fix mocha tests

--- src/js/load-pyodide.js
+++ src/js/load-pyodide.js
@@ -55,13 +55,11 @@ if (globalThis.document) {
   const pathPromise = import("path").then((M) => M.default);
   const fetchPromise = import("node-fetch").then((M) => M.default);
   const vmPromise = import("vm").then((M) => M.default);
-  require("path");
   loadScript = async (url) => {
     if (url.includes("://")) {
       // If it's a url, have to load it with fetch and then eval it.
       const fetch = await fetchPromise;
       const vm = await vmPromise;
-      globalThis.require = require;
       vm.runInThisContext(await (await fetch(url)).text());
     } else {
       // Otherwise, hopefully it is a relative path we can load from the file
