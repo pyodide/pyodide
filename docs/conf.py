ac61f71542c97fd3a6a8ae83aa6939c8e5adc252
Hood <hood@mit.edu>
2021-10-09 08:09:25 -0700

    Fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ import micropip  # noqa
 # We hacked it so that autodoc will look for submodules, but only if we import
 # them here. TODO: look these up in the source directory?
 import pyodide.console
-import pyodide.utils
+import pyodide.http
 import pyodide.webloop
 
 # The full version, including alpha/beta/rc tags.
