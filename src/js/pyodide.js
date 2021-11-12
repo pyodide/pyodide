1a135964f5b0e3a881b3893f7d39e9ebadabb681
Hood <hood@mit.edu>
2021-11-12 11:13:11 -0800

    Avoid catching fatal errors in EM_JS wrappers

--- src/js/pyodide.js
+++ src/js/pyodide.js
@@ -57,6 +57,8 @@ Module.fatal_error = function (e) {
     console.error(e);
     return;
   }
+  // Mark e so we know not to handle it later in EM_JS wrappers
+  e.pyodide_fatal_error = true;
   fatal_error_occurred = true;
   console.error(
     "Pyodide has suffered a fatal error. Please report this to the Pyodide maintainers."
