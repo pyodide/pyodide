13a818de4be42ede5184290786d8d80e425d14ac
DerThorsten <derthorstenbeier@gmail.com>
2021-11-19 11:23:39 +0100

    changed package

--- packages/pyb2d/test_pyb2d.py
+++ packages/pyb2d/test_pyb2d.py
@@ -2,16 +2,17 @@ import pytest
 
 
 @pytest.mark.driver_timeout(40)
-def test_pybox2d(selenium_standalone, request):
+def test_pyb2d(selenium_standalone, request):
     selenium = selenium_standalone
-    selenium.load_package("pybox2d")
+    selenium.load_package("pyb2d")
     assert (
         selenium.run(
             """
         import numpy as np
-        import pybox2d
-        w = pybox2d.world(gravity=(0,-10))
+        import b2d
+        w = b2d.world(gravity=(0,-10))
         """
         )
         > 0
     )
+    pass
