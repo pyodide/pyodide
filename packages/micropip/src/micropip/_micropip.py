3d46df1d07251ce3a7cc72ee3a027832e30b48aa
ryanking13 <def6488@gmail.com>
2021-12-02 16:46:45 +0900

    Remove __all__ from _micropip.py

--- packages/micropip/src/micropip/_micropip.py
+++ packages/micropip/src/micropip/_micropip.py
@@ -403,9 +403,5 @@ def _list():
     return PACKAGE_MANAGER.list()
 
 
-# prevent sphinx autodoc from generating duplicated docs
-__all__: List[str] = []
-
-
 if __name__ == "__main__":
     install("snowballstemmer")
