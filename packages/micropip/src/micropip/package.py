78a054b2bf05ccc1d08417f8cdfaece62ad22a11
ryanking13 <def6488@gmail.com>
2021-12-03 11:55:11 +0900

    Fix doctest

--- packages/micropip/src/micropip/package.py
+++ packages/micropip/src/micropip/package.py
@@ -13,8 +13,8 @@ def _format_table(headers: List[str], table: List[Iterable]) -> str:
     >>> print(_format_table(["Header1", "Header2"], [["val1", "val2"], ["val3", "val4"]]))
     Header1 | Header2
     ------- | -------
-    val1    | val2
-    val3    | val4
+    val1    | val2   
+    val3    | val4   
     """
 
     def format_row(values, widths, filler=""):
