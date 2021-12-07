73b0d876677cc5393df000e62f13b427c0f90a68
Hood Chatham <roberthoodchatham@gmail.com>
2021-12-07 07:34:01 -0800

    More specificl xfail message for cffi_example

--- packages/cffi_example/test_cffi_example.py
+++ packages/cffi_example/test_cffi_example.py
@@ -2,6 +2,13 @@ import pytest
 from pyodide_build.testing import run_in_pyodide
 
 
+CHROME_FAIL_v90_MSG = (
+    "Doesn't work in chrome v89 or v90, I think because of "
+    "https://bugs.chromium.org/p/chromium/issues/detail?id=1200031. "
+    "Confirmed locally to work in v91 and v96, and to break on v90."
+)
+
+
 @pytest.mark.parametrize(
     "pattern,name,flags,expected",
     [
@@ -14,7 +21,7 @@ from pyodide_build.testing import run_in_pyodide
 def test_fnmatch(selenium_module_scope, pattern, name, flags, expected):
     selenium = selenium_module_scope
     if selenium.browser == "chrome":
-        pytest.xfail("Doesn't work in chrome v90")
+        pytest.xfail(CHROME_FAIL_v90_MSG)
     selenium.load_package("cffi_example")
     result = selenium.run(
         f"""
@@ -28,7 +35,7 @@ def test_fnmatch(selenium_module_scope, pattern, name, flags, expected):
 @run_in_pyodide(
     packages=["cffi_example"],
     module_scope=True,
-    xfail_browsers={"chrome": "Doesn't work in chrome v90"},
+    xfail_browsers={"chrome": CHROME_FAIL_v90_MSG},
 )
 def test_person():
     from cffi_example.person import Person
