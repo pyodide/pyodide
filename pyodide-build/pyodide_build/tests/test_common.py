51ca1470a81bdbd83d8f50b534bb1efa1f65835e
Hood Chatham <roberthoodchatham@gmail.com>
2021-12-14 18:15:23 -0800

    Fix test_parse_package_subset (#2043)

--- pyodide-build/pyodide_build/tests/test_common.py
+++ pyodide-build/pyodide_build/tests/test_common.py
@@ -43,6 +43,7 @@ def test_parse_package_subset():
         "micropip",
         "regex",
         "fpcast-test",
+        "sharedlib-test-py",
     }
     # by default core packages are built
     assert _parse_package_subset(None) == _parse_package_subset("core")
@@ -62,6 +63,7 @@ def test_parse_package_subset():
         "scikit-learn",
         "joblib",
         "pytest",
+        "sharedlib-test-py",
     }
     # reserved key words can be combined with other packages
     assert _parse_package_subset("core, unknown") == _parse_package_subset("core") | {
