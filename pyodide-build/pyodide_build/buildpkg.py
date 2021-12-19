5d2f36fc343bf4656c9053f51c83bd4aa3d84341
Hood Chatham <roberthoodchatham@gmail.com>
2021-12-19 11:45:44 -0800

    No SITEPACKAGES environment variable

--- pyodide-build/pyodide_build/buildpkg.py
+++ pyodide-build/pyodide_build/buildpkg.py
@@ -82,7 +82,7 @@ def get_bash_runner():
             "PYTHONPATH": os.environ["PYTHONPATH"],
             "PYODIDE_ROOT": os.environ["PYODIDE_ROOT"],
             "PYTHONINCLUDE": os.environ["PYTHONINCLUDE"],
-            "SITEPACKAGES": os.environ["SITEPACKAGES"],
+            # "SITEPACKAGES": os.environ["SITEPACKAGES"],
             "PYODIDE_JOBS": os.environ["PYODIDE_JOBS"],
             "NUMPY_LIB": os.environ["NUMPY_LIB"],
             "PKGDIR": os.environ["PKGDIR"],
