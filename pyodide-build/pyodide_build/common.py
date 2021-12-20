a5f7fb8908fb3d57028c955cfbcb729eac00b92c
Hood Chatham <roberthoodchatham@gmail.com>
2021-12-20 01:46:56 -0800

    Fix typo

--- pyodide-build/pyodide_build/common.py
+++ pyodide-build/pyodide_build/common.py
@@ -105,7 +105,7 @@ def invoke_file_packager(
                 "npx",
                 "--no-install",
                 "terser",
-                root_dir / f"{name}js",
+                root_dir / f"{name}.js",
                 "-o",
                 root_dir / f"{name}.js",
             ],
