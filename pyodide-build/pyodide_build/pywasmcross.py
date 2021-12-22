5be98502ff8dfce8d7ea0161d99a728ad21b2218
Hood Chatham <roberthoodchatham@gmail.com>
2021-12-22 12:50:47 -0800

    Fix pillow build

--- pyodide-build/pyodide_build/pywasmcross.py
+++ pyodide-build/pyodide_build/pywasmcross.py
@@ -143,6 +143,7 @@ def capture_make_command_wrapper_symlinks(env: Dict[str, str]):
 
 def capture_compile(*, host_install_dir: str, skip_host: bool, env: Dict[str, str]):
     TOOLSDIR = Path(common.get_make_flag("TOOLSDIR"))
+    env['PYODIDE'] = '1'
     env["PATH"] = str(TOOLSDIR) + ":" + os.environ["PATH"]
     capture_make_command_wrapper_symlinks(env)
 
