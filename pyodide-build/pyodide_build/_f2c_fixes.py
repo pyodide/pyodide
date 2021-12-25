e2df663deff13e5435e51402a14a0e158197b37b
Hood Chatham <roberthoodchatham@gmail.com>
2021-12-25 12:04:11 -0800

    Use a Python script to automate f2cpatches

--- pyodide-build/pyodide_build/_f2c_fixes.py
+++ pyodide-build/pyodide_build/_f2c_fixes.py
@@ -1,6 +1,6 @@
 import re
-from pathlib import Path
 import subprocess
+from typing import List, Iterable, Iterator
 
 
 def fix_f2c_clapack_calls(f2c_output_name: str):
@@ -21,45 +21,45 @@ def fix_f2c_clapack_calls(f2c_output_name: str):
     # fmt: off
     lapack_czds_names = [
         'gbmv', 'gemm', 'gemv', 'symm', 'syr2k', 'syrk', 'tbmv', 'tbsv', 'tpmv',
-        'tpsv', 'trmm', 'trmv', 'trsm', 'trsv', 'spmv', 'spr', 'symv', 'syr', 
+        'tpsv', 'trmm', 'trmv', 'trsm', 'trsv', 'spmv', 'spr', 'symv', 'syr',
         'langb', 'lange', 'langt', 'lanhs', 'lansb', 'lansp', 'lansy', 'lantb',
-        'lantp', 'lantr', 'bdsqr', 'gbbrd', 'gbcon', 'gbrfs', 'gbsvx', 'gbtrs', 
-        'gebak', 'gebal', 'gecon', 'gees', 'geesx', 'geev', 'geevx', 'gels', 
-        'gerfs', 'gesdd', 'gesvd', 'gesvx', 'getrs', 'ggbak', 'ggbal', 'gges', 
-        'ggesx', 'ggev', 'ggevx', 'gghrd', 'gtcon', 'gtrfs', 'gtsvx', 'gttrs', 
-        'hgeqz', 'hsein', 'hseqr', 'lacpy', 'lagtm', 'lalsd', 'laqgb', 'laqge', 
-        'laqsb', 'laqsp', 'laqsy', 'larf', 'larfb', 'larft', 'larfx', 'larz', 
-        'larzb', 'larzt', 'lascl', 'laset', 'lasr', 'lasyf', 'latbs', 'latps', 
-        'latrd', 'latrs', 'lauu2', 'lauum', 'pbcon', 'pbequ', 'pbrfs', 'pbstf', 
-        'pbsv', 'pbsvx', 'pbtf2', 'pbtrf', 'pbtrs', 'pocon', 'porfs', 'posv', 
-        'posvx', 'potf2', 'potrf', 'potri', 'potrs', 'ppcon', 'ppequ', 'pprfs', 
-        'ppsv', 'ppsvx', 'pptrf', 'pptri', 'pptrs', 'pteqr', 'ptsvx', 'spcon', 
-        'sprfs', 'spsv', 'spsvx', 'sptrf', 'sptri', 'sptrs', 'stedc', 'stegr', 
-        'stemr', 'steqr', 'sycon', 'syrfs', 'sysv', 'sysvx', 'sytf2', 'sytrf', 
-        'sytri', 'sytrs', 'tbcon', 'tbrfs', 'tbtrs', 'tgevc', 'tgsja', 'tgsna', 
-        'tgsy2', 'tgsyl', 'tpcon', 'tprfs', 'tptri', 'tptrs', 'trcon', 'trevc', 
+        'lantp', 'lantr', 'bdsqr', 'gbbrd', 'gbcon', 'gbrfs', 'gbsvx', 'gbtrs',
+        'gebak', 'gebal', 'gecon', 'gees', 'geesx', 'geev', 'geevx', 'gels',
+        'gerfs', 'gesdd', 'gesvd', 'gesvx', 'getrs', 'ggbak', 'ggbal', 'gges',
+        'ggesx', 'ggev', 'ggevx', 'gghrd', 'gtcon', 'gtrfs', 'gtsvx', 'gttrs',
+        'hgeqz', 'hsein', 'hseqr', 'lacpy', 'lagtm', 'lalsd', 'laqgb', 'laqge',
+        'laqsb', 'laqsp', 'laqsy', 'larf', 'larfb', 'larft', 'larfx', 'larz',
+        'larzb', 'larzt', 'lascl', 'laset', 'lasr', 'lasyf', 'latbs', 'latps',
+        'latrd', 'latrs', 'lauu2', 'lauum', 'pbcon', 'pbequ', 'pbrfs', 'pbstf',
+        'pbsv', 'pbsvx', 'pbtf2', 'pbtrf', 'pbtrs', 'pocon', 'porfs', 'posv',
+        'posvx', 'potf2', 'potrf', 'potri', 'potrs', 'ppcon', 'ppequ', 'pprfs',
+        'ppsv', 'ppsvx', 'pptrf', 'pptri', 'pptrs', 'pteqr', 'ptsvx', 'spcon',
+        'sprfs', 'spsv', 'spsvx', 'sptrf', 'sptri', 'sptrs', 'stedc', 'stegr',
+        'stemr', 'steqr', 'sycon', 'syrfs', 'sysv', 'sysvx', 'sytf2', 'sytrf',
+        'sytri', 'sytrs', 'tbcon', 'tbrfs', 'tbtrs', 'tgevc', 'tgsja', 'tgsna',
+        'tgsy2', 'tgsyl', 'tpcon', 'tprfs', 'tptri', 'tptrs', 'trcon', 'trevc',
         'trexc', 'trrfs', 'trsen', 'trsna', 'trsyl', 'trti2', 'trtri', 'trtrs',
     ]
     lapack_ds_names = [
-        'sbmv', 'spr2', 'syr2', 'lamch', 'lanst', 'bdsdc', 'disna', 'larrc', 
-        'larrd', 'larre', 'lasdq', 'lasrt', 'opgtr', 'opmtr', 'orgbr', 'orgtr', 
-        'orm2l', 'orm2r', 'ormbr', 'ormhr', 'orml2', 'ormlq', 'ormql', 'ormqr', 
-        'ormr2', 'ormr3', 'ormrq', 'ormrz', 'ormtr', 'sbev', 'sbevd', 'sbevx', 
-        'sbgst', 'sbgv', 'sbgvd', 'sbgvx', 'sbtrd', 'spev', 'spevd', 'spevx', 
-        'spgst', 'spgv', 'spgvd', 'spgvx', 'sptrd', 'stebz', 'stev', 'stevd', 
-        'stevr', 'stevx', 'syev', 'syevd', 'syevr', 'syevx', 'sygs2', 'sygst', 
+        'sbmv', 'spr2', 'syr2', 'lamch', 'lanst', 'bdsdc', 'disna', 'larrc',
+        'larrd', 'larre', 'lasdq', 'lasrt', 'opgtr', 'opmtr', 'orgbr', 'orgtr',
+        'orm2l', 'orm2r', 'ormbr', 'ormhr', 'orml2', 'ormlq', 'ormql', 'ormqr',
+        'ormr2', 'ormr3', 'ormrq', 'ormrz', 'ormtr', 'sbev', 'sbevd', 'sbevx',
+        'sbgst', 'sbgv', 'sbgvd', 'sbgvx', 'sbtrd', 'spev', 'spevd', 'spevx',
+        'spgst', 'spgv', 'spgvd', 'spgvx', 'sptrd', 'stebz', 'stev', 'stevd',
+        'stevr', 'stevx', 'syev', 'syevd', 'syevr', 'syevx', 'sygs2', 'sygst',
         'sygv', 'sygvd', 'sygvx', 'sytd2', 'sytrd'
     ]
     lapack_cz_names = [
-        'hbmv', 'hemm', 'hemv', 'her', 'her2', 'her2k', 'herk', 'hpmv', 'hpr', 
-        'hpr2', 'lanhb', 'lanhe', 'lanhp', 'lanht', 'hbev', 'hbevd', 'hbevx', 
-        'hbgst', 'hbgv', 'hbgvd', 'hbgvx', 'hbtrd', 'hecon', 'heev', 'heevd', 
-        'heevr', 'heevx', 'hegs2', 'hegst', 'hegv', 'hegvd', 'hegvx', 'herfs', 
-        'hesv', 'hesvx', 'hetd2', 'hetf2', 'hetrd', 'hetrf', 'hetri', 'hetrs', 
-        'hpcon', 'hpev', 'hpevd', 'hpevx', 'hpgst', 'hpgv', 'hpgvd', 'hpgvx', 
-        'hprfs', 'hpsv', 'hpsvx', 'hptrd', 'hptrf', 'hptri', 'hptrs', 'lacp2', 
-        'lahef', 'laqhb', 'laqhe', 'laqhp', 'ptrfs', 'pttrs', 'ungbr', 'ungtr', 
-        'unm2l', 'unm2r', 'unmbr', 'unmhr', 'unml2', 'unmlq', 'unmql', 'unmqr', 
+        'hbmv', 'hemm', 'hemv', 'her', 'her2', 'her2k', 'herk', 'hpmv', 'hpr',
+        'hpr2', 'lanhb', 'lanhe', 'lanhp', 'lanht', 'hbev', 'hbevd', 'hbevx',
+        'hbgst', 'hbgv', 'hbgvd', 'hbgvx', 'hbtrd', 'hecon', 'heev', 'heevd',
+        'heevr', 'heevx', 'hegs2', 'hegst', 'hegv', 'hegvd', 'hegvx', 'herfs',
+        'hesv', 'hesvx', 'hetd2', 'hetf2', 'hetrd', 'hetrf', 'hetri', 'hetrs',
+        'hpcon', 'hpev', 'hpevd', 'hpevx', 'hpgst', 'hpgv', 'hpgvd', 'hpgvx',
+        'hprfs', 'hpsv', 'hpsvx', 'hptrd', 'hptrf', 'hptri', 'hptrs', 'lacp2',
+        'lahef', 'laqhb', 'laqhe', 'laqhp', 'ptrfs', 'pttrs', 'ungbr', 'ungtr',
+        'unm2l', 'unm2r', 'unmbr', 'unmhr', 'unml2', 'unmlq', 'unmql', 'unmqr',
         'unmr2', 'unmr3', 'unmrq', 'unmrz', 'unmtr', 'upgtr', 'upmtr',
     ]
     lapack_other_names = ['lsame_', 'ilaenv_']
@@ -71,71 +71,108 @@ def fix_f2c_clapack_calls(f2c_output_name: str):
         lapack_names.extend(f"{l}{name}_" for l in ["c", "z"])
     for name in lapack_ds_names:
         lapack_names.extend(f"{l}{name}_" for l in ["d", "s"])
-    patch_output(f2c_output_name)
+
+    if f2c_output_name.endswith("wrap_dummy_g77_abi.c"):
+        subprocess.run(
+            [
+                "patch",
+                str(f2c_output_name),
+                "../../patches/wrap_dummy_g77_abi.patch",
+            ]
+        )
 
     with open(f2c_output_name, "r") as f:
-        code = f.read()
+        lines = f.readlines()
+    if "id_dist" in f2c_output_name:
+        lines = fix_inconsistent_decls(lines)
 
+    if any(
+        f2c_output_name.endswith(x)
+        for x in ["wrap_dummy_g77_abi.c", "_lapack_subroutine_wrappers.c"]
+    ):
+        lines = fix_lapack_subroutine_wrappers(lines)
+
+    code = "".join(lines)
     for cur_name in lapack_names:
         code = re.sub(rf"\b{cur_name}\b", "w" + cur_name, code)
-    if f2c_output_name.endswith(
-        "_lapack_subroutine_wrappers.c"
-    ) or f2c_output_name.endswith("wrap_dummy_g77_abi.c"):
-        code = fix_lapack_subroutine_wrappers(code)
+
     with open(f2c_output_name, "w") as f:
         f.write(code)
 
 
-def patch_output(f2c_output_name: str):
-    if f2c_output_name:
-        c_file_name = Path(f2c_output_name).name
-        patch_file = (Path("../../f2cpatches/") / c_file_name).with_suffix(".patch")
-        if patch_file.exists():
-            subprocess.run(
-                [
-                    "patch",
-                    str(f2c_output_name),
-                    str(patch_file),
-                ]
-            )
-
-
-def fix_lapack_subroutine_wrappers(code: str) -> str:
-    lines = code.split("\n")
+def fix_lapack_subroutine_wrappers(lines: List[str]) -> List[str]:
     new_lines = []
-    in_subroutine = False
-    sub_lines = []
-    for line in lines:
+    for line in regroup_lines(lines):
         if line.startswith("/* Subroutine */"):
-            in_subroutine = True
-        if in_subroutine:
+            line = re.sub(",\s*ftnlen [a-z]*_len", "", line)
+        new_lines.append(line)
+    return new_lines
+
+
+def regroup_lines(lines: Iterable[str]) -> Iterator[str]:
+    line_iter = iter(lines)
+    for line in line_iter:
+        if not "/* Subroutine */" in line:
+            yield line
+            continue
+
+        is_definition = line.startswith("/* Subroutine */")
+        stop = ")" if is_definition else ";"
+
+        sub_lines = [line.strip()]
+        for line in line_iter:
             sub_lines.append(line.strip())
-            if ")" in line:
-                res_line = " ".join(sub_lines)
-                res_line = re.sub(",\s*ftnlen [a-z]*_len", "", res_line)
-                new_lines.append(res_line)
-                sub_lines = []
-                in_subroutine = False
+            if stop in line:
+                break
+        joined_line = " ".join(sub_lines)
+        if is_definition:
+            yield joined_line
         else:
-            new_lines.append(line)
-    return "\n".join(new_lines)
+            yield from (x + ";" for x in joined_line.split(";")[:-1])
+
+
+def fix_inconsistent_decls(lines: List[str]) -> List[str]:
+    func_types = {}
+    lines = list(regroup_lines(lines))
+    for line in lines:
+        if not line.startswith("/* Subroutine */"):
+            continue
+        [func_name, types] = get_subroutine_decl(line)
+        func_types[func_name] = types
+
+    for idx, line in enumerate(lines):
+        if not "extern /* Subroutine */" in line:
+            continue
+        decls = line.split(")")[:-1]
+        for decl in decls:
+            [func_name, types] = get_subroutine_decl(decl)
+            if func_name not in func_types or types == func_types[func_name]:
+                continue
+            types = func_types[func_name]
+            l = list(line.partition(func_name + "("))
+            l[2:] = list(l[2].partition(")"))
+            l[2] = ", ".join(types)
+            line = "".join(l)
+        lines[idx] = line
+    return lines
+
+
+def get_subroutine_decl(sub: str):
+    func_name = sub.partition("(")[0].rpartition(" ")[2]
+    args_str = sub.partition("(")[2].partition(")")[0]
+    args = args_str.split(",")
+    types = []
+    for arg in args:
+        arg = arg.strip()
+        if "*" in arg:
+            type = "".join(arg.partition("*")[:-1])
+        else:
+            type = arg.partition(" ")[0]
+        types.append(type.strip())
+    return [func_name, types]
 
 
 if __name__ == "__main__":
     fix_f2c_clapack_calls(
         "/home/hood/pyodide/packages/scipy/f2cfixes/_lapack_subroutine_wrappers.c"
     )
-
-    # c_file_name = Path(f2c_output_name).name
-    # patch = (Path("../../f2cpatches/") / c_file_name).with_suffix(".patch")
-    # replacement_c_file = (Path("../../f2cpatches/") / c_file_name).resolve()
-    # if replacement_c_file.exists():
-    #     subprocess.run([
-    #         "git",
-    #         "diff",
-    #         "--no-index",
-    #         "--",
-    #         f2c_output_name,
-    #         str(replacement_c_file),
-    #     ], stdout=open(patch, "w"))
-    #     shutil.copy(replacement_c_file, f2c_output_name)
