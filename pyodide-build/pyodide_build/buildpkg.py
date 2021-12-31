29ee3c4f6f88103e906c3f2128cd2afaa98b2ff4
Hood Chatham <roberthoodchatham@gmail.com>
2021-12-31 10:55:46 -0800

    Copy dist directory up

--- pyodide-build/pyodide_build/buildpkg.py
+++ pyodide-build/pyodide_build/buildpkg.py
@@ -632,6 +632,8 @@ def build_package(
             should_replay_compile=should_replay_compile,
             replay_from=replay_from,
         )
+        shutil.rmtree(srcpath/"dist", ignore_errors=True)
+        shutil.copytree(srcpath/"dist", pkg_root / "dist")
 
         create_packaged_token(build_dir)
 
