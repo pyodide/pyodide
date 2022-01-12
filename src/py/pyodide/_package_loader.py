86fd73ee54160de547ea06202ec13da54bd54fff
Hood Chatham <roberthoodchatham@gmail.com>
2022-01-12 09:30:10 -0800

    Apply formatter

--- src/py/pyodide/_package_loader.py
+++ src/py/pyodide/_package_loader.py
@@ -17,7 +17,7 @@ ZIP_TYPES = {".whl", ".zip"}
 TAR_TYPES = {".tar", ".gz", ".bz", ".gz", ".tgz", ".bz2", ".tbz2"}
 
 
-def unpack_buffer(filename : str, buffer : JsProxy, target : str = "site") -> JsProxy:
+def unpack_buffer(filename: str, buffer: JsProxy, target: str = "site") -> JsProxy:
     """
     Used to install a package either into sitepackages or into the standard library.
     This is a helper method called from ``loadPackage``.
@@ -38,7 +38,7 @@ def unpack_buffer(filename : str, buffer : JsProxy, target : str = "site") -> Js
 
     Returns
     -------
-        A Javascript Array of paths to dynamic libraries ('.so' files) that were in 
+        A Javascript Array of paths to dynamic libraries ('.so' files) that were in
         the archive. We need to precompile these Wasm binaries in `load-pyodide.js`.
         These paths point to the unpacked locations of the .so files.
     """
@@ -49,7 +49,7 @@ def unpack_buffer(filename : str, buffer : JsProxy, target : str = "site") -> Js
         return to_js(get_dynlibs(f, target_dir))
 
 
-def get_dynlibs(archive : IO[bytes], target_dir : Path) -> List[str]:
+def get_dynlibs(archive: IO[bytes], target_dir: Path) -> List[str]:
     """
     List out the paths to .so files in a zip or tar archive.
 
@@ -58,18 +58,18 @@ def get_dynlibs(archive : IO[bytes], target_dir : Path) -> List[str]:
     archive
         A binary representation of either a zip or a tar archive. We use the `.name`
         field to determine which file type.
-    
+
     target_dir
-        The directory the archive is unpacked into. Paths will be adjusted to point 
+        The directory the archive is unpacked into. Paths will be adjusted to point
         inside this directory.
 
     Returns
     -------
-        The list of paths to dynamic libraries ('.so' files) that were in the archive, 
+        The list of paths to dynamic libraries ('.so' files) that were in the archive,
         but adjusted to point to their unpacked locations.
     """
     suffix = Path(archive.name).suffix
-    dynlib_paths_iter : Iterable[str]
+    dynlib_paths_iter: Iterable[str]
     if suffix in ZIP_TYPES:
         dynlib_paths_iter = ZipFile(archive).namelist()
     elif suffix in TAR_TYPES:
@@ -79,5 +79,6 @@ def get_dynlibs(archive : IO[bytes], target_dir : Path) -> List[str]:
 
     return [
         str((target_dir / path).resolve())
-        for path in dynlib_paths_iter if path.endswith(".so")
+        for path in dynlib_paths_iter
+        if path.endswith(".so")
     ]
