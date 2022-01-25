9b5b6396ac5480a04e46bfe34fbab3c200ed3fa9
Hood Chatham <roberthoodchatham@gmail.com>
2022-01-25 08:43:39 -0800

    Use typescript (split from #2130, doesn't fix docs) (#2133)

--- src/js/compat.ts
+++ src/js/compat.ts
@@ -1,4 +1,6 @@
 // Detect if we're in node
+declare var process: any;
+
 export const IN_NODE =
   typeof process !== "undefined" &&
   process.release &&
@@ -6,10 +8,16 @@ export const IN_NODE =
   typeof process.browser ===
     "undefined"; /* This last condition checks if we run the browser shim of process */
 
-export let nodePathMod;
-export let nodeFetch;
-export let nodeFsPromisesMod;
-export let nodeVmMod;
+let nodePathMod: any;
+let nodeFetch: any;
+let nodeVmMod: any;
+/** @private */
+export let nodeFsPromisesMod: any;
+
+declare var globalThis: {
+  importScripts: (url: string) => void;
+  document?: any;
+};
 
 /**
  * If we're in node, it's most convenient to import various node modules on
@@ -20,21 +28,27 @@ export async function initNodeModules() {
   if (!IN_NODE) {
     return;
   }
+  // @ts-ignore
   nodePathMod = (await import(/* webpackIgnore: true */ "path")).default;
   nodeFsPromisesMod = await import(/* webpackIgnore: true */ "fs/promises");
+  // @ts-ignore
   nodeFetch = (await import(/* webpackIgnore: true */ "node-fetch")).default;
+  // @ts-ignore
   nodeVmMod = (await import(/* webpackIgnore: true */ "vm")).default;
 }
 
 /**
  * Load a binary file, only for use in Node. If the path explicitly is a URL,
  * then fetch from a URL, else load from the file system.
- * @param {str} indexURL base path to resolve relative paths
- * @param {str} path the path to load
+ * @param indexURL base path to resolve relative paths
+ * @param path the path to load
  * @returns An ArrayBuffer containing the binary data
  * @private
  */
-async function node_loadBinaryFile(indexURL, path) {
+async function node_loadBinaryFile(
+  indexURL: string,
+  path: string
+): Promise<Uint8Array> {
   if (path.includes("://")) {
     let response = await nodeFetch(path);
     if (!response.ok) {
@@ -51,14 +65,19 @@ async function node_loadBinaryFile(indexURL, path) {
  * Load a binary file, only for use in browser. Resolves relative paths against
  * indexURL.
  *
- * @param {str} indexURL base path to resolve relative paths
- * @param {str} path the path to load
- * @returns An ArrayBuffer containing the binary data
+ * @param indexURL base path to resolve relative paths
+ * @param path the path to load
+ * @returns A Uint8Array containing the binary data
  * @private
  */
-async function browser_loadBinaryFile(indexURL, path) {
+async function browser_loadBinaryFile(
+  indexURL: string,
+  path: string
+): Promise<Uint8Array> {
+  // @ts-ignore
   const base = new URL(indexURL, location);
   const url = new URL(path, base);
+  // @ts-ignore
   let response = await fetch(url);
   if (!response.ok) {
     throw new Error(`Failed to load '${url}': request failed.`);
@@ -66,7 +85,11 @@ async function browser_loadBinaryFile(indexURL, path) {
   return new Uint8Array(await response.arrayBuffer());
 }
 
-export let _loadBinaryFile;
+/** @private */
+export let _loadBinaryFile: (
+  indexURL: string,
+  path: string
+) => Promise<Uint8Array>;
 if (IN_NODE) {
   _loadBinaryFile = node_loadBinaryFile;
 } else {
@@ -75,11 +98,11 @@ if (IN_NODE) {
 
 /**
  * Currently loadScript is only used once to load `pyodide.asm.js`.
- * @param {string) url
+ * @param url
  * @async
  * @private
  */
-export let loadScript;
+export let loadScript: (url: string) => Promise<void>;
 
 if (globalThis.document) {
   // browser
@@ -98,10 +121,10 @@ if (globalThis.document) {
 
 /**
  * Load a text file and executes it as Javascript
- * @param {str} url The path to load. May be a url or a relative file system path.
+ * @param url The path to load. May be a url or a relative file system path.
  * @private
  */
-async function nodeLoadScript(url) {
+async function nodeLoadScript(url: string) {
   if (url.includes("://")) {
     // If it's a url, load it with fetch then eval it.
     nodeVmMod.runInThisContext(await (await nodeFetch(url)).text());
