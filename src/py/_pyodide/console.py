42cd4a49b7ea90d66c1787c825942bb8b9f0dd33
Luka Mamukashvili <ultraluka0@gmail.com>
2022-01-30 21:40:21 +0400

    MAINT Refactor *console.py* (#2140)

--- src/py/_pyodide/console.py
+++ src/py/_pyodide/console.py
@@ -10,7 +10,7 @@ from contextlib import (
 )
 from contextlib import _RedirectStream  # type: ignore
 import rlcompleter
-import platform
+from platform import python_version, python_build
 import sys
 from tokenize import TokenError
 import traceback
@@ -29,18 +29,10 @@ from _pyodide._base import should_quiet, CodeRunner
 
 __all__ = ["repr_shorten", "BANNER", "Console", "PyodideConsole", "ConsoleFuture"]
 
-
-def _banner():
-    """A banner similar to the one printed by the real Python interpreter."""
-    # copied from https://github.com/python/cpython/blob/799f8489d418b7f9207d333eac38214931bd7dcc/Lib/code.py#L214
-    cprt = 'Type "help", "copyright", "credits" or "license" for more information.'
-    version = platform.python_version()
-    build = f"({', '.join(platform.python_build())})"
-    return f"Python {version} {build} on WebAssembly VM\n{cprt}"
-
-
-BANNER = _banner()
-del _banner
+BANNER = f""" 
+Python {python_version()} ({', '.join(python_build())}) on WebAssembly VM
+Type "help", "copyright", "credits" or "license" for more information.
+""".strip()
 
 
 class redirect_stdin(_RedirectStream):
