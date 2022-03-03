a3ef372b64a0a51ea8076acea5e74fd3f2bfb147
Hood Chatham <roberthoodchatham@gmail.com>
2022-03-02 17:09:58 -0800

    Refactor core test suite
    The goal of this PR is to run more tests in the core test suite.
    In order to do this, I converted python_tests.txt into a yaml file
    so that we can have slightly more control over what goes on. The
    original xfail behavior we now get with the xfail key, but we can
    also skip specific test patterns with a skip key.
    
    I then went through and fairly carefully skipped as few tests was as
    reasonable. I made .so files for _test_sqlite3 and _test_ctypes so that
    they can be tested as well.

--- src/tests/test_core_python.py
+++ src/tests/test_core_python.py
@@ -1,39 +1,58 @@
+from functools import cache
 from pathlib import Path
+from typing import Any
 
 import pytest
+import yaml
+from yaml import CLoader as Loader
 
 from pyodide_build.common import UNVENDORED_STDLIB_MODULES
 
 
-def test_cpython_core(python_test, selenium, request):
-
-    name, error_flags = python_test
-
+def filter_info(info: dict[str, Any], browser: str) -> dict[str, Any]:
+    info = dict(info)
     # keep only flags related to the current browser
     flags_to_remove = ["firefox", "chrome", "node"]
-    flags_to_remove.remove(selenium.browser)
-    for flag in flags_to_remove:
-        if "crash-" + flag in error_flags:
-            error_flags.remove("crash-" + flag)
+    flags_to_remove.remove(browser)
+    for browser in flags_to_remove:
+        for key in list(info.keys()):
+            if key.endswith(browser):
+                del info[key]
+    return info
 
-    if any(flag.startswith("segfault") for flag in error_flags):
-        pytest.skip('known segfault with code: "{}"'.format(",".join(error_flags)))
 
-    if error_flags:
+def possibly_skip_test(request, info: dict[str, Any]) -> dict[str, Any]:
+    for reason in (
+        reason for [flag, reason] in info.items() if flag.startswith("segfault")
+    ):
+        pytest.skip(f"known segfault: {reason}")
+
+    for reason in (
+        reason for [flag, reason] in info.items() if flag.startswith("xfail")
+    ):
         if request.config.option.run_xfail:
             request.applymarker(
                 pytest.mark.xfail(
                     run=False,
-                    reason='known failure with code "{}"'.format(",".join(error_flags)),
+                    reason=f"known failure: {reason}",
                 )
             )
         else:
-            pytest.xfail('known failure with code "{}"'.format(",".join(error_flags)))
+            pytest.xfail(f"known failure: {reason}")
+    return info
+
+
+def test_cpython_core(main_test, selenium, request):
+    [name, info] = main_test
+    info = filter_info(info, selenium.browser)
+    possibly_skip_test(request, info)
+
+    ignore_tests = info.get("skip", [])
 
     selenium.load_package(UNVENDORED_STDLIB_MODULES)
     try:
         selenium.run(
-            """
+            f"""
 import asyncio
 import os
 import platform
@@ -49,53 +68,36 @@ if hasattr(_testcapi, "raise_SIGINT_then_send_None"):
     # This uses raise() which doesn't work.
     del _testcapi.raise_SIGINT_then_send_None
 
-with (
-    patch(
-        "subprocess.Popen",
-        new=Mock(side_effect=SkipTest("Cannot start a subprocess in Pyodide")),
-    ),
-    patch(
-        "threading.Thread.start",
-        new=Mock(side_effect=SkipTest("Cannot start a thread in Pyodide")),
-    ),
-    patch(
-        "signal.setitimer",
-        new=Mock(side_effect=SkipTest("setitimer is not implemented in Emscripten")),
-    ),
-    patch("os.fork", new=Mock(side_effect=SkipTest("Can't fork"))),
-    patch(
-        "os.get_inheritable",
-        new=Mock(side_effect=SkipTest("os.get_inheritable doesn't seem to work")),
-    ),
-    patch("asyncio.run", new=Mock(side_effect=SkipTest("asyncio.run doesn't work"))),
-):
-    try:
-        libregrtest.main(["{}"], verbose=True, verbose3=True)
-    except SystemExit as e:
-        if e.code != 0:
-            raise RuntimeError(f"Failed with code: {{e.code}}")
-            """.format(
-                name
-            )
+try:
+    libregrtest.main(["{name}"], ignore_tests={ignore_tests}, verbose=True, verbose3=True)
+except SystemExit as e:
+    if e.code != 0:
+        raise RuntimeError(f"Failed with code: {{e.code}}")
+            """
         )
     except selenium.JavascriptException:
         print(selenium.logs)
         raise
 
 
+def get_test_info(test):
+    if isinstance(test, dict):
+        [name, info] = next(iter(test.items()))
+    else:
+        name = test
+        info = {}
+    return [name, info]
+
+
+@cache
+def get_tests():
+    with open(Path(__file__).parent / "python_tests.yaml") as file:
+        data = yaml.load(file, Loader)
+
+    return {name: info for test in data for [name, info] in [get_test_info(test)]}
+
+
 def pytest_generate_tests(metafunc):
-    if "python_test" in metafunc.fixturenames:
-        test_modules = []
-        test_modules_ids = []
-        with open(Path(__file__).parent / "python_tests.txt") as fp:
-            for line in fp:
-                line = line.strip()
-                if line.startswith("#") or not line:
-                    continue
-                error_flags = line.split()
-                name = error_flags.pop(0)
-                test_modules.append((name, error_flags))
-                # explicitly define test ids to keep
-                # a human readable test name in pytest
-                test_modules_ids.append(name)
-        metafunc.parametrize("python_test", test_modules, ids=test_modules_ids)
+    if "main_test" in metafunc.fixturenames:
+        tests = get_tests()
+        metafunc.parametrize("main_test", tests.items(), ids=tests.keys())
