85615f74586052c487ee9024f48d3c9415baa4b4
Hood Chatham <roberthoodchatham@gmail.com>
2022-03-12 21:04:36 -0800

    Add types to pypabuild

--- pyodide-build/pyodide_build/pypabuild.py
+++ pyodide-build/pyodide_build/pypabuild.py
@@ -4,8 +4,8 @@ import sys
 import traceback
 from itertools import chain
 from pathlib import Path
+from typing import Mapping
 
-from build import BuildBackendException, ProjectBuilder  # type: ignore[import]
 from build.__main__ import (  # type: ignore[import]
     _STYLES,
     _error,
@@ -13,14 +13,17 @@ from build.__main__ import (  # type: ignore[import]
     _IsolatedEnvBuilder,
     _ProjectBuilder,
 )
+from build.env import IsolatedEnv  # type: ignore[import]
 from packaging.requirements import Requirement
+
+from build import BuildBackendException, ProjectBuilder  # type: ignore[import]
 
 from .common import get_hostsitepackages, get_pyversion
 
 UNISOLATED_PACKAGES = ["numpy", "scipy", "cffi", "pycparser", "pythran", "cython"]
 
 
-def symlink_unisolated_packages(env):
+def symlink_unisolated_packages(env: IsolatedEnv):
     pyversion = get_pyversion()
     site_packages_path = f"lib/{pyversion}/site-packages"
     env_site_packages = Path(env._path) / site_packages_path
@@ -33,7 +36,7 @@ def symlink_unisolated_packages(env):
             (env_site_packages / path.name).symlink_to(path)
 
 
-def remove_unisolated_requirements(env, requires: set[str]) -> set[str]:
+def remove_unisolated_requirements(requires: set[str]) -> set[str]:
     for reqstr in list(requires):
         req = Requirement(reqstr)
         for avoid_name in UNISOLATED_PACKAGES:
@@ -43,10 +46,10 @@ def remove_unisolated_requirements(env, requires: set[str]) -> set[str]:
 
 
 @contextlib.contextmanager
-def replace_env(env):
+def replace_env(build_env: Mapping[str, str]):
     old_environ = dict(os.environ)
     os.environ.clear()
-    os.environ.update(env)
+    os.environ.update(build_env)
     try:
         yield
     finally:
@@ -54,12 +57,18 @@ def replace_env(env):
         os.environ.update(old_environ)
 
 
-def install_reqs(env, reqs):
-    env.install(remove_unisolated_requirements(env, reqs))
+None
+
+
+def install_reqs(env: IsolatedEnv, reqs: set[str]):
+    env.install(remove_unisolated_requirements(reqs))
 
 
 def _build_in_isolated_env(
-    build_env, builder: ProjectBuilder, outdir, distribution: str, config_settings
+    build_env: Mapping[str, str],
+    builder: ProjectBuilder,
+    outdir: str,
+    distribution: str,
 ) -> str:
     with _IsolatedEnvBuilder() as env:
         builder.python_executable = env.executable
@@ -79,10 +88,10 @@ def _build_in_isolated_env(
         with replace_env(build_env):
             if not installed_requires_for_build:
                 install_reqs(env, builder.get_requires_for_build(distribution))
-            return builder.build(distribution, outdir, config_settings or {})
+            return builder.build(distribution, outdir, {})
 
 
-def build(build_env):
+def build(build_env: Mapping[str, str]):
     srcdir = Path.cwd()
     outdir = srcdir / "dist"
     builder = _ProjectBuilder(srcdir)
@@ -90,7 +99,7 @@ def build(build_env):
     try:
         with _handle_build_error():
             built = _build_in_isolated_env(
-                build_env, builder, outdir, distribution, None
+                build_env, builder, str(outdir), distribution
             )
             print("{bold}{green}Successfully built {}{reset}".format(built, **_STYLES))
     except Exception as e:  # pragma: no cover
