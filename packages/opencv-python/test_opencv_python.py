3aa8e58fcb3a55f7a8b126cf26e3f8cf788369a8
ryanking13 <def6488@gmail.com>
2022-03-25 13:35:48 +0000

    Replace lena with baboon and add more tests

--- packages/opencv-python/test_opencv_python.py
+++ packages/opencv-python/test_opencv_python.py
@@ -6,15 +6,16 @@ from pyodide_build.testing import run_in_pyodide
 REFERENCE_IMAGES_PATH = pathlib.Path(__file__).parent / "reference-images"
 
 
-def compare_with_reference_image(selenium, reference_image, var="img"):
+def compare_with_reference_image(selenium, reference_image, var="img", grayscale=True):
     reference_image_encoded = base64.b64encode(reference_image.read_bytes())
+    grayscale = "cv.IMREAD_GRAYSCALE" if grayscale else "cv.IMREAD_COLOR"
     deviation = selenium.run(
         f"""
         import base64
         import numpy as np
         import cv2 as cv
         arr = np.frombuffer(base64.b64decode({reference_image_encoded!r}), np.uint8)
-        ref_data = cv.imdecode(arr, cv.IMREAD_GRAYSCALE)
+        ref_data = cv.imdecode(arr, {grayscale})
 
         deviation = np.mean(np.abs({var} - ref_data))
         float(deviation)
@@ -116,7 +117,7 @@ def test_image_processing():
 
 
 def test_edge_detection(selenium):
-    original_img = base64.b64encode((REFERENCE_IMAGES_PATH / "lena.png").read_bytes())
+    original_img = base64.b64encode((REFERENCE_IMAGES_PATH / "baboon.png").read_bytes())
     selenium.load_package("opencv-python")
     selenium.run(
         f"""
@@ -134,13 +135,67 @@ def test_edge_detection(selenium):
     )
 
     assert compare_with_reference_image(
-        selenium, REFERENCE_IMAGES_PATH / "lena_sobel.png", "sobel"
+        selenium, REFERENCE_IMAGES_PATH / "baboon_sobel.png", "sobel"
     )
     assert compare_with_reference_image(
-        selenium, REFERENCE_IMAGES_PATH / "lena_laplacian.png", "laplacian"
+        selenium, REFERENCE_IMAGES_PATH / "baboon_laplacian.png", "laplacian"
     )
     assert compare_with_reference_image(
-        selenium, REFERENCE_IMAGES_PATH / "lena_canny.png", "canny"
+        selenium, REFERENCE_IMAGES_PATH / "baboon_canny.png", "canny"
+    )
+
+
+def test_photo(selenium):
+    original_img = base64.b64encode((REFERENCE_IMAGES_PATH / "baboon.png").read_bytes())
+    selenium.load_package("opencv-python")
+    selenium.run(
+        f"""
+        import base64
+        import cv2 as cv
+        import numpy as np
+        src = np.frombuffer(base64.b64decode({original_img!r}), np.uint8)
+        src = cv.imdecode(src, cv.IMREAD_COLOR)
+        grayscale, color_boost = cv.decolor(src)
+        None
+        """
+    )
+
+    assert compare_with_reference_image(
+        selenium, REFERENCE_IMAGES_PATH / "baboon_decolor_grayscale.png", "grayscale"
+    )
+    assert compare_with_reference_image(
+        selenium,
+        REFERENCE_IMAGES_PATH / "baboon_decolor_color_boost.png",
+        "color_boost",
+        grayscale=False,
+    )
+
+
+def test_stitch(selenium):
+    original_img_left = base64.b64encode(
+        (REFERENCE_IMAGES_PATH / "mountain1.png").read_bytes()
+    )
+    original_img_right = base64.b64encode(
+        (REFERENCE_IMAGES_PATH / "mountain2.png").read_bytes()
+    )
+    selenium.load_package("opencv-python")
+    selenium.run(
+        f"""
+        import base64
+        import cv2 as cv
+        import numpy as np
+        left = np.frombuffer(base64.b64decode({original_img_left!r}), np.uint8)
+        left = cv.imdecode(left, cv.IMREAD_COLOR)
+        right = np.frombuffer(base64.b64decode({original_img_right!r}), np.uint8)
+        right = cv.imdecode(right, cv.IMREAD_COLOR)
+        stitcher = cv.Stitcher.create(cv.Stitcher_PANORAMA)
+        status, panorama = stitcher.stitch([left, right])
+
+        # It seems that the result is not always the same due to the randomness, so check the status and size instead
+        assert status == cv.Stitcher_OK
+        assert panorama.shape[0] >= max(left.shape[0], right.shape[0])
+        assert panorama.shape[1] >= max(left.shape[1], right.shape[1])
+        """
     )
 
 
@@ -148,5 +203,74 @@ def test_video():
     raise NotImplementedError()
 
 
+def test_flann():
+    raise NotImplementedError()
+
+
+def test_dnn():
+    raise NotImplementedError()
+
+
 def test_ml():
     raise NotImplementedError()
+
+
+def test_objdetect():
+    raise NotImplementedError()
+
+
+def test_feature2d(selenium):
+    original_img = base64.b64encode((REFERENCE_IMAGES_PATH / "baboon.png").read_bytes())
+    selenium.load_package("opencv-python")
+    selenium.run(
+        f"""
+        import base64
+        import cv2 as cv
+        import numpy as np
+        src = np.frombuffer(base64.b64decode({original_img!r}), np.uint8)
+        src = cv.imdecode(src, cv.IMREAD_COLOR)
+
+        detector = cv.KAZE_create()
+        keypoints = detector.detect(src)
+
+        kaze = cv.drawKeypoints(src, keypoints, None, color=(0, 0, 255), flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        None
+        """
+    )
+
+    assert compare_with_reference_image(
+        selenium,
+        REFERENCE_IMAGES_PATH / "baboon_kaze.png",
+        "kaze",
+        grayscale=False,
+    )
+
+
+def test_calib3d(selenium):
+    original_img = base64.b64encode(
+        (REFERENCE_IMAGES_PATH / "chessboard.png").read_bytes()
+    )
+    selenium.load_package("opencv-python")
+    selenium.run(
+        f"""
+        import base64
+        import cv2 as cv
+        import numpy as np
+        src = np.frombuffer(base64.b64decode({original_img!r}), np.uint8)
+        src = cv.imdecode(src, cv.IMREAD_COLOR)
+
+        criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+        ret, corners = cv.findChessboardCorners(gray, (9, 6), None)
+        cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        cv.drawChessboardCorners(gray, (9, 6), corners, ret)
+        chessboard_corners = gray
+        None
+        """
+    )
+
+    assert compare_with_reference_image(
+        selenium,
+        REFERENCE_IMAGES_PATH / "chessboard_corners.png",
+        "chessboard_corners",
+    )
