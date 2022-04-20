d896e5e5404c45e38aec2a35a7bbcf177049a1a7
Michael Neil <mneil@mneilsworld.com>
2022-04-20 16:21:26 -0700

    Rename test files to typescript files
    Fix imports to drop the file extensions.

--- src/js/test/module.test.ts
+++ src/js/test/module.test.ts
@@ -1,9 +1,10 @@
 import assert from "assert";
-import { Module } from "../module.js";
+import * as pyodideModule from "../module";
 
 describe("Module", () => {
   describe("noWasmDecoding", () => {
     it("should be false ", () => {
+      const Module = pyodideModule.createModule();
       assert.equal(Module.noWasmDecoding, false);
     });
   });
