0f954ea31ca8a632fa7963b36d82ee183bf083ee
Tobias Megies <megies@geophysik.uni-muenchen.de>
2022-05-05 17:43:08 +0200

    obspy: basic test, just checking read/write mseed
    this is using one of our complex C extensions, which fails currently

---
+++ packages/obspy/test_obspy.py
@@ -0,0 +1,19 @@
+from pyodide_build import run_in_pyodide
+
+
+@run_in_pyodide(packages=["obspy"])
+def test_write_mseed():
+    """
+    Test using one of our C extensions, writing data to MiniSEED
+    """
+    import numpy as np
+    from obspy import Trace, Stream
+    from obspy.io.mseed.core import _write_mseed, _read_mseed
+    tr = Trace(data=np.random.randint(-100, 100, 1000))
+    st = Stream([tr])
+    _write_mseed(st, '/tmp/test.mseed')
+    st2 = _read_mseed('/tmp/test.mseed')
+    for tr in st2:
+        tr.stats.pop('mseed')
+        tr.stats.pop('_format')
+    assert st == st2
