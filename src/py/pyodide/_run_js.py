3e25fc374d41e5646e34d39db6900f7422c065c9
Hood Chatham <roberthoodchatham@gmail.com>
2022-05-05 10:33:09 -0600

    Add docstring

--- src/py/pyodide/_run_js.py
+++ src/py/pyodide/_run_js.py
@@ -1,4 +1,12 @@
-def run_js(code, /):
+from typing import Any
+
+def run_js(code : str, /) -> Any:
+    """
+    A wrapper for the JavaScript 'eval' function. 
+    
+    Runs 'code' as a Javascript code string and returns the result. Unlike
+    JavaScript's 'eval', if 'code' is not a string we raise a TypeError.
+    """
     from js import eval
 
     if not isinstance(code, str):
