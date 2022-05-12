5961b1fe40b68d9b78d206c540036f99b5e20727
Lester Fan <lesterfan37@gmail.com>
2022-05-12 18:18:32 -0400

    Add changelog entry and docstrings

--- src/py/pyodide/_browser_apis.py
+++ src/py/pyodide/_browser_apis.py
@@ -15,12 +15,18 @@ EVENT_LISTENERS: dict[tuple[int, str, Callable[[Any], None]], JsProxy] = {}
 
 
 def add_event_listener(elt: JsProxy, event: str, listener: Callable[[Any], None]):
+    """Wrapper for JavaScript's addEventListener() which automatically manages the lifetime
+    of a JsProxy corresponding to the listener param.
+    """
     proxy = create_proxy(listener)
     EVENT_LISTENERS[(elt.js_id, event, listener)] = proxy
     elt.addEventListener(event, proxy)
 
 
 def remove_event_listener(elt: JsProxy, event: str, listener: Callable[[Any], None]):
+    """Wrapper for JavaScript's removeEventListener() which automatically manages the lifetime
+    of a JsProxy corresponding to the listener param.
+    """
     proxy = EVENT_LISTENERS.pop((elt.js_id, event, listener))
     elt.removeEventListener(event, proxy)
     proxy.destroy()
@@ -30,6 +36,9 @@ TIMEOUTS: dict[int, Destroyable] = {}
 
 
 def set_timeout(callback: Callable[[], None], timeout: int) -> int | JsProxy:
+    """Wrapper for JavaScript's setTimeout() which automatically manages the lifetime
+    of a JsProxy corresponding to the callback param.
+    """
     id = -1
 
     def wrapper():
@@ -55,6 +64,9 @@ DUMMY_DESTROYABLE = Destroyable()
 
 
 def clear_timeout(timeout_retval: int | JsProxy):
+    """Wrapper for JavaScript's clearTimeout() which automatically manages the lifetime
+    of a JsProxy corresponding to the callback param.
+    """
     clearTimeout(timeout_retval)
     id = timeout_retval if isinstance(timeout_retval, int) else timeout_retval.js_id
     TIMEOUTS.pop(id, DUMMY_DESTROYABLE).destroy()
@@ -64,6 +76,9 @@ INTERVAL_CALLBACKS: dict[int, Destroyable] = {}
 
 
 def set_interval(callback: Callable[[], None], interval: int) -> int | JsProxy:
+    """Wrapper for JavaScript's setInterval() which automatically manages the lifetime
+    of a JsProxy corresponding to the callback param.
+    """
     proxy = create_proxy(callback)
     interval_retval = setInterval(proxy, interval)
     id = interval_retval if isinstance(interval_retval, int) else interval_retval.js_id
@@ -72,6 +87,9 @@ def set_interval(callback: Callable[[], None], interval: int) -> int | JsProxy:
 
 
 def clear_interval(interval_retval: int | JsProxy):
+    """Wrapper for JavaScript's clearInterval() which automatically manages the lifetime
+    of a JsProxy corresponding to the callback param.
+    """
     clearInterval(interval_retval)
     id = interval_retval if isinstance(interval_retval, int) else interval_retval.js_id
     INTERVAL_CALLBACKS.pop(id, DUMMY_DESTROYABLE).destroy()
