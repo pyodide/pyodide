5b92731fa8c34bd8e40242a3da47437a137b4343
ryanking13 <def6488@gmail.com>
2022-05-19 04:56:56 +0000

    Fix lint

--- src/tests/test_console.py
+++ src/tests/test_console.py
@@ -4,7 +4,6 @@ import time
 
 import pytest
 from pyodide_test_runner import run_in_pyodide
-from pyodide_test_runner.fixture import selenium_common
 
 from pyodide import CodeRunner, console  # noqa: E402
 from pyodide.console import Console, _CommandCompiler, _Compile  # noqa: E402
