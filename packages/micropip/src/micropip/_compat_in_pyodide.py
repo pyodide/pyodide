806e5dff61ee07aa76cb0dd9c984f8bae3eee83f
Hood Chatham <roberthoodchatham@gmail.com>
2022-05-26 18:16:24 -0700

    MAINT improve isolation for micropip tests (#2623)
    I added a new `wheel_base` fixture which creates a new temporary
    directory and mocks `site.getsitepackages()` to return it. I also removed
    the `WHEEL_BASE` global variable from micropip and instead calculate
    it each time in `micropip.install`.

--- packages/micropip/src/micropip/_compat_in_pyodide.py
+++ packages/micropip/src/micropip/_compat_in_pyodide.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from pyodide._core import IN_BROWSER
 from pyodide.http import pyfetch
 
@@ -8,13 +6,6 @@ try:
     from pyodide_js import loadedPackages
 
     BUILTIN_PACKAGES = pyodide_js._api.packages.to_py()
-
-    # Random note: getsitepackages is not available in a virtual environment...
-    # See https://github.com/pypa/virtualenv/issues/228 (issue is closed but
-    # problem is not fixed)
-    from site import getsitepackages
-
-    WHEEL_BASE = Path(getsitepackages()[0])
 except ImportError:
     if IN_BROWSER:
         raise
@@ -32,7 +23,6 @@ async def fetch_string(url: str, kwargs: dict[str, str]) -> str:
 __all__ = [
     "fetch_bytes",
     "fetch_string",
-    "WHEEL_BASE",
     "BUILTIN_PACKAGES",
     "loadedPackages",
     "pyodide_js",
