646358678608faabc4024601b361c1cdc067c7a7
Amit Saha <mail@echorand.me>
2022-05-28 07:33:21 +0000

    Add changelog and comments

--- packages/micropip/src/micropip/_micropip.py
+++ packages/micropip/src/micropip/_micropip.py
@@ -287,6 +287,19 @@ class Transaction:
         if req.marker:
             # handle environment markers
             # https://www.python.org/dev/peps/pep-0508/#environment-markers
+
+            # For a requirement being installed as part of an optional feature
+            # via the extra specifier, the evaluation of the marker requires
+            # the extra key in self.ctx to have the value specified in the
+            # primary requirement.
+
+            # The req.extras attribute is only set for the primary requirement
+            # and hence has to be available during the evaluation of the
+            # dependencies. Thus, we use the self.ctx_extras attribute above to
+            # store all the extra values we come across during the transaction and
+            # attempt the marker evaluation for all of these values. If any of the
+            # evaluations return true we include the dependency.
+
             def eval_marker(e: dict[str, str]) -> bool:
                 self.ctx.update(e)
                 # need the assertion here to make mypy happy:
@@ -295,6 +308,11 @@ class Transaction:
                 return req.marker.evaluate(self.ctx)
 
             self.ctx.update({"extra": ""})
+            # The current package may have been brought into the transaction
+            # without any of the optional requirement specification, but has
+            # another marker, such as implementation_name. In this scenario,
+            # self.ctx_extras is empty and hence the eval_marker() function
+            # will not be called at all.
             if not req.marker.evaluate(self.ctx) and not any(
                 [eval_marker(e) for e in self.ctx_extras]
             ):
