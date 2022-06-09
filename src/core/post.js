963fd2c83f88c59f3a20040d794b1a852f1d9aae
Hood Chatham <roberthoodchatham@gmail.com>
2022-06-08 20:38:45 -0700

    Reexpose preloadedWasm

--- src/core/post.js
+++ src/core/post.js
@@ -3,3 +3,6 @@ Module.UTF8ToString = UTF8ToString;
 Module.wasmTable = wasmTable;
 // Emscripten has a bug where it accidentally exposes an empty object as Module.ERRNO_CODES
 Module.ERRNO_CODES = ERRNO_CODES;
+// preloadedWasm isn't exposed on Module by default anymore. TODO: We should use
+// a less hacky approach via the fs argument to loadDynamicLibrary
+Module.preloadedWasm = preloadedWasm;
