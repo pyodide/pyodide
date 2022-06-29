8489b59eb0ab57bc9b67237771ebd92be2bf6013
Hood Chatham <roberthoodchatham@gmail.com>
2022-06-29 07:55:08 -0700

    Fixes to create_xbuildenv

--- pyodide-build/pyodide_build/create_xbuildenv.py
+++ pyodide-build/pyodide_build/create_xbuildenv.py
@@ -19,7 +19,7 @@ def make_parser(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
 def copy_xbuild_files(xbuildenv_path):
     PYODIDE_ROOT = get_pyodide_root()
     site_packages = Path(get_make_flag("HOSTSITEPACKAGES"))
-    xbuild_site_packages = xbuildenv_path / "site-packages"
+    xbuild_site_packages = xbuildenv_path / "site-packages-extras"
     for pkg in (PYODIDE_ROOT / "packages").glob("**/meta.yaml"):
         config = parse_package_config(pkg, check=False)
         xbuild_files = config.get("build", {}).get("cross-build-files", [])
@@ -29,12 +29,50 @@ def copy_xbuild_files(xbuildenv_path):
             shutil.copy(site_packages / path, target)
 
 
+def get_relative_path(pyodide_root, flag):
+    return Path(get_make_flag(flag)).relative_to(pyodide_root)
+
+
 def main(args):
     pyodide_root = get_pyodide_root()
+    pythoninclude = get_relative_path(pyodide_root, "PYTHONINCLUDE")
+    wasm_lib_dir = get_relative_path(pyodide_root, "WASM_LIBRARY_DIR")
+    sysconfig_dir = get_relative_path(pyodide_root, "SYSCONFIGDATA_DIR")
     xbuildenv_path = pyodide_root / "xbuildenv"
     shutil.rmtree(xbuildenv_path, ignore_errors=True)
+
+    xbuildenv_root = xbuildenv_path / "pyodide-root"
     xbuildenv_path.mkdir()
-    shutil.copytree(get_make_flag("PYTHONINCLUDE"), xbuildenv_path / "python-include")
+    to_copy: list[Path] = [
+        pythoninclude,
+        sysconfig_dir,
+        Path("Makefile.envs"),
+        wasm_lib_dir / "CLAPACK",
+        wasm_lib_dir / "cmake",
+    ]
+    # Some ad-hoc stuff here to moderate size. We'd like to include all of
+    # wasm_lib_dir but there's 180mb of it. Better to leave out all the video
+    # codecs and stuff.
+    for pkg in ["ssl", "libcrypto", "zlib", "xml", "mpfr"]:
+        to_copy.extend(
+            x.relative_to(pyodide_root)
+            for x in (pyodide_root / wasm_lib_dir / "include").glob(f"**/*{pkg}*")
+            if "boost" not in str(x)
+        )
+        to_copy.extend(
+            x.relative_to(pyodide_root)
+            for x in (pyodide_root / wasm_lib_dir / "lib").glob(f"**/*{pkg}*")
+        )
+
+    for path in to_copy:
+        if (pyodide_root / path).is_dir():
+            shutil.copytree(
+                pyodide_root / path, xbuildenv_root / path, dirs_exist_ok=True
+            )
+        else:
+            (xbuildenv_root / path).parent.mkdir(exist_ok=True, parents=True)
+            shutil.copy(pyodide_root / path, xbuildenv_root / path)
+
     copy_xbuild_files(xbuildenv_path)
     res = subprocess.run(
         ["pip", "freeze", "--path", get_make_flag("HOSTSITEPACKAGES")],
--- pyodide-build/pyodide_build/install_xbuildenv.py
+++ pyodide-build/pyodide_build/install_xbuildenv.py
@@ -15,16 +15,15 @@ def make_parser(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
         "on numpy or scipy.\n"
         "Note: this is a private endpoint that should not be used outside of the Pyodide Makefile."
     )
+    parser.add_argument("xbuild_env", type=str, nargs=1)
     return parser
 
 
 def main(args):
+    xbuildenv_path = Path(args.xbuild_env[0])
     pyodide_root = get_pyodide_root()
-    host_site_packages = Path(get_make_flag("HOSTSITEPACKAGES"))
-    xbuildenv_path = pyodide_root / "xbuildenv"
-    include_path = Path(get_make_flag("PYTHONINCLUDE"))
-    include_path.mkdir(exist_ok=True, parents=True)
-    shutil.copytree(xbuildenv_path / "python-include", include_path, dirs_exist_ok=True)
+    xbuildenv_root = xbuildenv_path / "pyodide-root"
+    host_site_packages = xbuildenv_root / Path(get_make_flag("HOSTSITEPACKAGES")).relative_to(pyodide_root)
     host_site_packages.mkdir(exist_ok=True, parents=True)
     subprocess.run(
         [
@@ -37,5 +36,5 @@ def main(args):
         ]
     )
     shutil.copytree(
-        xbuildenv_path / "site-packages", host_site_packages, dirs_exist_ok=True
+        xbuildenv_path / "site-packages-extras", host_site_packages, dirs_exist_ok=True
     )
