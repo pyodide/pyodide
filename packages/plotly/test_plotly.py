7baa75d455ae2b687cad290926d2b9b5b1801da6
Andres Barbaro <afbarbaro@yahoo.com>
2022-07-01 00:42:10 +0200

    Add plotly package
    Adds plotly to the provided packages

---
+++ packages/plotly/test_plotly.py
@@ -0,0 +1,12 @@
+from pyodide_test_runner import run_in_pyodide
+
+
+@run_in_pyodide(packages=["plotly"])
+def test_plotly(selenium):
+    import plotly.express as px
+
+    df = px.data.gapminder().query("country=='Canada'")
+    fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+    fig.show()
+
+    
