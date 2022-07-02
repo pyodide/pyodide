8c49ae09503056e909a2c972e689c67d624b3cc0
John Wason <wason@wasontech.com>
2022-07-02 17:32:44 -0400

    Fix formatting errors

--- packages/RobotRaconteur/test_robotraconteur_pyodide.py
+++ packages/RobotRaconteur/test_robotraconteur_pyodide.py
@@ -1,9 +1,11 @@
 from pyodide_test_runner import run_in_pyodide
 
+
 @run_in_pyodide(packages=["RobotRaconteur", "numpy"])
 def test_robotraconteur_import(selenium):
     from RobotRaconteur.Client import RRN
-    ver = RRN.RobotRaconteurVersion
+    _ = RRN.RobotRaconteurVersion
+
 
 @run_in_pyodide(packages=["RobotRaconteur", "numpy"])
 def test_robotraconteur_exceptions(selenium):
@@ -11,6 +13,6 @@ def test_robotraconteur_exceptions(selenium):
     import pytest
     RRN = RR.RobotRaconteurNode.s
     RRN.SetNodeName("test_node")
-    assert RRN.NodeName=="test_node"
+    assert RRN.NodeName == "test_node"
     with pytest.raises(Exception):
-       RRN.SetNodeName("test_node")
+        RRN.SetNodeName("test_node")
