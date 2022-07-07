7901ad76c2afaff88f78bb622bd022c3f96ec56b
ryanking13 <def6488@gmail.com>
2022-07-07 06:32:37 +0000

    Revert "Set system name to Generic"
    This reverts commit 2f5594ccbcf5d0f112eba61b407700c0c0ac7c6a.

--- tools/cmake/Toolchain.cmake
+++ tools/cmake/Toolchain.cmake
@@ -4,7 +4,6 @@
 # with some modifications to make it work with Pyodide build system.                          #
 ###############################################################################################
 
-set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_CROSSCOMPILING TRUE)
 
 # Note: this is False in original Emscripten toolchain,
