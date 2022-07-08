b1a3709722dcb2d2fe0d2fba11ed6ead830f4d91
Hood Chatham <roberthoodchatham@gmail.com>
2022-07-08 10:49:38 -0700

    Use attr reference to version in setup.cfg

--- tools/bump_version.py
+++ tools/bump_version.py
@@ -45,11 +45,6 @@ PYTHON_TARGETS = [
         prerelease=True,
     ),
     Target(
-        ROOT / "pyodide-build/setup.cfg",
-        build_version_pattern("version = {python_version}"),
-        prerelease=True,
-    ),
-    Target(
         ROOT / "pyodide-build/pyodide_build/__init__.py",
         pattern=build_version_pattern('__version__ = "{python_version}"'),
         prerelease=True,
