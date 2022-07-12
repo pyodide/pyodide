8844d4b7765cbff4756dd88c9767776a4ca437e0
Hanno Rein <hanno@hanno-rein.de>
2022-07-12 12:05:47 -0400

    Added REBOUND package.

---
+++ packages/rebound/test_rebound.py
@@ -0,0 +1,24 @@
+from functools import reduce
+
+import pytest
+from pyodide_test_runner import run_in_pyodide
+
+@run_in_pyodide(
+    packages=[
+        "rebound",
+    ]
+)
+def test_integrate(selenium):
+    import rebound
+    sim = rebound.Simulation()
+    sim.add(m=1)
+    sim.add(m=1e-3,a=1)
+    sim.add(m=1e-3,a=2)
+    sim.integrate(10)
+
+    assert (
+        numpy.fabs(sim.t-10.0) < 1e-10
+    ), "Orbit integration not working"
+    return None
+
+
