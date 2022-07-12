3382667507e4f730d021716a7237402004b15ae3
Jo Bovy <bovy@astro.utoronto.ca>
2022-07-11 21:35:34 -0400

    Run a selection of astropy tests with a single pytest parameterization

--- packages/astropy/test_astropy.py
+++ packages/astropy/test_astropy.py
@@ -1,62 +1,46 @@
 import pytest
 
-astropy_js_test_setup= """await pyodide.loadPackage(['micropip','astropy']);
-const micropip = pyodide.pyimport("micropip");
-await micropip.install(['pytest','pytest_remotedata','pytest_doctestplus','pytest_astropy_header']);"""
-
-astropy_test_verbosity= "False"
-
 @pytest.mark.skip_refcount_check
-def test_constants(selenium):
+@pytest.mark.parametrize(
+    "package,specific_test",
+    [
+        ('constants','test_'),
+        ('units','test_logarithmic.py'),
+        ('units','test_physical.py'),
+        ('units','test_quantity.py'),
+        ('units','test_quantity_ufuncs.py'),
+        ('units','test_quantity_non_ufuncs.py'),
+        ('units','test_units.py'),
+        ('coordinates','test_angles.py'),
+        ('coordinates','test_angular_separation.py'),
+        ('coordinates','test_arrays.py'),
+        ('coordinates','test_celestial_transformations.py'),
+        ('coordinates','test_erfa_astrom.py'),
+        ('coordinates','test_frames_with_velocity.py'),
+        ('coordinates','test_icrs_observed_transformations.py'),
+        ('coordinates','test_matching.py'),
+        ('coordinates','test_representation.py and not test_unitphysics'),
+        ('coordinates','test_sky_coord.py'),
+        ('coordinates','test_sky_coord_velocities.py'),
+        ('coordinates','test_transformations.py'),
+        ('cosmology','test_base.py'),
+        ('cosmology','test_lambdacdm.py'),
+        ('cosmology','test_w0cdm.py'),
+        ('cosmology','test_w0wacdm.py'),
+        ('cosmology','test_units.py'),
+    ]
+)
+def test_astropy(selenium,package,specific_test):
     selenium.run_js(
-        f"""{astropy_js_test_setup}
+        f"""await pyodide.loadPackage(['micropip','astropy']);
+        const micropip = pyodide.pyimport("micropip");
+        await micropip.install(['pytest','pytest_remotedata','pytest_doctestplus','pytest_astropy_header']);
         pyodide.runPython(`
             import pytest
             import astropy
-            assert astropy.test(package='constants',
-                                verbose={astropy_test_verbosity},
-                                args="-k 'not test_angle_multithreading and not test_unitphysics'") == pytest.ExitCode.OK
-        `);
-        """
-    )
-
-@pytest.mark.skip_refcount_check
-def test_coordinates(selenium):
-    selenium.run_js(
-        f"""{astropy_js_test_setup}
-        pyodide.runPython(`
-            import pytest
-            import astropy
-            assert astropy.test(package='coordinates',
-                                verbose={astropy_test_verbosity},
-                                args="-k 'not test_angle_multithreading and not test_unitphysics'") == pytest.ExitCode.OK
-        `);
-        """
-    )
-
-@pytest.mark.skip_refcount_check
-def test_cosmology(selenium):
-    selenium.run_js(
-        f"""{astropy_js_test_setup}
-        pyodide.runPython(`
-            import pytest
-            import astropy
-            assert astropy.test(package='cosmology',
-                                verbose={astropy_test_verbosity}) == pytest.ExitCode.OK
-        `);
-        """
-    )
-
-@pytest.mark.skip_refcount_check
-def test_units(selenium):
-    selenium.run_js(
-        f"""{astropy_js_test_setup}
-        pyodide.runPython(`
-            import pytest
-            import astropy
-            assert astropy.test(package='units',
-                                verbose={astropy_test_verbosity},
-                                args="-k 'not test_thread_safety'") == pytest.ExitCode.OK
+            assert astropy.test(package='{package}',
+                                verbose=False,
+                                args="-k '{specific_test} and not thread'") == pytest.ExitCode.OK
         `);
         """
     )
