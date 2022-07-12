2220eda601e42454c6be38b7a154823fa6d7ba55
Jo Bovy <bovy@astro.utoronto.ca>
2022-07-12 11:11:01 -0400

    Use current cache dir when deciding whether to re-run tests

--- tools/pytest_wrapper.py
+++ tools/pytest_wrapper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 import sys
 
@@ -13,6 +14,13 @@ def remove_num_threads_option(args: list[str]) -> None:
             del args[i : i + 2]
             break
 
+def cache_dir(args: list[str]) -> None:
+    """Find the name of the cache-dir in the argument list"""
+    for i in range(0, len(args)):
+        if args[i] == "-o" and args[i + 1].startswith("cache_dir"):
+            return args[i + 1].split("=")[1]
+            break
+    return ".pytest_cache"
 
 if __name__ == "__main__":
     try:
@@ -22,7 +30,7 @@ if __name__ == "__main__":
         pass
 
     # Failed tests. Look up number of failed tests
-    with open(".pytest_cache/v/cache/lastfailed") as f:
+    with open(os.path.join(cache_dir(args),"v/cache/lastfailed")) as f:
         num_failed = sum(1 for line in f) - 2
 
     if num_failed > 9:
