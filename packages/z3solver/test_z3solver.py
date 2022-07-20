183f174272d3c2713bedd2d58ba5578b93affe2b
Nikolaj Bjorner <nbjorner@microsoft.com>
2022-07-20 03:18:33 +0000

    craving that lindt
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- packages/z3solver/test_z3solver.py
+++ packages/z3solver/test_z3solver.py
@@ -5,23 +5,27 @@ from pyodide_test_runner import run_in_pyodide
     packages=["z3solver"],
 )
 def test_z3_socrates(selenium):
-    """ test to see if z3 is alive """
+    """test to see if z3 is alive"""
+
     import z3
-    obj = z3.DeclareSort('Object')
-    human = z3.Function('Human', obj, z3.BoolSort())
-    mortal = z3.Function('Mortal', obj, z3.BoolSort())
+    obj = z3.DeclareSort("Object")
+    human = z3.Function("Human", obj, z3.BoolSort())
+    mortal = z3.Function("Mortal", obj, z3.BoolSort())
     # a well known philosopher
-    socrates = z3.Const('socrates', obj)
+    socrates = z3.Const("socrates", obj)
     # free variables used in forall must be declared Const in python
-    x = z3.Const('x', obj)
+    x = z3.Const("x", obj)
     axioms = [z3.ForAll([x], z3.Implies(human(x), mortal(x))), human(socrates)]
     s = z3.Solver()
     s.add(axioms)
-    print(s.check()) # prints sat so axioms are coherent
-    assert(z3.sat == s.check())
+    print(s.check())  
+    # prints sat so axioms are coherent
+    assert z3.sat == s.check()
     # classical refutation
     s.add(z3.Not(mortal(socrates)))
     is_sat = s.check()
     print(is_sat)
-    assert(is_sat == z3.unsat)
-    print(is_sat) # prints unsat so socrates is Mortal
+    assert is_sat == z3.unsat
+    # prints unsat so socrates is Mortal    
+    print(is_sat) 
+
