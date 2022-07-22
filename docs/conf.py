c2874e99c67592aa03df98291caec14fca049a9e
ryanking13 <def6488@gmail.com>
2022-07-22 01:49:31 +0000

    Fix doc build

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ autosummary_generate = True
 autodoc_default_flags = ["members", "inherited-members"]
 
 # Add modules to be mocked.
-mock_modules = ["ruamel.yaml", "tomli"]
+mock_modules = ["ruamel.yaml", "tomli", "build"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
