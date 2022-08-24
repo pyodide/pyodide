7d9f8df9432e019a93515b4b65c74180200787e0
mgrover1 <mgroverwx@gmail.com>
2022-08-24 13:18:11 -0500

    add pyart package

---
+++ packages/arm_pyart/test_pyart.py
@@ -0,0 +1,9 @@
+from pyodide_test_runner import run_in_pyodide
+
+
+@run_in_pyodide(packages=["arm_pyart"])
+def test_xarray():
+    import pyart
+
+    radar = pyart.testing.make_empty_ppi_radar(100, 100, 1)
+    radar.range['data'][0] == 0.0
