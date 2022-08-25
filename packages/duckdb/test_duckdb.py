02ec6b149c45949e51a152d8b077a98c1c9a5a65
Ian Calvert <ianjcalvert@gmail.com>
2022-08-25 08:33:58 +0100

    Add numpy as an explicit requirement & remove pandas from the test

--- packages/duckdb/test_duckdb.py
+++ packages/duckdb/test_duckdb.py
@@ -1,19 +1,13 @@
 from pytest_pyodide import run_in_pyodide
 
 
-@run_in_pyodide(packages=["duckdb", "pandas"])
+@run_in_pyodide(packages=["duckdb"])
 def test_duckdb(selenium):
     import duckdb
-    import pandas as pd
 
-    example_df = pd.DataFrame({"col_1": [1, 2, 3, 4, 5], "col_2": [10, 20, 30, 40, 50]})
-    con = duckdb.connection(":memory:")
-    con.register("example_table", example_df)
-    query = """
-    SELECT SUM(col_1) as sum_col_1, SUM(col_2) as sum_col_2
-    FROM example_table
-    """
-    query_result = con.execute(query).fetchall()
-    assert query_result[0][0] == 15
-    assert query_result[1][0] == 150
+    con = duckdb.connect(":memory:")
+    con.execute("CREATE TABLE t(id int, content string);")
+    con.execute("INSERT INTO t VALUES (42, 'hello'), (43, 'world');")
+    query_result = con.execute("SELECT rowid, id, content FROM t;").fetchall()
     con.close()
+    assert query_result == [(0, 42, "hello"), (1, 43, "world")]
