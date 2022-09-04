e3ce00f27ed41f11d1f225d81102a87923c2ebc0
Hood Chatham <roberthoodchatham@gmail.com>
2022-09-03 20:41:00 -0700

    Add a test that uses -m

--- src/tests/test_cmdline_runner.py
+++ src/tests/test_cmdline_runner.py
@@ -4,7 +4,7 @@ import sys
 import pytest
 
 import pyodide
-from pyodide_build.common import get_pyodide_root
+from pyodide_build.common import emscripten_version, get_pyodide_root
 
 
 @pytest.mark.xfail_browsers(chrome="node only", firefox="node only")
@@ -31,6 +31,15 @@ def test_cmdline_runner(selenium):
     assert result.returncode == 0
     assert result.stdout.strip() == pyodide.__version__
     assert result.stderr == ""
+
+    result = subprocess.run(
+        [pyodide_root / "tools/python.js", "-m", "platform"],
+        capture_output=True,
+        encoding="utf8",
+    )
+    assert result.returncode == 0
+    assert result.stderr == ""
+    assert result.stdout.strip() == f"Emscripten-{emscripten_version()}-wasm32-32bit"
 
     result = subprocess.run(
         [pyodide_root / "tools/python.js", "-c"], capture_output=True, encoding="utf8"
