58dc324389a6cd8d254076402d43f8f74cbeae36
Hood Chatham <roberthoodchatham@gmail.com>
2022-09-04 22:30:55 -0700

    Rearrange tests

--- src/tests/test_cmdline_runner.py
+++ src/tests/test_cmdline_runner.py
@@ -1,4 +1,5 @@
 import re
+import subprocess
 import sys
 
 import pytest
@@ -6,19 +7,22 @@ import pytest
 import pyodide
 from pyodide_build.common import emscripten_version, get_pyodide_root
 
+mark = pytest.mark.xfail_browsers(chrome="node only", firefox="node only")
 
-@pytest.mark.xfail_browsers(chrome="node only", firefox="node only")
-def test_cmdline_runner(selenium):
-    pyodide_root = get_pyodide_root()
-    import subprocess
+pyodide_root = get_pyodide_root()
+script_path = pyodide_root / "tools/python"
 
-    script_path = pyodide_root / "tools/python"
 
+@mark
+def test_python_version(selenium):
     result = subprocess.run([script_path, "-V"], capture_output=True, encoding="utf8")
     assert result.returncode == 0
     assert result.stdout.strip() == "Python " + sys.version.partition(" ")[0]
     assert result.stderr == ""
 
+
+@mark
+def test_dash_c(selenium):
     result = subprocess.run(
         [
             script_path,
@@ -32,6 +36,9 @@ def test_cmdline_runner(selenium):
     assert result.stdout.strip() == pyodide.__version__
     assert result.stderr == ""
 
+
+@mark
+def test_ensure_future(selenium):
     result = subprocess.run(
         [
             script_path,
@@ -51,6 +58,29 @@ asyncio.ensure_future(test())
     assert result.stderr == ""
     assert result.stdout.strip() == "done"
 
+
+@mark
+def test_flush_on_exit(selenium):
+    result = subprocess.run(
+        [
+            script_path,
+            "-c",
+            """\
+print("a", end="")
+print("b", end="")
+print("c", end="")
+""",
+        ],
+        capture_output=True,
+        encoding="utf8",
+    )
+    assert result.returncode == 0
+    assert result.stderr == ""
+    assert result.stdout.strip() == "abc"
+
+
+@mark
+def test_dash_m(selenium):
     result = subprocess.run(
         [script_path, "-m", "platform"],
         capture_output=True,
@@ -60,6 +90,9 @@ asyncio.ensure_future(test())
     assert result.stderr == ""
     assert result.stdout.strip() == f"Emscripten-{emscripten_version()}-wasm32-32bit"
 
+
+@mark
+def test_invalid_cmdline_option(selenium):
     result = subprocess.run([script_path, "-c"], capture_output=True, encoding="utf8")
     assert result.returncode != 0
     assert result.stdout == ""
