835203a66201fede56478dac3300f94e0d858199
Gyeongjae Choi <def6488@gmail.com>
2022-09-06 03:06:16 +0000

    Add version variable for JS package

---
+++ src/js/version.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * The Pyodide version.
+ *
+ * The version scheme follows [PEP 440](https://www.python.org/dev/peps/pep-0440/).
+ * We don't follow semver, because we want to compare this with the version of Pyodide Python package.
+ */
+const version: string = "22.0.0.dev0";
+
+export default version;
