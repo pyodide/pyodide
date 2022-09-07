925267f99adb3d07130d58c5f59628c972d13c6a
Gyeongjae Choi <def6488@gmail.com>
2022-09-07 02:22:11 +0000

    Fix libmagic path

--- packages/python-magic/test_magic.py
+++ packages/python-magic/test_magic.py
@@ -3,13 +3,16 @@ from pytest_pyodide import run_in_pyodide
 
 @run_in_pyodide(packages=["python-magic"])
 def test_magic(selenium):
+    import site
+
     import magic
 
     buffer = b"abcde"
     assert magic.from_buffer(buffer) == "ASCII text, with no line terminators"
     assert magic.from_buffer(buffer, mime=True) == "text/plain"
 
-    filename = "/usr/lib/libmagic.so"
+    site_packages = site.getsitepackages()[0]
+    filename = site_packages + "/libmagic.so"
     assert (
         magic.from_file(filename)
         == "WebAssembly (wasm) binary module version 0x1 (MVP)"
