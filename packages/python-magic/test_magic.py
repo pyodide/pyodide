c4e7d83831e2f7061267957dfc390affeca973b9
Gyeongjae Choi <def6488@gmail.com>
2022-09-07 04:27:37 +0000

    Revert "Fix libmagic path"
    This reverts commit 925267f99adb3d07130d58c5f59628c972d13c6a.

--- packages/python-magic/test_magic.py
+++ packages/python-magic/test_magic.py
@@ -3,16 +3,13 @@ from pytest_pyodide import run_in_pyodide
 
 @run_in_pyodide(packages=["python-magic"])
 def test_magic(selenium):
-    import site
-
     import magic
 
     buffer = b"abcde"
     assert magic.from_buffer(buffer) == "ASCII text, with no line terminators"
     assert magic.from_buffer(buffer, mime=True) == "text/plain"
 
-    site_packages = site.getsitepackages()[0]
-    filename = site_packages + "/libmagic.so"
+    filename = "/usr/lib/libmagic.so"
     assert (
         magic.from_file(filename)
         == "WebAssembly (wasm) binary module version 0x1 (MVP)"
