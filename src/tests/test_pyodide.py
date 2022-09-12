da74f913f90bc24ab17167c8b7ff456e44856a75
Hood Chatham <roberthoodchatham@gmail.com>
2022-09-11 19:10:54 -0500

    Use __dirname for indexURL in node, include source maps in release again (#3088)
    Webpack generates a warning for missing source maps. This fixes it. 
    See issue #3086.

--- src/tests/test_pyodide.py
+++ src/tests/test_pyodide.py
@@ -1,5 +1,6 @@
 import re
 import shutil
+import subprocess
 from collections.abc import Sequence
 from pathlib import Path
 from textwrap import dedent
@@ -1308,8 +1309,6 @@ def test_args(selenium_standalone_noload):
 @pytest.mark.xfail_browsers(chrome="Node only", firefox="Node only", safari="Node only")
 def test_relative_index_url(selenium, tmp_path):
     tmp_dir = Path(tmp_path)
-    import subprocess
-
     version_result = subprocess.run(
         ["node", "-v"], capture_output=True, encoding="utf8"
     )
@@ -1357,6 +1356,42 @@ def test_relative_index_url(selenium, tmp_path):
         assert result.stdout.strip().split("\n")[-1] == str(ROOT_PATH / "dist") + "/"
     finally:
         print_result(result)
+
+
+@pytest.mark.xfail_browsers(chrome="Node only", firefox="Node only", safari="Node only")
+def test_index_url_calculation_source_map(selenium):
+    import os
+
+    node_options = ["--enable-source-maps"]
+
+    result = subprocess.run(["node", "-v"], capture_output=True, encoding="utf8")
+    if result.stdout.startswith("v14"):
+        node_options.append("--experimental-wasm-bigint")
+
+    DIST_DIR = str(Path.cwd() / "dist")
+
+    env = os.environ.copy()
+    env["DIST_DIR"] = DIST_DIR
+    result = subprocess.run(
+        [
+            "node",
+            *node_options,
+            "-e",
+            """
+            const { loadPyodide } = require(`${process.env.DIST_DIR}/pyodide`);
+            async function main() {
+                const py = await loadPyodide();
+                console.log("indexURL:", py._module.API.config.indexURL);
+            }
+            main();
+            """,
+        ],
+        env=env,
+        capture_output=True,
+        encoding="utf8",
+    )
+
+    assert f"indexURL: {DIST_DIR}" in result.stdout
 
 
 @pytest.mark.xfail_browsers(
