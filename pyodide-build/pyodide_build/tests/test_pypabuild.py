7cd94079bbf534a3a1ec365fae15abcbe3c5e12e
Gyeongjae Choi <def6488@gmail.com>
2022-09-20 01:25:35 +0000

    Remove a test that is not available before build

--- pyodide-build/pyodide_build/tests/test_pypabuild.py
+++ pyodide-build/pyodide_build/tests/test_pypabuild.py
@@ -1,7 +1,4 @@
-import pathlib
-
 from pyodide_build import pypabuild
-from pyodide_build.common import get_pyversion
 
 
 class MockIsolatedEnv:
@@ -33,14 +30,3 @@ def test_install_reqs(tmp_path):
     pypabuild.install_reqs(env, set(pypabuild.AVOIDED_REQUIREMENTS))  # type: ignore[arg-type]
     for req in pypabuild.AVOIDED_REQUIREMENTS:
         assert req not in env.installed
-
-
-def test_symlink_unisolated_packages(tmp_path):
-    env = MockIsolatedEnv(tmp_path)
-    pypabuild.symlink_unisolated_packages(env)  # type: ignore[arg-type]
-    pyversion = get_pyversion()
-
-    site_packages_path = pathlib.Path(f"{env.path}/lib/{pyversion}/site-packages")
-    files = [path.name for path in site_packages_path.glob("*.py")]
-
-    assert "_sysconfigdata__emscripten_wasm32-emscripten.py" in files
