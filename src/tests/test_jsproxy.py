cf3f355306e138175e5c02b08a2cf188b7988919
Hood Chatham <roberthoodchatham@gmail.com>
2022-09-22 15:36:38 -0700

    Add descr_get handler to JsMethod (#3130)

--- src/tests/test_jsproxy.py
+++ src/tests/test_jsproxy.py
@@ -1276,3 +1276,20 @@ def test_jsarray_reverse(selenium):
 
     assert a.to_py() == l
     assert b.to_bytes() == bytes(l)
+
+
+@run_in_pyodide
+def test_jsproxy_descr_get(selenium):
+    from pyodide.code import run_js
+
+    class T:
+        a: int
+        b: int
+        f = run_js("function f(x) {return this[x]; }; f")
+
+    t = T()
+    t.a = 7
+    t.b = 66
+    assert t.f("a") == 7
+    assert t.f("b") == 66
+    assert t.f("c") is None
