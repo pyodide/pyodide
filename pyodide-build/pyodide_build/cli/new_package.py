916b67bf1e41f333df275d30e02fe00428d20bce
Gyeongjae Choi <def6488@gmail.com>
2022-10-11 01:10:35 +0000

    Add packages-dir option in package cli

--- pyodide-build/pyodide_build/cli/new_package.py
+++ pyodide-build/pyodide_build/cli/new_package.py
@@ -23,21 +23,26 @@ def new_package(
     source_format: str = typer.Option(
         None,
         help="Which source format is preferred. Options are wheel or sdist. "
-        "If none is provided, then either a wheel or an sdist will be used. ",
+        "If not specified, then either a wheel or an sdist will be used. ",
     ),
     root: str = typer.Option(
         None, help="The root directory of the Pyodide.", envvar="PYODIDE_ROOT"
+    ),
+    packages_dir: str = typer.Option(
+        None,
+        help="The directory containing the packages."
+        "If not specified, the default is `packages` in the root directory.",
     ),
 ) -> None:
     """
     Create a new package.
     """
+    pyodide_root = common.search_pyodide_root(Path.cwd()) if not root else Path(root)
+    packages_dir_ = (
+        pyodide_root / "packages" if not packages_dir else Path(packages_dir)
+    )
 
-    # FIXME: root can be None but typer does not support complex type annoations...
-    if root is None:
-        root = common.search_pyodide_root(Path.cwd())  # type: ignore[unreachable]
-
-    mkpkg.make_package(Path(root) / "packages", name, version, source_fmt=source_format)  # type: ignore[arg-type]
+    mkpkg.make_package(packages_dir_, name, version, source_fmt=source_format)  # type: ignore[arg-type]
 
 
 @app.command("update")  # type: ignore[misc]
@@ -50,7 +55,7 @@ def update_package(
     source_format: str = typer.Option(
         None,
         help="Which source format is preferred. Options are wheel or sdist. "
-        "If none is provided, the type will be kept the same if possible.",
+        "If not specified, the type will be kept the same if possible.",
     ),
     update_patched: bool = typer.Option(
         False, help="Force update the package even if it contains patches."
@@ -58,17 +63,23 @@ def update_package(
     root: str = typer.Option(
         None, help="The root directory of the Pyodide.", envvar="PYODIDE_ROOT"
     ),
+    packages_dir: str = typer.Option(
+        None,
+        help="The directory containing the packages."
+        "If not specified, the default is `packages` in the root directory.",
+    ),
 ) -> None:
     """
     Update an existing package.
     """
 
-    # FIXME: root can be None but typer does not support complex type annoations...
-    if root is None:
-        root = common.search_pyodide_root(Path.cwd())  # type: ignore[unreachable]
+    pyodide_root = common.search_pyodide_root(Path.cwd()) if not root else Path(root)
+    packages_dir_ = (
+        pyodide_root / "packages" if not packages_dir else Path(packages_dir)
+    )
 
     mkpkg.update_package(
-        Path(root) / "packages",
+        packages_dir_,
         name,
         version,
         source_fmt=source_format,  # type: ignore[arg-type]
