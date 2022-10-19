5231901882b156353a03113a8e10481240dbebb0
Gyeongjae Choi <def6488@gmail.com>
2022-10-19 03:02:07 +0000

    Add geopandas

---
+++ packages/geopandas/test_geopandas.py
@@ -0,0 +1,53 @@
+import pytest
+from pytest_pyodide import run_in_pyodide
+
+
+@pytest.mark.driver_timeout(300)
+@run_in_pyodide(packages=["geopandas", "geopandas-tests", "pytest", "pandas-tests"])
+def test_runtest(selenium):
+    from pathlib import Path
+
+    import geopandas
+    import pytest
+
+    test_path = Path(geopandas.__file__).parent / "tests"
+
+    def runtest(test_filter, ignore_filters):
+
+        ignore_filter = []
+        for ignore in ignore_filters:
+            ignore_filter.append("--ignore-glob")
+            ignore_filter.append(ignore)
+
+        ret = pytest.main(
+            [
+                "--pyargs",
+                str(test_path),
+                "--continue-on-collection-errors",
+                # "-v",
+                *ignore_filter,
+                "-k",
+                test_filter,
+            ]
+        )
+        assert ret == 0
+
+    runtest(
+        (
+            "not test_transform2 "  # CppException std::invalid_argument: non double value
+            "and not test_no_additional_imports "  # subprocess
+            "and not test_pandas_kind "  # scipy required
+        ),
+        [
+            str(
+                test_path / "test_dissolve.py"
+            ),  # CppException osgeo::proj::io::ParsingException: unrecognized format / unknown name
+            str(
+                test_path / "test_geodataframe.py"
+            ),  # CppException osgeo::proj::io::ParsingException: unrecognized format / unknown name
+            str(
+                test_path / "test_testing.py"
+            ),  # CppException osgeo::proj::io::ParsingException: unrecognized format / unknown name
+            str(test_path / "test_array.py"),  # libc.so required
+        ],
+    )
