d6ce3383a173fa33d10b2d5b5fb2f298fe41b5c6
Gyeongjae Choi <def6488@gmail.com>
2022-10-31 06:45:47 +0000

    Add method prototype

--- pyodide-build/pyodide_build/cli/build.py
+++ pyodide-build/pyodide_build/cli/build.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import typer  # type: ignore[import]
 
 from .. import common
@@ -23,10 +25,29 @@ def main(
     build_wheel(exports, ctx)
 
 
-@app.command("graph")
-def build_graph():
-    """Build the dependency graph for the packages (in-tree build)."""
-    pass
+@app.command("graph")  # type: ignore[misc]
+def build_graph(
+    packages: list[str] = typer.Argument(
+        ..., help="Packages to build, or * for all packages"
+    ),
+    output: Path = typer.Option(
+        None,
+        help="Path to output the dependency graph",
+    ),
+    root: str = typer.Option(
+        None, help="The root directory of the Pyodide.", envvar="PYODIDE_ROOT"
+    ),
+    recipe_dir: str = typer.Option(
+        None,
+        help="The directory containing the recipe of packages."
+        "If not specified, the default is `packages` in the root directory.",
+    ),
+) -> None:
+    """Build packages and create repodata.json (in-tree build)"""
+    # pyodide_root = common.search_pyodide_root(Path.cwd()) if not root else Path(root)
+    # recipe_dir_ = pyodide_root / "packages" if not recipe_dir else Path(recipe_dir)
+
+    # buildall.build_packages()
 
 
 @app.command("wheel")  # type: ignore[misc]
