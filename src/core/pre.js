4e2857547aa253e43e757f0010a7be5bcb9232b3
Hood Chatham <roberthoodchatham@gmail.com>
2022-10-30 22:24:37 -0700

    Fix node v14

--- src/core/pre.js
+++ src/core/pre.js
@@ -4,7 +4,12 @@ const Tests = {};
 API.tests = Tests;
 API.version = "0.22.0.dev0";
 Module.hiwire = Hiwire;
-Module.suspenderGlobal = new WebAssembly.Global(
-  { value: "externref", mutable: true },
-  null,
-);
+try {
+  Module.suspenderGlobal = new WebAssembly.Global(
+    { value: "externref", mutable: true },
+    null,
+  );
+} catch (e) {
+  // Browser doesn't support externref. This implies it also doesn't support
+  // stack switching so we won't need a suspender.
+}
