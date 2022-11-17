4a1913d3adc672680f5a02c385841e303f6254ee
Joe Marshall <joe.marshall@nottingham.ac.uk>
2022-11-17 01:05:14 +0000

    build packages from pypi direct (#3196)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Hood Chatham <roberthoodchatham@gmail.com>
    Co-authored-by: Gyeongjae Choi <def6488@gmail.com>
    Co-authored-by: Roman Yurchak <rth.yurchak@gmail.com>

--- docs/project/changelog.md
+++ docs/project/changelog.md
@@ -106,6 +106,18 @@ substitutions:
   `pyodide-build mkpkg` will be replaced by `pyodide sekeleton pypi`.
   {pr}`3175`
 
+- Added a new CLI command `pyodide build-recipes` which build packages from recipe folder.
+  It replaces `pyodide-build buildall`.
+  {pr}`3196`
+
+- Added subcommands for `pyodide build` which builds packages from various sources.
+  | command | result |
+  |-------------|-------|
+  | `pyodide build pypi` | build or fetch a single package from pypi |
+  | `pyodide build source` | build the current source folder (same as pyodide build) |
+  | `pyodide build url` | build or fetch a package from a url either tgz, tar.gz zip or wheel |
+  {pr}`3196`
+
 - {{ Fix }} Fixed bug in `split` argument of {any}`repr_shorten`. Added {any}`shorten` function.
   {pr}`3178`
 
--- pyodide-build/pyodide_build/tests/test_cli.py
+++ pyodide-build/pyodide_build/tests/test_cli.py
@@ -1,6 +1,18 @@
+import os
+import shutil
+from pathlib import Path
+
+import pytest
 from typer.testing import CliRunner  # type: ignore[import]
 
-from pyodide_build.cli import skeleton
+from pyodide_build import __version__ as pyodide_build_version
+from pyodide_build import common
+from pyodide_build.cli import build, skeleton
+
+only_node = pytest.mark.xfail_browsers(
+    chrome="node only", firefox="node only", safari="node only"
+)
+
 
 runner = CliRunner()
 
@@ -44,3 +56,79 @@ def test_skeleton_pypi(tmp_path):
     )
     assert result.exit_code != 0
     assert "already exists" in str(result.exception)
+
+
+def test_build_recipe_with_pyodide(tmp_path, monkeypatch, request, runtime):
+    if runtime != "node":
+        pytest.xfail("node only")
+    test_build_recipe(tmp_path, monkeypatch, request)
+
+
+def test_build_recipe(tmp_path, monkeypatch, request):
+    if "dev" in pyodide_build_version:
+        if "EMSDK" not in os.environ or "PYODIDE_ROOT" not in os.environ:
+            pytest.skip(
+                reason="Can't build recipe in dev mode without building pyodide first"
+            )
+    output_dir = tmp_path / "dist"
+    recipe_dir = Path(__file__).parent / "_test_recipes"
+
+    pkgs = {
+        "pkg_test_graph1": {"pkg_test_graph2"},
+        "pkg_test_graph3": {},
+    }
+
+    pkgs_to_build = pkgs.keys() | {p for v in pkgs.values() for p in v}
+
+    monkeypatch.setattr(common, "ALWAYS_PACKAGES", {})
+
+    for build_dir in recipe_dir.rglob("build"):
+        shutil.rmtree(build_dir)
+
+    result = runner.invoke(
+        build.app,
+        [
+            "recipe",
+            *pkgs.keys(),
+            "--recipe-dir",
+            recipe_dir,
+            "--output",
+            output_dir,
+        ],
+    )
+
+    assert result.exit_code == 0, result.stdout
+
+    for pkg in pkgs_to_build:
+        assert f"built {pkg} in" in result.stdout
+
+    built_wheels = set(output_dir.glob("*.whl"))
+    assert len(built_wheels) == len(pkgs_to_build)
+
+
+def test_fetch_or_build_pypi_with_pyodide(tmp_path, runtime):
+    if runtime != "node":
+        pytest.xfail("node only")
+    test_fetch_or_build_pypi(tmp_path)
+
+
+def test_fetch_or_build_pypi(tmp_path):
+    if "dev" in pyodide_build_version:
+        if "EMSDK" not in os.environ or "PYODIDE_ROOT" not in os.environ:
+            pytest.skip(
+                reason="Can't build recipe in dev mode without building pyodide first. Skipping test"
+            )
+    output_dir = tmp_path / "dist"
+    # one pure-python package (doesn't need building) and one sdist package (needs building)
+    pkgs = ["pytest-pyodide", "pycryptodome==3.15.0"]
+
+    os.chdir(tmp_path)
+    for p in pkgs:
+        result = runner.invoke(
+            build.app,
+            ["main", p],
+        )
+        assert result.exit_code == 0, result.stdout
+
+    built_wheels = set(output_dir.glob("*.whl"))
+    assert len(built_wheels) == len(pkgs)
