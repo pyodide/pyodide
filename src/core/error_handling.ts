4dfea95a6354846b77655fc40417fb84765eac56
Hood Chatham <roberthoodchatham@gmail.com>
2022-11-28 22:33:42 -0800

    Fix typescript compilation

--- src/core/error_handling.ts
+++ src/core/error_handling.ts
@@ -15,7 +15,7 @@ function ensureCaughtObjectIsError(e: any): Error {
     typeof e.message !== "string"
   ) {
     // We caught something really weird. Be brave!
-    const typeTag = getTypeTag(e);
+    const typeTag = API.getTypeTag(e);
     let msg = `A value of type ${typeof e} with tag ${typeTag} was thrown as an error!`;
     try {
       msg += `\nString interpolation of the thrown value gives """${e}""".`;
