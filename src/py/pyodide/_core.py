c7b37c390c70bb94553b95a42dbd9a9ec81c376a
Hood Chatham <roberthoodchatham@gmail.com>
2022-12-23 11:27:27 -0800

    Format comment again

--- src/py/pyodide/_core.py
+++ src/py/pyodide/_core.py
@@ -33,10 +33,13 @@ if IN_BROWSER:
 
     import _pyodide._core_docs
 
-    # This is intentionally opaque to static analysis tools (e.g., mypy) Note:
-    # Normally one would handle this by adding type stubs for _pyodide_core, but
-    # since we already are getting the correct types from _core_docs, adding a type
-    # stub would introduce a redundancy that would be difficult to maintain.
+    # This is intentionally opaque to static analysis tools (e.g., mypy)
+    #
+    # Note:
+    #   Normally one would handle this by adding type stubs for
+    #   _pyodide_core, but since we already are getting the correct types
+    #   from _core_docs, adding a type stub would introduce a redundancy
+    #   that would be difficult to maintain.
     for t in [
         "ConversionError",
         "JsException",
