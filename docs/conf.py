4114f20986a3caa923d9bd512a3795a8b9044a49
Gyeongjae Choi <def6488@gmail.com>
2022-12-28 12:17:23 +0900

    DOC Fix various sphinx warnings (#3391)
    Fix various sphinx build warnings.
    
    I removed **all cross-references** in the changelog document. It causes trouble 
    whenever we make an API change.
    For example, Hood did some great work of splitting JsProxy classes into subclasses
    in this release, but the changelog of older versions still contains cross-references to
    `JsProxy.<method>` which doesn't exist anymore. It doesn't make sense to change it
    to e.g. `JsBuffer.<method>` or `JsArray.<method>` as those classes are not available in
    that version, so I think the reasonable option would be not using cross-references.

--- docs/conf.py
+++ docs/conf.py
@@ -90,7 +90,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md", "sphinx_pyodide"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
