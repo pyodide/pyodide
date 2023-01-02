1fbbaa62441db49444d8e4ccd4b2e658c4d166ff
ryanking13 <def6488@gmail.com>
2023-01-02 07:08:20 +0000

    Add create_zip method

---
+++ pyodide-build/pyodide_build/cli/create_zip.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+import typer  # type: ignore[import]
+
+from ..pyzip import create_zip as _create_zip
+
+
+def create_zip(
+    libdir: Path = typer.Argument(
+        ..., help="Path to the directory containing the Python standard library."
+    ),
+    pycompile: bool = typer.Option(
+        False, help="Whether to compile the .py files into .pyc."
+    ),
+    output: Path = typer.Option(
+        "python.zip", help="Path to the output zip file. Defaults to python.zip."
+    ),
+) -> None:
+    """
+    Bundle Python standard libraries into a zip file.
+    """
+    _create_zip(libdir, output, pycompile=pycompile, filterfunc=None)
+    typer.echo(f"Zip file created at {output}")
