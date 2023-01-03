1169cf59a3d81bc84af147fca3ebe23acf6c11ca
Hood Chatham <roberthoodchatham@gmail.com>
2023-01-02 23:26:37 -0500

    Hide deprecated arguments from docs (#3396)
    This adds a mechanism to hide deprecated or private arguments from the documentation and
    applies it to errorCallbackDeprecated.

--- src/js/api.ts
+++ src/js/api.ts
@@ -100,6 +100,7 @@ let loadPackagesFromImportsPositionalCallbackDeprecationWarned = false;
  *    (optional)
  * @param options.checkIntegrity If true, check the integrity of the downloaded
  *    packages (default: true)
+ * @param errorCallbackDeprecated @ignore
  * @async
  */
 export async function loadPackagesFromImports(
@@ -116,14 +117,16 @@ export async function loadPackagesFromImports(
   if (typeof options === "function") {
     if (!loadPackagesFromImportsPositionalCallbackDeprecationWarned) {
       console.warn(
-        "Passing a messageCallback or errorCallback as the second or third argument to loadPackagesFromImports is deprecated and will be removed in v0.24. Instead use { messageCallback : callbackFunc }",
+        "Passing a messageCallback (resp. errorCallback) as the second (resp. third) argument to loadPackageFromImports " +
+          "is deprecated and will be removed in v0.24. Instead use:\n" +
+          "   { messageCallback : callbackFunc }",
       );
-      options = {
-        messageCallback: options,
-        errorCallback: errorCallbackDeprecated,
-      };
       loadPackagesFromImportsPositionalCallbackDeprecationWarned = true;
     }
+    options = {
+      messageCallback: options,
+      errorCallback: errorCallbackDeprecated,
+    };
   }
 
   let pyimports = API.pyodide_code.find_imports(code);
