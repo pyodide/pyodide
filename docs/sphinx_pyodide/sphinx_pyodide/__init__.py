4e84ea80d5aaa296b13f79eba7e28313d0a90eb2
Hood Chatham <roberthoodchatham@gmail.com>
2023-01-06 00:06:04 -0500

    DOCS Improve rendering of JavaScript property types (#3426)

--- docs/sphinx_pyodide/sphinx_pyodide/__init__.py
+++ docs/sphinx_pyodide/sphinx_pyodide/__init__.py
@@ -12,7 +12,38 @@ def wrap_analyzer(app):
     app._sphinxjs_analyzer = PyodideAnalyzer(app._sphinxjs_analyzer)
 
 
+def patch_templates():
+    """Patch in a different jinja2 loader so we can override templates with our
+    own versions.
+    """
+    from pathlib import Path
+
+    from jinja2 import ChoiceLoader, Environment, FileSystemLoader, PackageLoader
+    from sphinx_js.analyzer_utils import dotted_path
+    from sphinx_js.renderers import JsRenderer
+
+    loader = ChoiceLoader(
+        [
+            FileSystemLoader(Path(__file__).parent / "templates"),
+            PackageLoader("sphinx_js", "templates"),
+        ]
+    )
+    env = Environment(loader=loader)
+
+    def patched_rst_method(self, partial_path, obj, use_short_name=False):
+        """Return rendered RST about an entity with the given name and IR
+        object."""
+        dotted_name = partial_path[-1] if use_short_name else dotted_path(partial_path)
+
+        # Render to RST using Jinja:
+        template = env.get_template(self._template)
+        return template.render(**self._template_vars(dotted_name, obj))
+
+    JsRenderer.rst = patched_rst_method
+
+
 def setup(app):
+    patch_templates()
     monkeypatch_module_documenter()
     app.add_lexer("pyodide", PyodideLexer)
     app.add_lexer("html-pyodide", HtmlPyodideLexer)
