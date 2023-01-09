8ad70936943d5373daeac82bea8c9f2ac3af1d96
ryanking13 <def6488@gmail.com>
2023-01-09 02:23:18 +0000

    Add console wrapper

---
+++ pyodide-build/pyodide_build/rich_console.py
@@ -0,0 +1,41 @@
+from rich.console import Console
+from rich.theme import Theme
+
+color_theme = Theme(
+    {
+        "info": "cyan1",
+        "warning": "yellow1",
+        "error": "red1",
+        "success": "green1",
+    }
+)
+
+
+class PyodideConsole(Console):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs, theme=color_theme)
+
+    def info(self, *args, **kwargs):
+        self.print(*args, **kwargs, style="info")
+
+    def warning(self, *args, **kwargs):
+        self.print(*args, **kwargs, style="warning")
+
+    def error(self, *args, **kwargs):
+        self.print(*args, **kwargs, style="error")
+
+    def success(self, *args, **kwargs):
+        self.print(*args, **kwargs, style="success")
+
+
+console_stdout = PyodideConsole()
+console_stderr = PyodideConsole(stderr=True)
+
+
+if __name__ == "__main__":
+    # For testing the colors
+    console_stdout.print("print")
+    console_stdout.info("info")
+    console_stdout.warning("warning")
+    console_stdout.error("error")
+    console_stdout.success("success")
