ee129a56411cf209892265817136cd64f561690a
Hood Chatham <roberthoodchatham@gmail.com>
2023-01-15 13:01:36 -0800

    Add tests

--- docs/sphinx_pyodide/sphinx_pyodide/__init__.py
+++ docs/sphinx_pyodide/sphinx_pyodide/__init__.py
@@ -1,9 +1,3 @@
-import inspect
-import re
-from typing import Any
-
-from sphinx_autodoc_typehints import format_annotation, get_all_type_hints
-
 from .jsdoc import (
     PyodideAnalyzer,
     get_jsdoc_content_directive,
@@ -47,139 +41,6 @@ def patch_templates():
     JsRenderer.rst = patched_rst_method
 
 
-def handle_screwed_up_return_info(app, what, name, obj, options, lines):
-    """If there is a "Returns" block in the docstring but it has only one line
-    of info, napoleon will mess up and think it's the return type.
-
-    Fix this by converting it from a ":rtype:" to a ":return:" and adding the
-    correct :rtype: with sphinx_autodoc_typehints.format_annotation.
-    """
-
-    #  Find the rtype
-    for at, line in enumerate(lines):
-        if line.startswith(":rtype:"):
-            idx = at
-            break
-    else:
-        return
-
-    # Calculate the correct value
-    type_hints = get_all_type_hints(app.config.autodoc_mock_imports, obj, name)
-    if "return" not in type_hints:
-        return
-    formatted_annotation = format_annotation(type_hints["return"], app.config)
-
-    # Sometimes the rtype is already right. Don't double it up!
-    if lines[at] == f":rtype: {formatted_annotation}":
-        return
-    # Convert current :rtype: into a :return: and add a new :rtype: with the
-    # calculated contents.
-    lines[at] = line.replace(":rtype:", ":return:")
-    lines.insert(idx, f":rtype: {formatted_annotation}")
-
-
-def ensure_argument_types(app, what, name, obj, options, lines):
-    """If there is no Parameters section at all, this adds type
-    annotations for all the arguments.
-    """
-    for at, line in enumerate(lines):
-        if line.startswith(":param"):
-            return
-        if line.startswith(("..", ":return", ":rtype")):
-            at = at
-            break
-    type_hints = get_all_type_hints(app.config.autodoc_mock_imports, obj, name)
-    to_add = [""]
-    type_hints.pop("return", None)
-    for (key, value) in type_hints.items():
-        formatted_annotation = format_annotation(value, app.config)
-        to_add.append(f":type {key}: {formatted_annotation}")
-        to_add.append(f":param {key}:")
-
-    lines[at:at] = to_add
-
-
-LEADING_STAR_PAT = re.compile(r"(^\s*)\*")
-
-
-def handle_bulleted_return_annotation(app, what, name, obj, options, lines):
-    """
-    For some reason autodoc messes up the rendering of return value annotations:
-    if the annotation takes up multiple lines, it displays it as a bulleted list
-    with the last bullet italicized. This removes the bullet and the
-    italicization of the last line.
-    """
-    for idx, line in enumerate(lines):
-        if line.startswith(":returns:"):
-            cur = idx
-            break
-    else:
-        return
-    line = line.removeprefix(":returns:").strip()
-    if not line.startswith("*"):
-        return
-    if line.startswith("* **"):
-        return
-    # Remove bullet from first line
-    lines[cur] = lines[cur].replace(":returns: *", ":returns:")
-    for idx in range(cur + 1, len(lines)):
-        if lines[idx].strip() == "":
-            break
-        # Remove bullet from rest of lines
-        lines[idx] = LEADING_STAR_PAT.sub(r"\1", lines[idx])
-    # Remove italicization of last line
-    lines[idx - 1] = LEADING_STAR_PAT.sub(r"\1", lines[idx - 1]).rstrip()[:-1]
-
-
-def fix_constructor_arg_and_attr_same_name(app, what, name, obj, options, lines):
-    """Napoleon has a bug that causes it to grab type annotations for
-    constructor args from
-
-    https://github.com/sphinx-doc/sphinx/pull/11131
-    """
-    cons_type_hints = get_all_type_hints(
-        app.config.autodoc_mock_imports, obj.__init__, name
-    )
-    attrs_type_hints = get_all_type_hints(app.config.autodoc_mock_imports, obj, name)
-    for (key, value) in cons_type_hints.items():
-        if key not in attrs_type_hints:
-            continue
-        for at, line in enumerate(lines):
-            if line.startswith(f":type {key}:"):
-                at = at
-                break
-        else:
-            continue
-        formatted_annotation = format_annotation(value, app.config)
-        lines[:] = [line for line in lines if not line.startswith(f":type {key}:")]
-        lines.insert(at, f":type {key}: {formatted_annotation}")
-
-
-def process_docstring(
-    app: Any,
-    what: str,
-    name: str,
-    obj: Any,
-    options: Any,
-    lines: list[str],  # noqa: U100
-) -> None:
-    """sphinx_autodoc_typehints adds an :rtype: to properties even though IMO it
-    is redundant. Undo this.
-
-    Fixed upstream: https://github.com/tox-dev/sphinx-autodoc-typehints/pull/287
-    """
-    if inspect.isdatadescriptor(obj):
-        lines[:] = [line for line in lines if not line.startswith(":rtype:")]
-
-    if what in ["method", "function"]:
-        handle_bulleted_return_annotation(app, what, name, obj, options, lines)
-        handle_screwed_up_return_info(app, what, name, obj, options, lines)
-        ensure_argument_types(app, what, name, obj, options, lines)
-
-    if what == "class":
-        fix_constructor_arg_and_attr_same_name(app, what, name, obj, options, lines)
-
-
 def setup(app):
     patch_templates()
     app.add_lexer("pyodide", PyodideLexer)
@@ -189,4 +50,6 @@ def setup(app):
     app.add_directive("js-doc-summary", get_jsdoc_summary_directive(app))
     app.add_directive("js-doc-content", get_jsdoc_content_directive(app))
     app.add_directive("pyodide-package-list", get_packages_summary_directive(app))
+    from .napoleon_fixes import process_docstring
+
     app.connect("autodoc-process-docstring", process_docstring)
---
+++ docs/sphinx_pyodide/sphinx_pyodide/napoleon_fixes.py
@@ -0,0 +1,164 @@
+import inspect
+import re
+from typing import Any
+
+from sphinx.application import Sphinx
+from sphinx.ext.autodoc import Options
+from sphinx_autodoc_typehints import format_annotation, get_all_type_hints
+
+
+def fix_screwed_up_return_info(
+    app: Sphinx, what: str, name: str, obj: Any, lines: list[str]
+) -> None:
+    """If there is a "Returns" block in the docstring but it has only one line
+    of info, napoleon will mess up and think it's the return type.
+
+    Fix this by converting it from a ":rtype:" to a ":return:" and adding the
+    correct :rtype: with sphinx_autodoc_typehints.format_annotation.
+    """
+
+    #  Find the rtype
+    for at, line in enumerate(lines):
+        if line.startswith(":rtype:"):
+            idx = at
+            break
+    else:
+        return
+
+    # Calculate the correct value
+    type_hints = get_all_type_hints(app.config.autodoc_mock_imports, obj, name)
+    if "return" not in type_hints:
+        return
+    formatted_annotation = format_annotation(type_hints["return"], app.config)
+
+    # Sometimes the rtype is already right. Don't double it up!
+    if lines[at] == f":rtype: {formatted_annotation}":
+        return
+    # Convert current :rtype: into a :return: and add a new :rtype: with the
+    # calculated contents.
+    lines[at] = line.replace(":rtype:", ":return:")
+    lines.insert(idx, f":rtype: {formatted_annotation}")
+
+
+def ensure_argument_types(
+    app: Sphinx, what: str, name: str, obj: Any, lines: list[str]
+) -> None:
+    """If there is no Parameters section at all, this adds type
+    annotations for all the arguments.
+    """
+    for at, line in enumerate(lines):
+        if line.startswith(":param"):
+            return
+        if line.startswith(("..", ":return", ":rtype")):
+            at = at
+            if line.startswith(".."):
+                lines.insert(at, "")
+            break
+    else:
+        # Confusing. See test_ensure_argument_types_only_summary.
+        while lines[-3:] != ["", "", ""]:
+            lines.append("")
+            at += 1
+        at -= 1
+
+    type_hints = get_all_type_hints(app.config.autodoc_mock_imports, obj, name)
+    to_add = []
+    if lines[at].strip() != "":
+        to_add.append("")
+    type_hints.pop("return", None)
+    for (key, value) in type_hints.items():
+        formatted_annotation = format_annotation(value, app.config)
+        to_add.append(f":type {key}: {formatted_annotation}")
+        to_add.append(f":param {key}:")
+
+    lines[at:at] = to_add
+
+
+LEADING_STAR_PAT = re.compile(r"(^\s*) \*")
+
+
+def fix_bulleted_return_annotation(lines: list[str]) -> None:
+    """
+    For some reason autodoc messes up the rendering of return value annotations:
+    if the annotation takes up multiple lines, it displays it as a bulleted list
+    with the last bullet italicized. This removes the bullet and the
+    italicization of the last line.
+    """
+    orig_lines = lines[:]
+    for idx, line in enumerate(lines):
+        if line.startswith(":returns:"):
+            cur = idx
+            break
+    else:
+        return
+    line = line.removeprefix(":returns:").strip()
+    if not line.startswith("*"):
+        return
+    if line.startswith("* **"):
+        return
+    # Remove bullet from first line
+    lines[cur] = lines[cur].replace(":returns: *", ":returns:")
+    for idx in range(cur + 1, len(lines)):
+        if lines[idx].strip() == "":
+            idx -= 1
+            break
+        # Remove bullet from rest of lines
+        lines[idx] = LEADING_STAR_PAT.sub(r"\1", lines[idx])
+    # Remove italicization of last line
+    lines[idx] = " " + LEADING_STAR_PAT.sub(r"\1", lines[idx]).rstrip()[:-1]
+    print("orig_lines", orig_lines)
+    print("lines", lines)
+
+
+def fix_constructor_arg_and_attr_same_name(
+    app: Sphinx, what: str, name: str, obj: Any, lines: list[str]
+) -> None:
+    """Napoleon has a bug that causes it to grab type annotations for
+    constructor args from attribute annotations.
+
+    https://github.com/sphinx-doc/sphinx/pull/11131
+    """
+    cons_type_hints = get_all_type_hints(
+        app.config.autodoc_mock_imports, obj.__init__, name
+    )
+    attrs_type_hints = get_all_type_hints(app.config.autodoc_mock_imports, obj, name)
+    for (key, value) in cons_type_hints.items():
+        if key not in attrs_type_hints:
+            continue
+        for at, line in enumerate(lines):
+            if line.startswith(f":type {key}:"):
+                at = at
+                break
+        else:
+            continue
+        formatted_annotation = format_annotation(value, app.config)
+        lines[:] = [line for line in lines if not line.startswith(f":type {key}:")]
+        lines.insert(at, f":type {key}: {formatted_annotation}")
+
+
+def process_docstring(
+    app: Sphinx,
+    what: str,
+    name: str,
+    obj: Any,
+    options: Options | None,
+    lines: list[str],
+) -> None:
+    """sphinx_autodoc_typehints adds an :rtype: to properties even though IMO it
+    is redundant. Undo this.
+
+    Fixed upstream: https://github.com/tox-dev/sphinx-autodoc-typehints/pull/287
+    """
+    if inspect.isdatadescriptor(obj):
+        lines[:] = [line for line in lines if not line.startswith(":rtype:")]
+
+    if what in ["method", "function"]:
+        fix_bulleted_return_annotation(lines)
+        fix_screwed_up_return_info(app, what, name, obj, lines)
+        ensure_argument_types(app, what, name, obj, lines)
+
+    if what == "class":
+        fix_constructor_arg_and_attr_same_name(app, what, name, obj, lines)
+
+
+__all__ = ["process_docstring"]
