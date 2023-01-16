63a33625195e83233984320b30c9b1b087a3e0d5
ryanking13 <def6488@gmail.com>
2023-01-16 02:14:53 +0000

    Update configs

--- src/core/pre.js
+++ src/core/pre.js
@@ -6,3 +6,8 @@ API.version = "0.22.0";
 Module.hiwire = Hiwire;
 const getTypeTag = (x) => Object.prototype.toString.call(x);
 API.getTypeTag = getTypeTag;
+
+if (typeof document !== "undefined") {
+  // Emscripten SDL libraries relies on Module.canvas
+  Module.canvas = document.querySelector("canvas#canvas");
+}
