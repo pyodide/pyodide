c2a605093e7683dc447a4b5e578df6c491e94e35
ryanking13 <def6488@gmail.com>
2023-01-27 00:33:53 +0000

    Fix test

--- pyodide-build/pyodide_build/tests/test_buildall.py
+++ pyodide-build/pyodide_build/tests/test_buildall.py
@@ -1,4 +1,3 @@
-import argparse
 import hashlib
 import zipfile
 from pathlib import Path
@@ -8,6 +7,7 @@ from typing import Any
 import pytest
 
 from pyodide_build import buildall, io
+from pyodide_build.pywasmcross import BuildArgs
 
 RECIPE_DIR = Path(__file__).parent / "_test_recipes"
 
@@ -107,9 +107,7 @@ def test_build_dependencies(n_jobs, monkeypatch):
 
     pkg_map = buildall.generate_dependency_graph(RECIPE_DIR, {"pkg_1", "pkg_2"})
 
-    buildall.build_from_graph(
-        pkg_map, argparse.Namespace(n_jobs=n_jobs, force_rebuild=True)
-    )
+    buildall.build_from_graph(pkg_map, BuildArgs(), n_jobs=n_jobs, force_rebuild=True)
 
     assert set(build_list) == {
         "pkg_1",
@@ -141,9 +139,7 @@ def test_build_all_dependencies(n_jobs, monkeypatch):
 
     pkg_map = buildall.generate_dependency_graph(RECIPE_DIR, packages={"*"})
 
-    buildall.build_from_graph(
-        pkg_map, argparse.Namespace(n_jobs=n_jobs, force_rebuild=False)
-    )
+    buildall.build_from_graph(pkg_map, BuildArgs(), n_jobs=n_jobs, force_rebuild=False)
 
 
 @pytest.mark.parametrize("n_jobs", [1, 4])
@@ -160,7 +156,7 @@ def test_build_error(n_jobs, monkeypatch):
 
     with pytest.raises(ValueError, match="Failed build"):
         buildall.build_from_graph(
-            pkg_map, argparse.Namespace(n_jobs=n_jobs, force_rebuild=True)
+            pkg_map, BuildArgs(), n_jobs=n_jobs, force_rebuild=True
         )
 
 
