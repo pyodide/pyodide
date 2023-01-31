447a1cad22c5fdbc1dd5772bcb1bdb57422729e2
ryanking13 <def6488@gmail.com>
2023-01-31 05:32:49 +0000

    Fix test

--- pyodide-build/pyodide_build/tests/test_cli.py
+++ pyodide-build/pyodide_build/tests/test_cli.py
@@ -170,7 +170,7 @@ def test_build_recipe_no_deps_force_rebuild(selenium, tmp_path):
     )
 
     assert result.exit_code == 0
-    assert f"running bdist_wheel" not in result.stdout
+    assert "Creating virtualenv isolated environment" not in result.stdout
     assert f"Succeeded building package {pkg}" in result.stdout
 
     result = runner.invoke(
@@ -185,7 +185,7 @@ def test_build_recipe_no_deps_force_rebuild(selenium, tmp_path):
     )
 
     assert result.exit_code == 0
-    assert f"running bdist_wheel" in result.stdout
+    assert "Creating virtualenv isolated environment" in result.stdout
     assert f"Succeeded building package {pkg}" in result.stdout
 
 
