66d08e24f0eb3400bd09c375eccf8ec70f9b683b
Hood Chatham <roberthoodchatham@gmail.com>
2023-01-31 21:46:53 -0800

    Add ffi.ts

---
+++ src/js/ffi.ts
@@ -0,0 +1,60 @@
+export type {
+  PyProxy,
+  PyProxyWithLength,
+  PyProxyWithGet,
+  PyProxyWithSet,
+  PyProxyWithHas,
+  PyDict,
+  PyIterable,
+  PyAsyncIterable,
+  PyIterator,
+  PyAsyncIterator,
+  PyGenerator,
+  PyAsyncGenerator,
+  PyAwaitable,
+  PyCallable,
+  PyProxyBuffer,
+  PyBufferView,
+} from "./pyproxy.gen";
+
+import {
+  PyProxy,
+  PyProxyWithLength,
+  PyProxyWithGet,
+  PyProxyWithSet,
+  PyProxyWithHas,
+  PyDict,
+  PyIterable,
+  PyAsyncIterable,
+  PyIterator,
+  PyAsyncIterator,
+  PyGenerator,
+  PyAsyncGenerator,
+  PyAwaitable,
+  PyCallable,
+  PyProxyBuffer,
+  PyBufferView,
+} from "./pyproxy.gen";
+
+/**
+ * See :ref:`js-api-pyodide-ffi`
+ * @hidetype
+ */
+export const ffi = {
+  PyProxy,
+  PyProxyWithLength,
+  PyProxyWithGet,
+  PyProxyWithSet,
+  PyProxyWithHas,
+  PyDict,
+  PyIterable,
+  PyAsyncIterable,
+  PyIterator,
+  PyAsyncIterator,
+  PyGenerator,
+  PyAsyncGenerator,
+  PyAwaitable,
+  PyCallable,
+  PyProxyBuffer,
+  PyBufferView,
+};
