1a6b2dc17c8bb4a817a47f4f1c03c5c27d0f5fac
Joe Marshall <joe.marshall@nottingham.ac.uk>
2023-02-28 22:19:18 +0000

    fixed test for fastparquet

--- packages/fastparquet/test_fastparquet.py
+++ packages/fastparquet/test_fastparquet.py
@@ -3,18 +3,20 @@ from pytest_pyodide import run_in_pyodide
 
 COMPRESSIONS = ("SNAPPY", "GZIP", "LZ4", "BROTLI", "ZSTD")
 
+
 # just check that we can read and write random data
 @pytest.mark.parametrize("compression", COMPRESSIONS)
-@run_in_pyodide(packages=["fastparquet"])
+@run_in_pyodide(packages=["fastparquet", "packaging"])
 def test_simple_table(selenium, compression):
-    from tempfile import NamedTemporaryFile
-
     import fastparquet  # type: ignore
-    import numpy as np
     import pandas as pd  # type: ignore
+    import numpy as np  # type: ignore
+    from tempfile import TemporaryDirectory
+    from pathlib import Path
 
-    df = pd.DataFrame(np.random.randn(131072, 8), columns=list("ABCDEFGH"))
-    with NamedTemporaryFile(suffix=".parquet") as tf:
-        fastparquet.write(tf, df, compression=compression)
-        df2 = fastparquet.read(tf)
-        assert df2 == df
+    df = pd.DataFrame(np.random.randn(131072, 4), columns=list("ABCD"))
+    with TemporaryDirectory() as td:
+        name = Path(td) / "test.parquet"
+        fastparquet.write(name, df, compression=compression)
+        df2 = fastparquet.ParquetFile(name).to_pandas()
+        assert df2.equals(df)
