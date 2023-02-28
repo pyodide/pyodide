6ee26a05bf17dcafbe84b960361c55dca1271cef
Joe Marshall <joe.marshall@nottingham.ac.uk>
2023-02-28 14:08:50 +0000

    added tests for fastparquet

---
+++ packages/fastparquet/test_fastparquet.py
@@ -0,0 +1,18 @@
+import pytest
+from pytest_pyodide import run_in_pyodide
+
+COMPRESSIONS = ("SNAPPY", "GZIP", "LZ4", "BROTLI", "ZSTD")
+
+# just check that we can read and write random data
+@pytest.mark.parametrize("compression", COMPRESSIONS)
+@run_in_pyodide(packages=["fastparquet"])
+def test_simple_table(selenium,compression):
+    import fastparquet
+    import pandas as pd
+    import numpy as np
+    from tempfile import NamedTemporaryFile
+    df = pd.DataFrame(np.random.randn(131072, 8), columns=list('ABCDEFGH'))
+    with NamedTemporaryFile(suffix=".parquet") as tf:
+        fastparquet.write(tf, df,compression=compression)
+        df2=fastparquet.read(tf)
+        assert(df2==df)
