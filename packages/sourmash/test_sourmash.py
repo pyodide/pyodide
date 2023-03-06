a3774cf9d0720b5ecc5fe8254331188702b6d12a
Luiz Irber <github@luizirber.org>
2023-03-05 17:04:34 -0800

    add sourmash test

---
+++ packages/sourmash/test_sourmash.py
@@ -0,0 +1,22 @@
+import pytest
+from pytest_pyodide import run_in_pyodide
+
+
+@run_in_pyodide(packages=["sourmash"])
+def test_simple_save_load(selenium):
+    from pathlib import Path
+    from tempfile import TemporaryDirectory
+
+    import sourmash
+
+    mh = sourmash.MinHash(0, 5, scaled=1)
+    mh.add_sequence("ACGTAGGTATAGGATACCTCGCTAGTACGTGCA")
+    ss = sourmash.SourmashSignature(mh, name="foo")
+
+    with TemporaryDirectory() as td:
+        name = Path(td) / "test.sig"
+        with open(name, "w") as fp:
+            sourmash.save_signatures([ss], fp=fp)
+
+        loaded = sourmash.load_one_signature(str(name))
+        assert loaded.equals(ss)
