be3317bc225b41f118fd57f691b351febaca5fb8
Luiz Irber <github@luizirber.org>
2023-03-15 09:53:15 -0700

    mark test for longer timeout

--- packages/sourmash/test_sourmash.py
+++ packages/sourmash/test_sourmash.py
@@ -1,6 +1,8 @@
+import pytest
 from pytest_pyodide import run_in_pyodide
 
 
+@pytest.mark.driver_timeout(60)
 @run_in_pyodide(packages=["sourmash"])
 def test_simple_save_load(selenium):
     from pathlib import Path
