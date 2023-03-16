a33364e951d9368b772cb0a3a728761047cae857
ryanking13 <def6488@gmail.com>
2023-03-16 09:39:46 +0000

    Add test

---
+++ src/tests/test_pyodide_hook.py
@@ -0,0 +1,50 @@
+def test_hook_indexedDB(selenium_standalone_noload):
+    selenium = selenium_standalone_noload
+
+    selenium.run_js(
+        """
+        indexedDBHook = {
+            beforeFileSystemInitialized: (Module) => {
+                Module.preRun.push(() => {
+                    const mountDir = '/lib';
+                    Module.FS.mkdirTree(mountDir);
+                    Module.FS.mount(Module.IDBFS, {root : "."}, "/lib");
+
+                    Module.addRunDependency("install-idbfs");
+                    Module.FS.syncfs(true, () => {
+                        Module.removeRunDependency("install-idbfs");
+                    })
+
+                    const hasCache = localStorage.getItem('PYODIDE_IDB_CACHE') === 'true';
+                    if(hasCache){
+                        Module.fileSystemInitialized = true;
+                    }
+                });
+            }
+        }
+        """
+    )
+
+    selenium.run_js(
+        """
+        let pyodide = await loadPyodide({
+            hooks : [indexedDBHook],
+        });
+
+        pyodide.runPython('f = open("/lib/hello.txt", "w"); f.write("hello world"); f.close()');
+        pyodide.runPython('assert open("/lib/hello.txt").read() == "hello world"');
+
+        await new Promise((resolve, _) => pyodide.FS.syncfs(false, resolve));
+        localStorage.setItem('PYODIDE_IDB_CACHE', 'true');
+        """
+    )
+
+    selenium.run_js(
+        """
+        let pyodide2 = await loadPyodide({
+            hooks : [indexedDBHook],
+        });
+
+        pyodide2.runPython('open("/lib/hello.txt").read() == "hello world"');
+        """
+    )
