30a5afaf9fbbdb1a27c743519470685e7b7b021b
chandraOloid <57661133+chandraOloid@users.noreply.github.com>
2023-03-23 13:39:28 +0530

    Add files via upload

---
+++ packages/dlib/test_dlib.py
@@ -0,0 +1,9 @@
+from pytest_pyodide import run_in_pyodide
+
+
+@run_in_pyodide(packages=["dlib"])
+def test_pyyaml(selenium):
+    import dlib
+    version = dlib.__version__
+    major, minor, sub = version.split('.')
+    assert major == '19' and minor == '24'
