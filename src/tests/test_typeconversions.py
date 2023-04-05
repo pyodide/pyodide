pyodide/pyodide

commit ffbccde2b6593e8944f1203bd6817647cbb0b0f1

NFC Improve and simplify compute_typeflags function in jsproxy.c (#3283)
This improves compute_typeflags by moving the implementation
into JavaScript. This makes it more readable (because the conditions
are all in one place right next to each other rather than spread through
hiwire.c) and faster (because we don't have to call 15 different JavaScript
functions).

src/core/jsproxy.c
@@ -68,7 +68,6 @@ _Py_IDENTIFIER(_js_type_flags);
 Js_IDENTIFIER(then);
 Js_IDENTIFIER(finally);
 Js_IDENTIFIER(has);
-Js_IDENTIFIER(get);
 Js_IDENTIFIER(set);
 Js_IDENTIFIER(delete);
 Js_IDENTIFIER(includes);
@@ -215,7 +214,7 @@ JsProxy_GetAttr(PyObject* self, PyObject* attr)
     FAIL();
   }
 
-  if (!hiwire_is_pyproxy(idresult) && hiwire_is_function(idresult)) {
+  if (!pyproxy_Check(idresult) && hiwire_is_function(idresult)) {
     pyresult = JsProxy_create_with_this(idresult, JsProxy_REF(self));
   } else {
     pyresult = js2python(idresult);
@@ -1996,7 +1995,7 @@ finally:
     // If we succeeded and the result was a promise then we destroy the
     // arguments in async_done_callback instead of here. Otherwise, destroy the
     // arguments and return value now.
-    if (idresult != NULL && hiwire_is_pyproxy(idresult)) {
+    if (idresult != NULL && pyproxy_Check(idresult)) {
       JsArray_Push_unchecked(proxies, idresult);
     }
     destroy_proxies(proxies,
@@ -2819,77 +2818,49 @@ finally:
   return (PyTypeObject*)type;
 }
 
-EM_JS_NUM(int, JsProxy_array_detect, (JsRef idobj), {
-  try {
-    let obj = Hiwire.get_value(idobj);
-    if (Array.isArray(obj)) {
-      return IS_ARRAY;
-    }
-    let typeTag = Object.prototype.toString.call(obj);
-    // We want to treat some standard array-like objects as Array.
-    // clang-format off
-    if(typeTag === "[object HTMLCollection]" || typeTag === "[object NodeList]"){
-      // clang-format on
-      return IS_NODE_LIST;
-    }
-    // What if it's a TypedArray?
-    // clang-format off
-    if (ArrayBuffer.isView(obj) && obj.constructor.name !== "DataView") {
-      // clang-format on
-      return IS_TYPEDARRAY;
-    }
-    return 0;
-  } catch (e) {
+#define SET_FLAG_IF(flag, cond)                                                \
+  if (cond) {                                                                  \
+    type_flags |= flag                                                         \
+  }
+
+EM_JS_NUM(int, compute_typeflags, (JsRef idobj), {
+  let obj = Hiwire.get_value(idobj);
+  let type_flags = 0;
+  // clang-format off
+  if (API.isPyProxy(obj) && obj.$$.ptr === 0) {
     return 0;
   }
+
+  const constructorName = obj.constructor ? obj.constructor.name : "";
+  let typeTag = Object.prototype.toString.call(obj);
+
+  SET_FLAG_IF(IS_CALLABLE, typeof obj === "function")
+  SET_FLAG_IF(IS_AWAITABLE, typeof obj.then === 'function')
+  SET_FLAG_IF(IS_ITERABLE, typeof obj[Symbol.iterator] === 'function')
+  SET_FLAG_IF(IS_ITERATOR, typeof obj.next === 'function')
+  SET_FLAG_IF(HAS_LENGTH,
+    (typeof obj.size === "number") ||
+    (typeof obj.length === "number" && typeof obj !== "function"));
+  SET_FLAG_IF(HAS_GET, typeof obj.get === "function");
+  SET_FLAG_IF(HAS_SET, typeof obj.set === "function");
+  SET_FLAG_IF(HAS_HAS, typeof obj.has === "function");
+  SET_FLAG_IF(HAS_INCLUDES, typeof obj.includes === "function");
+  SET_FLAG_IF(IS_BUFFER,
+              ArrayBuffer.isView(obj) || (constructorName === "ArrayBuffer"));
+  SET_FLAG_IF(IS_DOUBLE_PROXY, API.isPyProxy(obj));
+  SET_FLAG_IF(IS_ARRAY, Array.isArray(obj));
+  SET_FLAG_IF(IS_NODE_LIST,
+              typeTag === "[object HTMLCollection]" ||
+              typeTag === "[object NodeList]");
+  SET_FLAG_IF(IS_TYPEDARRAY,
+              ArrayBuffer.isView(obj) && obj.constructor.name !== "DataView");
+  // clang-format on
+  return type_flags;
 });
+#undef SET_FLAG_IF
 
 ////////////////////////////////////////////////////////////
 // Public functions
-
-static int
-compute_typeflags(JsRef object)
-{
-  int type_flags = 0;
-  if (hiwire_is_function(object)) {
-    type_flags |= IS_CALLABLE;
-  }
-  if (hiwire_is_promise(object)) {
-    type_flags |= IS_AWAITABLE;
-  }
-  if (hiwire_is_iterable(object)) {
-    type_flags |= IS_ITERABLE;
-  }
-  if (hiwire_is_iterator(object)) {
-    type_flags |= IS_ITERATOR;
-  }
-  if (hiwire_has_length(object)) {
-    type_flags |= HAS_LENGTH;
-  }
-  if (hiwire_HasMethodId(object, &JsId_get)) {
-    type_flags |= HAS_GET;
-  }
-  if (hiwire_HasMethodId(object, &JsId_set)) {
-    type_flags |= HAS_SET;
-  }
-  if (hiwire_HasMethodId(object, &JsId_has)) {
-    type_flags |= HAS_HAS;
-  }
-  if (hiwire_HasMethodId(object, &JsId_includes)) {
-    type_flags |= HAS_INCLUDES;
-  }
-  if (hiwire_is_typedarray(object)) {
-    type_flags |= IS_BUFFER;
-  }
-  if (hiwire_is_promise(object)) {
-    type_flags |= IS_AWAITABLE;
-  }
-  if (pyproxy_Check(object)) {
-    type_flags |= IS_DOUBLE_PROXY;
-  }
-  type_flags |= JsProxy_array_detect(object);
-  return type_flags;
-}
 
 static PyObject*
 create_proxy_of_type(int type_flags, JsRef object, JsRef this)
@@ -2936,6 +2907,11 @@ JsProxy_create_with_this(JsRef object, JsRef this)
     return JsProxy_new_error(object);
   } else {
     type_flags = compute_typeflags(object);
+    if (type_flags == -1) {
+      PyErr_SetString(internal_error,
+                      "Internal error occurred in compute_typeflags");
+      return NULL;
+    }
   }
   return create_proxy_of_type(type_flags, object, this);
 }
@@ -3013,6 +2989,7 @@ JsProxy_init_docstrings()
   GetProxyDocClass(JsBuffer);
   GetProxyDocClass(JsArray);
   GetProxyDocClass(JsDoubleProxy);
+#undef GetProxyDocClass
 
   // Load the docstrings for JsProxy methods from the corresponding stubs in
   // _pyodide._core_docs.set_method_docstring uses
@@ -3070,20 +3047,25 @@ JsProxy_init(PyObject* core_module)
   FAIL_IF_MINUS_ONE(JsProxy_init_docstrings());
   FAIL_IF_MINUS_ONE(PyModule_AddFunctions(core_module, methods));
 
-  PyModule_AddIntMacro(core_module, IS_ITERABLE);
-  PyModule_AddIntMacro(core_module, IS_ITERATOR);
-  PyModule_AddIntMacro(core_module, HAS_LENGTH);
-  PyModule_AddIntMacro(core_module, HAS_GET);
-  PyModule_AddIntMacro(core_module, HAS_SET);
-  PyModule_AddIntMacro(core_module, HAS_HAS);
-  PyModule_AddIntMacro(core_module, HAS_INCLUDES);
-  PyModule_AddIntMacro(core_module, IS_AWAITABLE);
-  PyModule_AddIntMacro(core_module, IS_BUFFER);
-  PyModule_AddIntMacro(core_module, IS_CALLABLE);
-  PyModule_AddIntMacro(core_module, IS_ARRAY);
-  PyModule_AddIntMacro(core_module, IS_NODE_LIST);
-  PyModule_AddIntMacro(core_module, IS_TYPEDARRAY);
-  PyModule_AddIntMacro(core_module, IS_DOUBLE_PROXY);
+#define AddFlag(flag)                                                          \
+  FAIL_IF_MINUS_ONE(PyModule_AddIntConstant(core_module, #flag, flag))
+
+  AddFlag(IS_ITERABLE);
+  AddFlag(IS_ITERATOR);
+  AddFlag(HAS_LENGTH);
+  AddFlag(HAS_GET);
+  AddFlag(HAS_SET);
+  AddFlag(HAS_HAS);
+  AddFlag(HAS_INCLUDES);
+  AddFlag(IS_AWAITABLE);
+  AddFlag(IS_BUFFER);
+  AddFlag(IS_CALLABLE);
+  AddFlag(IS_ARRAY);
+  AddFlag(IS_NODE_LIST);
+  AddFlag(IS_TYPEDARRAY);
+  AddFlag(IS_DOUBLE_PROXY);
+
+#undef AddFlag
 
   asyncio_module = PyImport_ImportModule("asyncio");
   FAIL_IF_NULL(asyncio_module);

src/core/hiwire.c
@@ -625,10 +625,6 @@ EM_JS_BOOL(bool, hiwire_get_bool, (JsRef idobj), {
   // clang-format on
 });
 
-EM_JS_BOOL(bool, hiwire_is_pyproxy, (JsRef idobj), {
-  return API.isPyProxy(Hiwire.get_value(idobj));
-});
-
 EM_JS_BOOL(bool, hiwire_is_function, (JsRef idobj), {
   // clang-format off
   return typeof Hiwire.get_value(idobj) === 'function';
@@ -690,13 +686,6 @@ MAKE_OPERATOR(not_equal, !==);
 MAKE_OPERATOR(greater_than, >);
 MAKE_OPERATOR(greater_than_equal, >=);
 
-EM_JS_BOOL(bool, hiwire_is_iterator, (JsRef idobj), {
-  let jsobj = Hiwire.get_value(idobj);
-  // clang-format off
-  return typeof jsobj.next === 'function';
-  // clang-format on
-});
-
 EM_JS_NUM(int, hiwire_next, (JsRef idobj, JsRef* result_ptr), {
   let jsobj = Hiwire.get_value(idobj);
   // clang-format off
@@ -705,13 +694,6 @@ EM_JS_NUM(int, hiwire_next, (JsRef idobj, JsRef* result_ptr), {
   let result_id = Hiwire.new_value(value);
   DEREF_U32(result_ptr, 0) = result_id;
   return done;
-});
-
-EM_JS_BOOL(bool, hiwire_is_iterable, (JsRef idobj), {
-  let jsobj = Hiwire.get_value(idobj);
-  // clang-format off
-  return typeof jsobj[Symbol.iterator] === 'function';
-  // clang-format on
 });
 
 EM_JS_REF(JsRef, hiwire_get_iterator, (JsRef idobj), {
@@ -748,13 +730,6 @@ EM_JS_REF(JsRef, hiwire_reversed_iterator, (JsRef idarray), {
 
   return Hiwire.new_value(new Module._reversedIterator(array));
 })
-
-EM_JS_BOOL(bool, hiwire_is_typedarray, (JsRef idobj), {
-  let jsobj = Hiwire.get_value(idobj);
-  // clang-format off
-  return ArrayBuffer.isView(jsobj) || (jsobj.constructor && jsobj.constructor.name === "ArrayBuffer");
-  // clang-format on
-});
 
 EM_JS_NUM(errcode, hiwire_assign_to_ptr, (JsRef idobj, void* ptr), {
   let jsobj = Hiwire.get_value(idobj);

src/core/hiwire.h
@@ -309,12 +309,6 @@ bool
 hiwire_get_bool(JsRef idobj);
 
 /**
- * Check whether the object is a PyProxy.
- */
-bool
-hiwire_is_pyproxy(JsRef idobj);
-
-/**
  * Check if the object is a function.
  */
 bool
@@ -407,12 +401,6 @@ bool
 hiwire_greater_than_equal(JsRef ida, JsRef idb);
 
 /**
- * Check if `typeof obj.next === "function"`
- */
-bool
-hiwire_is_iterator(JsRef idobj);
-
-/**
  * Calls the `next` function on an iterator.
  *
  * Returns -1 if an error occurs. Otherwise, `next` should return an object with
@@ -421,12 +409,6 @@ hiwire_is_iterator(JsRef idobj);
  */
 int
 hiwire_next(JsRef idobj, JsRef* result);
-
-/**
- * Check if `typeof obj[Symbol.iterator] === "function"`
- */
-bool
-hiwire_is_iterable(JsRef idobj);
 
 /**
  * Returns the iterator associated with the given object, if any.
@@ -439,12 +421,6 @@ hiwire_get_iterator(JsRef idobj);
  */
 JsRef
 hiwire_reversed_iterator(JsRef idobj);
-
-/**
- * Returns 1 if the value is a typedarray.
- */
-bool
-hiwire_is_typedarray(JsRef idobj);
 
 /**
  * Copies the buffer contents of a given ArrayBuffer view or ArrayBuffer into

src/core/js2python.js
@@ -115,6 +115,10 @@ JS_FILE(js2python_init, () => {
     } else if (value === false) {
       return __js2python_false();
     } else if (API.isPyProxy(value)) {
+      if (value.$$.ptr == 0) {
+        // Make sure to throw an error!
+        Module.PyProxy_getPtr(value);
+      }
       if (value.$$props.roundtrip) {
         if (id === undefined) {
           id = Hiwire.new_value(value);

src/core/python2js.c
@@ -786,7 +786,7 @@ to_js(PyObject* self,
   js_result = python2js_custom(
     obj, depth, proxies, js_dict_converter, js_default_converter);
   FAIL_IF_NULL(js_result);
-  if (hiwire_is_pyproxy(js_result)) {
+  if (pyproxy_Check(js_result)) {
     // Oops, just created a PyProxy. Wrap it I guess?
     py_result = JsProxy_create(js_result);
   } else {

src/tests/test_pyodide.py
@@ -681,25 +681,18 @@ def test_create_proxy_roundtrip(selenium):
     run_js("(o) => { o.f.destroy(); }")(o)
 
 
+@run_in_pyodide
 def test_return_destroyed_value(selenium):
-    selenium.run_js(
-        r"""
-        self.f = function(x){ return x };
-        pyodide.runPython(`
-            from pyodide.ffi import create_proxy, JsException
-            from js import f
-            p = create_proxy([])
-            p.destroy()
-            try:
-                f(p)
-            except JsException as e:
-                assert str(e) == (
-                    "Error: Object has already been destroyed\\n"
-                    'The object was of type "list" and had repr "[]"'
-                )
-        `);
-        """
-    )
+    import pytest
+
+    from pyodide.code import run_js
+    from pyodide.ffi import JsException, create_proxy
+
+    f = run_js("(function(x){ return x; })")
+    p = create_proxy([])
+    p.destroy()
+    with pytest.raises(JsException, match='The object was of type "list" and had repr'):
+        f(p)
 
 
 def test_docstrings_a():

commit 900a7aca2f6950125fef0a741c15a8da10ff2d3e

Make SET_FLAG_IF more paranoid
This fixes a regression introduced in #3283.

The bug also depends on this branch here:
https://github.com/pyodide/pyodide/blob/main/src/core/python2js.c#L833-L834
which is a bit suspect...

src/core/jsproxy.c
@@ -3982,8 +3982,11 @@ finally:
 }
 
 #define SET_FLAG_IF(flag, cond)                                                \
-  if (cond) {                                                                  \
-    type_flags |= flag;                                                        \
+  try {                                                                        \
+    if (cond) {                                                                \
+      type_flags |= flag;                                                      \
+    }                                                                          \
+  } catch (e) {                                                                \
   }
 
 EM_JS_NUM(int, JsProxy_compute_typeflags, (JsRef idobj), {

src/tests/test_typeconversions.py
@@ -411,6 +411,21 @@ def test_hyp_tojs_no_crash(selenium, obj):
         del __main__.x
 
 
+@pytest.mark.skip_refcount_check
+@pytest.mark.skip_pyproxy_check
+@given(obj=any_strategy)
+@example(obj=range(0, 2147483648))  # length is too big to fit in ssize_t
+@settings(
+    std_hypothesis_settings,
+    max_examples=25,
+)
+@run_in_pyodide
+def test_hypothesis(selenium_standalone, obj):
+    from pyodide.ffi import to_js
+
+    to_js(obj)
+
+
 @pytest.mark.parametrize(
     "py,js",
     [