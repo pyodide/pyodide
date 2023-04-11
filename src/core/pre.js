77c9ded2c18aad967bce3eb02cd381b9fa14d589
Hood Chatham <roberthoodchatham@gmail.com>
2023-04-11 09:15:55 -0700

    More improvements to JsProxy_compute_typeflags (#3750)
    This is a followup to #3740 which rearranges the logic a bit more on further consideration.
    
    I found out that if you create a revocable Proxy and then revoke it, a huge number of browser
    internal operations raise TypeErrors. Such an object is thus an excellent stress test for our
    handling of explosive objects. Overall we do quite well, `isPyProxy` and `compute_type_flags`
    are actually the only spots where we run into trouble (and `PyProxy_Check` is the only spot
    that is raising a fatal error).
    
    This changes it so that an object with a `length` getter that throws an error or returns
    not an integer will have a `__len__` in Python that always throws an error. It can't check
    the type anymore because it avoids invoking getters so if the result is a return value from
    a getter it won't know the type. The same is true for the `Error.name`, `Error.message`, and
    `Error.stack` fields.
    
    To check for methods, I still use `typeof x[method] === "function"`, invoking the getter.
    Usually functions don't have getters anyways and I think it is weirder to have false positives
    in this case. If there is a getter and it raises an error, we catch it and throw it away.

--- src/core/pre.js
+++ src/core/pre.js
@@ -4,5 +4,49 @@ const Tests = {};
 API.tests = Tests;
 API.version = "0.24.0.dev0";
 Module.hiwire = Hiwire;
-const getTypeTag = (x) => Object.prototype.toString.call(x);
+function getTypeTag(x) {
+  try {
+    return Object.prototype.toString.call(x);
+  } catch (e) {
+    return "";
+  }
+}
 API.getTypeTag = getTypeTag;
+
+/**
+ * Safe property check
+ *
+ * Observe whether a property exists or not without invoking getters.
+ * Should never throw as long as arguments have the correct types.
+ *
+ * obj: an object
+ * prop: a string or symbol
+ */
+function hasProperty(obj, prop) {
+  try {
+    while (obj) {
+      if (Object.getOwnPropertyDescriptor(obj, prop)) {
+        return true;
+      }
+      obj = Object.getPrototypeOf(obj);
+    }
+  } catch (e) {}
+  return false;
+}
+
+/**
+ * Observe whether a method exists or not
+ *
+ * Invokes getters but catches any error produced by a getter and throws it away.
+ * Never throws an error
+ *
+ * obj: an object
+ * prop: a string or symbol
+ */
+function hasMethod(obj, prop) {
+  try {
+    return typeof obj[prop] === "function";
+  } catch (e) {
+    return false;
+  }
+}
