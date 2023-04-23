dbc0dc7bd70bd176599132a3beb31a06fbf12661
Bart Broere <mail@bartbroere.eu>
2023-04-23 13:32:47 +0200

    Add regression test for xyzservices

---
+++ packages/xyzservices/test_xyzservices.py
@@ -0,0 +1,15 @@
+def test_xyzservices(selenium):
+    """
+    Check whether any errors occur by testing basic functionality.
+    Intended to function as a regression test.
+    Might fail if xyzservices is upgraded and the data
+    or API changes.
+    """
+    selenium.load_package("xyzservices")
+    selenium.run(
+        """
+        import xyzservices.providers
+        # assert the module produces something for this
+        assert xyzservices.providers.CartoDB.Positron.url
+        """
+    )
