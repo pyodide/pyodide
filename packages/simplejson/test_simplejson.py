16a24695be434331c03a2ae36bd1b7a52b81f5eb
Bart Broere <mail@bartbroere.eu>
2023-04-28 11:00:22 +0000

    Add test

---
+++ packages/simplejson/test_simplejson.py
@@ -0,0 +1,14 @@
+from pytest_pyodide.decorator import run_in_pyodide
+
+
+@run_in_pyodide(packages=["simplejson"])
+def test_simplejson(selenium):
+    import simplejson
+
+    # test whether the basic functionality works
+    dumped = simplejson.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True)
+    expected = '{"a": 0, "b": 0, "c": 0}'
+    assert dumped == expected
+
+    # test whether C extensions have been built succesfully
+    assert simplejson._import_c_make_encoder() is not None
