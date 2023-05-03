69bbc529456b181fa5242f8eee007eb877a9a9a1
Hood Chatham <roberthoodchatham@gmail.com>
2023-05-02 20:52:38 -0700

    Version 0.23.2

--- src/core/pre.js
+++ src/core/pre.js
@@ -2,7 +2,7 @@ const API = Module.API;
 const Hiwire = {};
 const Tests = {};
 API.tests = Tests;
-API.version = "0.23.1";
+API.version = "0.23.2";
 Module.hiwire = Hiwire;
 const getTypeTag = (x) => Object.prototype.toString.call(x);
 API.getTypeTag = getTypeTag;
