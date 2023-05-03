f8d6479855a3acb53a13c723bf4d7319c2eaea44
Bart Broere <mail@bartbroere.eu>
2023-05-03 08:07:16 +0000

    Check for binary component

--- packages/protobuf/test_protobuf.py
+++ packages/protobuf/test_protobuf.py
@@ -5,7 +5,11 @@ from pytest_pyodide.decorator import run_in_pyodide
 @run_in_pyodide(packages=["protobuf"])
 def test_protobuf(selenium):
     from google.protobuf.descriptor_pb2 import FieldDescriptorProto
+    from google.protobuf.internal import api_implementation
     from google.protobuf.proto_builder import MakeSimpleProtoClass
+
+    _message = api_implementation._c_module
+    assert _message is not None  # check presence of binary component
 
     MakeSimpleProtoClass(
         {
