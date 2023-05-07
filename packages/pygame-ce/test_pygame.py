c66cb4c9e00ab39f501964d4c2a7a4398913d05a
ryanking13 <def6488@gmail.com>
2023-05-07 07:20:06 +0000

    Update build script

--- packages/pygame-ce/test_pygame.py
+++ packages/pygame-ce/test_pygame.py
@@ -1,15 +1,39 @@
+import pytest
 from pytest_pyodide import run_in_pyodide
 
 
+@pytest.fixture(scope="function")
+def selenium_sdl(selenium_standalone):
+    if selenium_standalone.browser == "node":
+        pytest.skip("No document object")
+
+    selenium_standalone.run_js(
+        """
+        var sdl2Canvas = document.createElement("canvas");
+        sdl2Canvas.id = "canvas";
+
+        document.body.appendChild(sdl2Canvas);
+        // Temporary workaround for pyodide#3697
+        pyodide._api._skip_unwind_fatal_error = true;
+        pyodide.canvas.setCanvas2D(sdl2Canvas);
+        """
+    )
+    yield selenium_standalone
+
+
+@pytest.mark.skip_refcount_check
+@pytest.mark.skip_pyproxy_check
 @run_in_pyodide(packages=["pygame-ce"])
-def test_idle(selenium):
+def test_idle(selenium_sdl):
     import pygame
 
     print(pygame.__version__)
 
 
+@pytest.mark.skip_refcount_check
+@pytest.mark.skip_pyproxy_check
 @run_in_pyodide(packages=["pygame-ce"])
-def test_example(selenium):
-    import pygame.examples.aliens
+def test_example(selenium_sdl):
+    import pygame.examples.stars
 
-    pygame.examples.aliens.main()
+    pygame.examples.stars.main()
