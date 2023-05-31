4082e38b6b840fd3ac17e758baea1d90cc103cf7
Hood Chatham <roberthoodchatham@gmail.com>
2023-05-31 09:20:53 -0700

    Monkeypatch JsException

--- conftest.py
+++ conftest.py
@@ -15,6 +15,9 @@ sys.path.append(str(ROOT_PATH / "src" / "py"))
 
 import pytest_pyodide.runner
 from pytest_pyodide.utils import package_is_built as _package_is_built
+import pytest_pyodide.pyodide
+from pyodide.ffi import JsException
+pytest_pyodide.decorator.JsException = JsException
 
 # There are a bunch of global objects that occasionally enter the hiwire cache
 # but never leave. The refcount checks get angry about them if they aren't preloaded.
