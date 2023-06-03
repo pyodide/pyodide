a8a0df05534ce151e7b0328d7d7846ddfea1d007
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2023-06-03 22:38:25 +0200

    added test file

---
+++ packages/peewee/test_peewee.py
@@ -0,0 +1,39 @@
+from pytest_pyodide import run_in_pyodide
+
+import os
+from peewee import *
+
+@run_in_pyodide(packages=["peewee"])
+def test_peewee(selenium):
+    database = SqliteDatabase(os.path.join('/tmp', 'database.db'))
+
+    # Define a model class
+    class Person(Model):
+        name = CharField()
+        age = IntegerField()
+
+        class Meta:
+            database = database
+
+    # Connect to the database, create tables, and bind the model
+    with database:
+        database.create_tables([Person])
+        
+        # Create a new person
+        person = Person.create(name="John Doe", age=25)
+        
+        # Retrieve all people from the database
+        people = Person.select()
+        
+        # Verify that the person was created and retrieved
+        assert person in people
+        
+        # Update a person's age
+        person.age = 30
+        person.save()
+        
+        # Delete a person
+        person.delete_instance()
+        
+        # Verify that the person was deleted
+        assert person not in Person.select()
