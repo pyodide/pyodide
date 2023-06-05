b9c47c4b8a2928958a5194a4ef5c8f077ee0afdc
Hood Chatham <roberthoodchatham@gmail.com>
2023-06-05 16:47:27 -0700

    Add test for pyo3 catching rust panics

---
+++ packages/rust-panic-test/src/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+from setuptools_rust import Binding,RustExtension
+
+setup(
+    name="rust_test",
+    version="1.0",
+    rust_extensions=[
+        RustExtension(
+            "rust_test",
+            "Cargo.toml",
+            binding=Binding.PyO3
+        )
+    ],
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
+)
