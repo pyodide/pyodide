ff68bc722b36c80d7605aad95bab923b5d0962ba
Juniper Tyree <juniper.tyree@helsinki.fi>
2023-06-06 08:12:48 +0000

    Add Cartopy package

---
+++ packages/Cartopy/test_cartopy.py
@@ -0,0 +1,23 @@
+import pathlib
+
+import pytest
+from pytest_pyodide import run_in_pyodide
+
+
+@pytest.mark.driver_timeout(60)
+@run_in_pyodide(packages=["cartopy"])
+def test_imports(selenium):
+    import cartopy
+    import cartopy.trace
+
+
+@pytest.mark.driver_timeout(60)
+@run_in_pyodide(packages=["cartopy", "matplotlib"])
+def test_matplotlib(selenium):
+    import cartopy.crs as ccrs
+    import matplotlib.pyplot as plt
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.coastlines()
+
+    plt.show()
