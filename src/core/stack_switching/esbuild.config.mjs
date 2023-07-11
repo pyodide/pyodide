e5cad66dc150fd0a46a058326d379388462a1aa1
Hood Chatham <roberthoodchatham@gmail.com>
2023-07-10 20:28:02 -0700

    More changes from next

--- src/core/stack_switching/esbuild.config.mjs
+++ src/core/stack_switching/esbuild.config.mjs
@@ -1,5 +1,6 @@
 /**
- *
+ * Bundle the stack switching folder as iife and then export names exported from
+ * stack_switching.mjs onto Module and into the Emscripten namespace.
  */
 
 import { build } from "esbuild";
@@ -35,11 +36,11 @@ function watPlugin() {
   };
 }
 
-const outfile = join(__dirname, "continuations.out.js");
-const globalName = "Continuations";
+const outfile = join(__dirname, "stack_switching.out.js");
+const globalName = "StackSwitching";
 
 const config = {
-  entryPoints: [join(__dirname, "continuations.mjs")],
+  entryPoints: [join(__dirname, "stack_switching.mjs")],
   outfile,
   format: "iife",
   bundle: true,
@@ -48,14 +49,25 @@ const config = {
   metafile: true,
 };
 
+// First build as "esm" to get the list of exports. The metafile doesn't list
+// exports except when we set `format: "esm"`. Setting bundle: false saves a
+// tiny amount of time on this pass.
 const { metafile } = await build(
   Object.assign({}, config, { format: "esm", bundle: false }),
 );
-const exports = Object.entries(metafile.outputs)[0][1].exports;
-const footer = `
-  const {${exports}} = Continuations;
-  Object.assign(Module, Continuations);
-`.replaceAll(/\s/g, "");
 
+// The file name is the metafile.outputs key. It is relative to the current
+// working directory, so it's annoying to work it out. There will only be one
+// key in any case, so we just extract it with Object.values().
+const exports = Object.values(metafile.outputs)[0].exports;
+
+// Add a footer that destructures the exports into the Emscripten namespace.
+// Also Object.assign them onto Module.
+const footer = `
+  const {${exports}} = ${globalName};
+  Object.assign(Module, ${globalName});
+`.replaceAll(/\s/g, "");
 config.footer = { js: footer };
+
+// Build again, this time as an iife bundle with our extra footer.
 await build(config);
