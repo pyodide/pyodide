1834cd256044eee026a6e01c1d787bf3ff6b93a2
Max Halford <maxhalford25@gmail.com>
2023-10-05 12:41:42 +0200

    add driver_timeout(60)

--- packages/river/test_river.py
+++ packages/river/test_river.py
@@ -1,6 +1,8 @@
+import pytest
 from pytest_pyodide import run_in_pyodide
 
 
+@pytest.mark.driver_timeout(60)
 @run_in_pyodide(packages=["river"])
 def test_linear_regression(selenium):
     from river import datasets, evaluate, linear_model, metrics, preprocessing
