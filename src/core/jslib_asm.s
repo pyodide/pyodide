1857f41415ccad6364df00cefcd4aedaa7cd020f
Hood Chatham <roberthoodchatham@gmail.com>
2023-10-11 14:15:59 -0700

    Cleanup

--- src/core/jslib_asm.s
+++ src/core/jslib_asm.s
@@ -1,10 +1,3 @@
-# 1 "src/core/jslib_asm.S"
-# 1 "<built-in>" 1
-# 1 "<built-in>" 3
-# 347 "<built-in>" 3
-# 1 "<command line>" 1
-# 1 "<built-in>" 2
-# 1 "src/core/jslib_asm.S" 2
 .globl Jsv_is_null
 Jsv_is_null:
   .functype Jsv_is_null (externref) -> (i32)
