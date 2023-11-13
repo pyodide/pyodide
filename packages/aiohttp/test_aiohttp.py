56c94871077503fa8dfa288e7222cf7dbebec1fd
Hood Chatham <roberthoodchatham@gmail.com>
2023-11-13 13:02:00 -0800

    Add a test

---
+++ packages/aiohttp/test_aiohttp.py
@@ -0,0 +1,27 @@
+from pytest_pyodide import run_in_pyodide
+from pathlib import Path
+import pytest
+
+@run_in_pyodide(packages=["aiohttp"])
+async def aiohttp_test_helper(selenium, patch, base_url, lock_data):
+    exec(patch, {})
+    import json
+    import aiohttp
+    async with aiohttp.ClientSession() as session:
+        async with session.get(base_url + '/pyodide-lock.json') as response:
+
+            assert response.status == 200
+            assert response.headers['content-type'] == "application/json"
+
+            body = await response.json()
+            expected = json.loads(lock_data)
+            assert body == expected
+
+
+
+    
+
+def test_aiohttp(selenium):
+    patch = (Path(__file__).parent / "aiohttp_patch.py").read_text()
+    lock_data = (Path(pytest.pyodide_dist_dir) / "pyodide-lock.json").read_text()
+    aiohttp_test_helper(selenium, patch, selenium.base_url, lock_data)
