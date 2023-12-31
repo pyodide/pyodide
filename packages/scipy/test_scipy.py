b5b8c9734408faf0ccb1e99e138a680091169272
Loïc Estève <loic.esteve@ymail.com>
2023-12-31 13:11:11 +0100

    wip

--- packages/scipy/test_scipy.py
+++ packages/scipy/test_scipy.py
@@ -44,6 +44,15 @@ def test_binom_ppf(selenium):
 
 
 @pytest.mark.driver_timeout(40)
+@run_in_pyodide(packages=["scipy"])
+def test_simple(selenium):
+    import numpy as np
+    from scipy.special import powm1
+
+    powm1(np.nan, 1)
+
+
+@pytest.mark.driver_timeout(40)
 @run_in_pyodide(packages=["pytest", "scipy-tests"])
 def test_scipy_pytest(selenium):
     import pytest
@@ -59,16 +68,89 @@ def test_scipy_pytest(selenium):
                 filter,
             ]
         )
-
     runtest("odr", "explicit")
     runtest("signal.tests.test_ltisys", "TestImpulse2")
     runtest("stats.tests.test_multivariate", "haar")
     runtest("sparse.linalg._eigen", "test_svds_parameter_k_which")
-    runtest("fft.tests.test_multithreading", "test_threaded_same and 2-fft2")
-    runtest("optimize.tests.test_zeros", "gh3089_8394")
-    runtest("sparse.linalg._dsolve.tests", "spilu_nnz0")
-    runtest("special.tests.test_cython_special", "test_cython_api and erfinv")
-    runtest("stats.tests.test_continuous_basic", "studentized_range-args96-isf")
+    # runtest("fft.tests.test_multithreading", "test_threaded_same and 2-fft2")
+    # runtest("optimize.tests.test_zeros", "gh3089_8394")
+    # runtest("sparse.linalg._dsolve.tests", "spilu_nnz0")
+    # runtest("special.tests.test_cython_special", "test_cython_api and erfinv")
+    # runtest("stats.tests.test_continuous_basic", "studentized_range-args96-isf")
+
+@pytest.mark.driver_timeout(10)
+@pytest.mark.parametrize("module,filter", [
+    ("odr", "explicit"),
+    ("signal.tests.test_ltisys", "TestImpulse2"),
+    ("stats.tests.test_multivariate", "haar"),
+    ("sparse.linalg._eigen", "test_svds_parameter_k_which"),
+    ("fft.tests.test_multithreading", "test_threaded_same and 2-fft2"),
+    ("optimize.tests.test_zeros", "gh3089_8394"),
+    ("sparse.linalg._dsolve.tests", "spilu_nnz0"),
+    ("special.tests.test_cython_special", "test_cython_api and erfinv"),
+    ("stats.tests.test_continuous_basic", "studentized_range-args96-isf"),])
+@run_in_pyodide(packages=["pytest", "scipy-tests"])
+def test_scipy_pytest_parametrized(selenium, module, filter):
+    import pytest
+
+    def runtest(module, filter):
+        pytest.main(
+            [
+                "--pyargs",
+                f"scipy.{module}",
+                "--continue-on-collection-errors",
+                "-vv",
+                "-k",
+                filter,
+            ]
+        )
+
+    runtest(module, filter)
+
+
+@pytest.mark.driver_timeout(10)
+@run_in_pyodide(packages=["scipy-tests"])
+def test_mine(selenium):
+    from scipy.sparse import csc_matrix
+    from scipy.sparse.linalg._dsolve import spilu
+
+    A = csc_matrix((5,5), dtype='d')
+    spilu(A)
+# @pytest.mark.driver_timeout(40)
+# @run_in_pyodide(packages=["pytest", "scipy-tests"])
+# def test_scipy_pytest_fft(selenium):
+#     import pytest
+
+#     runtest("fft.tests.test_multithreading", "test_threaded_same and 2-fft2")
+
+
+# @pytest.mark.driver_timeout(40)
+# @run_in_pyodide(packages=["pytest", "scipy-tests"])
+# def test_scipy_pytest_optimize(selenium):
+#     import pytest
+
+#     runtest("optimize.tests.test_zeros", "gh3089_8394")
+
+# @pytest.mark.driver_timeout(40)
+# @run_in_pyodide(packages=["pytest", "scipy-tests"])
+# def test_scipy_pytest_linalg(selenium):
+#     import pytest
+
+#     runtest("sparse.linalg._dsolve.tests", "spilu_nnz0")
+
+# @pytest.mark.driver_timeout(40)
+# @run_in_pyodide(packages=["pytest", "scipy-tests"])
+# def test_scipy_pytest_special(selenium):
+#     import pytest
+
+#     runtest("special.tests.test_cython_special", "test_cython_api and erfinv")
+
+# @pytest.mark.driver_timeout(40)
+# @run_in_pyodide(packages=["pytest", "scipy-tests"])
+# def test_scipy_pytest_stats(selenium):
+#     import pytest
+
+#     runtest("stats.tests.test_continuous_basic", "studentized_range-args96-isf")
 
 
 @pytest.mark.driver_timeout(40)
