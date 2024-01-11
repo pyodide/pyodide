38ab5b30f94e341b860061e448d9f37658c68c9a
Gyeongjae Choi <def6488@gmail.com>
2024-01-11 16:12:18 +0000

    Add cpython patch

--- cpython/pyconfig.defs.h
+++
@@ -1,7 +0,0 @@
-
-/* FIXME: LONG_BIT is miscalculated to 64 for some reason starting from Emscripten 3.1.50 */
-/* https://github.com/emscripten-core/emscripten/pull/20752 this PR is probably related, */
-/* but it is not clear why it calculates LONG_BIT incorrectly */
-
-#undef LONG_BIT
-#define LONG_BIT 32
