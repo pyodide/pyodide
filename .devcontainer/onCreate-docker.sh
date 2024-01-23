c99f1ace0d64511448bc47c2fd58100317ec07fe
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-01-22 23:04:52 -0800

    .devcontainer/onCreate-*.sh: Set PYODIDE_RECIPE_BUILD_DIR in bashrc

--- .devcontainer/onCreate-docker.sh
+++ .devcontainer/onCreate-docker.sh
@@ -6,6 +6,8 @@ set -e
 # https://pyodide.org/en/stable/development/new-packages.html#prerequisites
 pip install -e ./pyodide-build
 
+echo "export PYODIDE_RECIPE_BUILD_DIR=/tmp/pyodide-build" >> ~/.bashrc
+
 # Building emsdk and cpython takes a few minutes to run, so we do not run it here.
 #
 # make -C emsdk
