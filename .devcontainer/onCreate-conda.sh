e582c3766c2ffd57c027221f46e99d5606a368b7
Matthias Köppe <mkoeppe@math.ucdavis.edu>
2024-01-26 18:02:22 -0800

    dev containers: Create a convenience symlink to the package build directory (#4425)

--- .devcontainer/onCreate-conda.sh
+++ .devcontainer/onCreate-conda.sh
@@ -18,7 +18,11 @@ echo "conda activate pyodide-env" >> ~/.bashrc
 # https://pyodide.org/en/stable/development/building-from-sources.html#using-docker
 export EMSDK_NUM_CORE=12 EMCC_CORES=12 PYODIDE_JOBS=12
 echo "export EMSDK_NUM_CORE=12 EMCC_CORES=12 PYODIDE_JOBS=12" >> ~/.bashrc
-echo "export PYODIDE_RECIPE_BUILD_DIR=/tmp/pyodide-build" >> ~/.bashrc
+
+export PYODIDE_RECIPE_BUILD_DIR=/tmp/pyodide-build
+mkdir -p "$PYODIDE_RECIPE_BUILD_DIR"
+echo "export PYODIDE_RECIPE_BUILD_DIR=$PYODIDE_RECIPE_BUILD_DIR" >> ~/.bashrc
+ln -sf "$PYODIDE_RECIPE_BUILD_DIR" packages/.build || echo "Note: Could not create convenience symlink packages/.build"
 
 conda run -n pyodide-env --live-stream pip install -r requirements.txt
 
--- .devcontainer/onCreate-docker.sh
+++ .devcontainer/onCreate-docker.sh
@@ -6,7 +6,10 @@ set -e
 # https://pyodide.org/en/stable/development/new-packages.html#prerequisites
 pip install -e ./pyodide-build
 
-echo "export PYODIDE_RECIPE_BUILD_DIR=/tmp/pyodide-build" >> ~/.bashrc
+export PYODIDE_RECIPE_BUILD_DIR=/tmp/pyodide-build
+mkdir -p "$PYODIDE_RECIPE_BUILD_DIR"
+echo "export PYODIDE_RECIPE_BUILD_DIR=$PYODIDE_RECIPE_BUILD_DIR" >> ~/.bashrc
+ln -sf "$PYODIDE_RECIPE_BUILD_DIR" packages/.build || echo "Note: Could not create convenience symlink packages/.build"
 
 # Building emsdk and cpython takes a few minutes to run, so we do not run it here.
 #
