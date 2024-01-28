8fcfc9681e25b616550b565d7a4ec2a5e6d2a9f5
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-01-27 16:30:05 -0800

    packages/sagemath*: New

---
+++ packages/sagemath-categories/test_sagemath_categories.py
@@ -0,0 +1,18 @@
+import pytest
+from pytest_pyodide import run_in_pyodide
+
+
+@run_in_pyodide(packages=["sagemath-categories"])
+def test_sagemath_categories(selenium):
+    import sage.all__sagemath_categories
+
+
+@run_in_pyodide(packages=["sagemath-categories", "sagemath-repl"])
+def test_sagemath_categories_doctests(selenium):
+    from sage.doctest.control import DocTestController, DocTestDefaults
+    args = DocTestDefaults()
+    args.stats_path = None
+    args.installed = True
+    DC = DocTestController(args, [])
+    err = DC.run()
+    assert err == 0
