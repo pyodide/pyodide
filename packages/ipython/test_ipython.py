d93b18b77699fdd275b82600cf15c2fd10f500f5
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-01-30 11:36:59 -0800

    packages/ipython/test_ipython.py: New

---
+++ packages/ipython/test_ipython.py
@@ -0,0 +1,7 @@
+from pytest_pyodide import run_in_pyodide
+
+
+@run_in_pyodide(packages=["ipython"])
+def test_ipython(selenium):
+    from IPython.lib import pretty
+    assert pretty.pretty(1) == "1"
