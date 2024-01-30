32f0b52a0c1e3747440508ea5cb46b30986e9bf4
David Contreras <dvd.cnt@gmail.com>
2024-01-30 17:52:23 -0600

    Fixed Fatal error with CoolProp

--- packages/coolprop/test_coolprop.py
+++ packages/coolprop/test_coolprop.py
@@ -4,12 +4,24 @@ from pytest_pyodide import run_in_pyodide
 @run_in_pyodide(packages=["coolprop"])
 def test_simple_propssi(selenium):
     from CoolProp.CoolProp import PropsSI
+    import pytest
 
     assert round(PropsSI("T", "P", 101325, "Q", 0, "Water"), 3) == 373.124
-
+    
+    with pytest.raises(ValueError):
+        PropsSI("T", "P", 101325, "Q", 0, "Walter")
 
 @run_in_pyodide(packages=["coolprop"])
 def test_simple_phasesi(selenium):
     from CoolProp.CoolProp import PhaseSI
-
     assert PhaseSI("P", 101325, "Q", 0, "Water") == "twophase"
+
+@run_in_pyodide(packages=["coolprop"])
+def test_simple_HAPropsSI(selenium):
+    from CoolProp.HumidAirProp import HAPropsSI
+    import pytest
+
+    assert round(HAPropsSI('H','T',298.15,'P',101325,'R',0.5), 3) == 50423.450
+    
+    with pytest.raises(ValueError):
+        HAPropsSI('H','T',298.15,'P',-101325,'R',0.5)
