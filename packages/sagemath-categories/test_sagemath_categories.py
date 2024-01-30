560f8e813a00e6be80c8659afea4bf1834fb99d0
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-01-30 11:14:06 -0800

    packages/sagemath-*: Update

--- packages/sagemath-categories/test_sagemath_categories.py
+++ packages/sagemath-categories/test_sagemath_categories.py
@@ -6,13 +6,15 @@ def test_sagemath_categories(selenium):
     pass
 
 
+@pytest.mark.driver_timeout(3600)
 @run_in_pyodide(packages=["sagemath-categories", "sagemath-repl"])
 def test_sagemath_categories_doctests(selenium):
     from sage.doctest.control import DocTestController, DocTestDefaults
 
     args = DocTestDefaults()
-    args.stats_path = None
     args.installed = True
+    args.environment = "sage.all__sagemath_categories"
+    args.global_iterations = 1
     DC = DocTestController(args, [])
     err = DC.run()
     assert err == 0
