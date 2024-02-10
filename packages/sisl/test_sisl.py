d5067db9b2576fbbc561e896dcd8e719f1fed383
Hood Chatham <roberthoodchatham@gmail.com>
2024-02-10 10:53:59 -0800

    Remove sisl test_version

--- packages/sisl/test_sisl.py
+++ packages/sisl/test_sisl.py
@@ -2,14 +2,6 @@ import pytest
 from pytest_pyodide import run_in_pyodide
 
 
-@pytest.mark.driver_timeout(40)
-@run_in_pyodide(packages=["sisl-tests", "pytest"])
-def test_version(selenium):
-    import sisl
-
-    assert sisl.__version__ == "0.14.2"
-
-
 @run_in_pyodide(packages=["sisl-tests", "pytest"])
 def test_nodes(selenium):
     import pytest
