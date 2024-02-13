eedd9763c15c26d572d53403b4fc04e361b8a31f
Hood Chatham <roberthoodchatham@gmail.com>
2024-02-13 13:18:42 -0800

    Fix check_editable_pyodide_build

--- tools/check_editable_pyodide_build.py
+++ tools/check_editable_pyodide_build.py
@@ -2,12 +2,17 @@
 import sys
 from pathlib import Path
 
-try:
-    import pyodide_build
-except ImportError:
-    sys.exit(1)
+def main():
+    try:
+        import pyodide_build
+    except ImportError:
+        sys.exit(1)
 
-install_path = Path(pyodide_build.__path__[0])
-editable_install_path = Path(__file__).parents[1] / "pyodide-build/pyodide_build"
+    install_path = Path(pyodide_build.__path__[0])
+    editable_install_path = Path(__file__).parents[1] / "pyodide-build/pyodide_build"
 
-sys.exit(install_path != editable_install_path)
+    sys.exit(install_path != editable_install_path)
+
+
+if __name__ == "__main__":
+    main()
