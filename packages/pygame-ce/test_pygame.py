b2444fcd024b3c273e82b7f8cee8624ac1100ce7
Gyeongjae Choi <def6488@gmail.com>
2024-02-15 13:10:06 +0000

    Add test

--- packages/pygame-ce/test_pygame.py
+++ packages/pygame-ce/test_pygame.py
@@ -13,17 +13,55 @@ def selenium_sdl(selenium_standalone):
         sdl2Canvas.id = "canvas";
 
         document.body.appendChild(sdl2Canvas);
-        // Temporary workaround for pyodide#3697
-        pyodide._api._skip_unwind_fatal_error = true;
         pyodide.canvas.setCanvas2D(sdl2Canvas);
         """
     )
     yield selenium_standalone
 
 
-@pytest.mark.skip_refcount_check
-@pytest.mark.skip_pyproxy_check
 @run_in_pyodide(packages=["pygame-ce"])
 def test_init(selenium_sdl):
     import pygame.display
     pygame.display.init()
+
+
+def test_basic_display(selenium_sdl):
+    @run_in_pyodide(packages=["pygame-ce"])
+
+    async def run(selenium):
+        import pygame
+        import pygame.display
+
+        screen = pygame.display.set_mode([100, 100])
+        pygame.display.set_caption("Caption")
+
+        # black background
+        screen.fill((0, 0, 0))
+
+        # red rectangle
+        pygame.draw.rect(screen, (255, 0, 0), [30, 30, 3, 3])
+
+        # blue circle
+        pygame.draw.circle(screen, (0, 0, 255), [50, 50], 5)
+
+        pygame.display.flip()
+
+    run(selenium_sdl)
+
+    selenium_sdl.run_js(
+        """
+        canvas = document.getElementById("canvas");
+        context = canvas.getContext("2d");
+        blackPixel = context.getImageData(0, 0, 1, 1).data
+
+        assert(() => blackPixel[0] === 0)
+        assert(() => blackPixel[1] === 0)
+        assert(() => blackPixel[2] === 0)
+
+        bluePixel = context.getImageData(50, 50, 1, 1).data
+
+        assert(() => bluePixel[0] === 0)
+        assert(() => bluePixel[1] === 0)
+        assert(() => bluePixel[2] === 255)
+        """
+    )
