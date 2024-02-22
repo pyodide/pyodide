1add5226d473e882c5971487f625dc2f1e8a5861
Hood Chatham <roberthoodchatham@gmail.com>
2024-02-21 20:54:24 -0800

    Add test and fix mypy-tests

--- packages/httpx/httpx_patch.py
+++ packages/httpx/httpx_patch.py
@@ -6,7 +6,7 @@ from httpx._transports.default import AsyncResponseStream
 from httpx._types import AsyncByteStream
 from httpx._utils import Timer
 
-from js import Headers as js_Headers
+from js import Headers as js_Headers  # type:ignore[attr-defined]
 from js import fetch
 from pyodide.ffi import create_proxy
 
@@ -17,12 +17,13 @@ def acquire_buffer(content):
         yield None
         return
     body_px = create_proxy(content)
-    body_buf = body_px.getBuffer("u8")
+    body_buf = body_px.getBuffer("u8")  # type:ignore[attr-defined]
     try:
         yield body_buf.data
     finally:
         body_px.destroy()
         body_buf.release()
+
 
 async def js_readable_stream_iter(js_readable_stream):
     reader = js_readable_stream.getReader()
@@ -33,7 +34,7 @@ async def js_readable_stream_iter(js_readable_stream):
         yield res.value.to_bytes()
 
 
-async def _send_single_request(self, request: Request) -> Response:
+async def _send_single_request(self: AsyncClient, request: Request) -> Response:
     """
     Sends a single request, without handling any redirections.
     """
@@ -45,24 +46,24 @@ async def _send_single_request(self, request: Request) -> Response:
             "Attempted to send an sync request with an AsyncClient instance."
         )
 
-
     js_headers = js_Headers.new(request.headers.multi_items())
-    with acquire_buffer(request.content) as body:
-        js_resp = await fetch(str(request.url), method=request.method, headers=js_headers, body=body)
-
-
+    with acquire_buffer(request.content) as request_body:
+        js_resp = await fetch(  # type:ignore[call-arg]
+            str(request.url),
+            method=request.method,
+            headers=js_headers,
+            body=request_body,
+        )
+    response_body = js_resp.body  # type:ignore[attr-defined]
     response = Response(
         status_code=js_resp.status,
         headers=Headers(js_resp.headers),
-        stream=AsyncResponseStream(js_readable_stream_iter(js_resp.body)),
+        stream=AsyncResponseStream(js_readable_stream_iter(response_body)),
     )
-
 
     assert isinstance(response.stream, AsyncByteStream)
     response.request = request
-    response.stream = BoundAsyncStream(
-        response.stream, response=response, timer=timer
-    )
+    response.stream = BoundAsyncStream(response.stream, response=response, timer=timer)
     self.cookies.extract_cookies(response)
     response.default_encoding = self._default_encoding
 
@@ -76,5 +77,6 @@ async def _send_single_request(self, request: Request) -> Response:
     )
 
     return response
+
 
 AsyncClient._send_single_request = _send_single_request
