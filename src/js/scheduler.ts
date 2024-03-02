2ce68b683b574ca4bdd495bee1dde32499c8e7d3
Gyeongjae Choi <def6488@gmail.com>
2024-03-02 15:50:31 +0000

    Another approach

--- src/js/scheduler.ts
+++ src/js/scheduler.ts
@@ -1,40 +1,75 @@
+import { IN_BROWSER_MAIN_THREAD, IN_NODE, IN_BROWSER_WEB_WORKER } from './environments';
+// Implementation of zero-delay scheduler for immediate callbacks
+// Notes for future reference:
+// - This is a workaround for the throttling of setTimeout in modern browsers
+// - Promise.resolve().then() is not a good option. As it uses microtask queue unlike setTimeout which uses macrotask queue
+//   - Ref: https://github.com/YuzuJS/setImmediate/pull/56
+// - MessageChannel works for Chrome, Firefox, and Node.js but it has some issues in Safari (slow and messes up the event loop), and Deno
+//   - Ref: https://github.com/pyodide/pyodide/pull/4583
+//   - Ref: https://github.com/zloirock/core-js/issues/624
+//   - Ref: https://github.com/YuzuJS/setImmediate/issues/80
+// - References for the implementation:
+//   - https://github.com/YuzuJS/setImmediate
+//   - https://github.com/zloirock/core-js/blob/master/packages/core-js/internals/task.js
+
+
+const scheduleCallbackImmediateMessagePrefix = "sched$" + Math.random().toString(36).slice(2) + "$";
+const tasks: Record<number, () => void> = {};
+let nextTaskHandle = 0;
+
+function installPostMessageHandler() {
+    if (!IN_BROWSER_MAIN_THREAD) {
+        return;
+    }
+
+    const onGlobalMessage = (event: MessageEvent) => {
+        if (typeof event.data === "string" && event.data.indexOf(scheduleCallbackImmediateMessagePrefix) === 0) {
+            const handle = +event.data.slice(scheduleCallbackImmediateMessagePrefix.length);
+            const task = tasks[handle];
+            if (task) {
+                try {
+                    task();
+                } finally {
+                    delete tasks[handle];
+                };
+            }
+        }
+    }
+
+    globalThis.addEventListener("message", onGlobalMessage, false);
+}
+
+installPostMessageHandler();
+
+function scheduleCallbackImmediate(callback: () => void) {
+    if (IN_NODE) {
+        // node has setImmediate, let's use it
+        setImmediate(callback);
+    } else if (IN_BROWSER_MAIN_THREAD) {
+        // use postMessage
+        tasks[nextTaskHandle] = callback;
+        globalThis.postMessage(scheduleCallbackImmediateMessagePrefix + nextTaskHandle, "*");
+        nextTaskHandle++;
+    } else if (IN_BROWSER_WEB_WORKER) {
+        // use MessageChannel
+        const channel = new MessageChannel();
+        channel.port1.onmessage = () => callback();
+        channel.port2.postMessage(null);
+    } else {
+        // fallback to setTimeout
+        setTimeout(callback, 0);
+    }
+
+}
+
 /**
  * Schedule a callback. Supports both immediate and delayed callbacks.
  * @param callback The callback to be scheduled
  * @param timeout The delay in milliseconds before the callback is called
  */
-
-type MessageChannelWithCallback = MessageChannel & { _callback?: () => void };
-
-const messageChannelsQueue: MessageChannelWithCallback[] = [];
-
-function schduleCallbackImmediate(callback: () => void) {
-  let channel = messageChannelsQueue.pop();
-  if (channel === undefined) {
-    channel = new MessageChannel();
-    channel.port1.onmessage = () => {
-      try {
-        channel?._callback?.();
-      } finally {
-        messageChannelsQueue.push(channel!);
-      }
-    };
-  }
-
-  channel._callback = callback;
-  channel.port2.postMessage("");
-}
-
-
 export function scheduleCallback(callback: () => void, timeout: number = 0) {
-  // In modern browsers, setTimeout has throttling minimum delay (mostly 4ms).
-  // So to support immediate callbacks, we use MessageChannel if the delay is less than 4ms.
-  // Ref0: https://github.com/pyodide/pyodide/issues/4006
-  // Ref1: (firefox) https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
-  // Ref2: (chrome) https://developer.chrome.com/blog/timer-throttling-in-chrome-88
-
   if (timeout < 4) {
-    schduleCallbackImmediate(callback)
+    scheduleCallbackImmediate(callback);
   } else {
     setTimeout(callback, timeout);
   }
