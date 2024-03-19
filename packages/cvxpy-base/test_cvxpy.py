c91b392bd7bd7dff0b5dab929d49a6ccc1480e68
Philipp Schiele <44360364+phschiele@users.noreply.github.com>
2024-03-19 18:18:05 +0000

    require cvxpy-base in test

--- packages/cvxpy-base/test_cvxpy.py
+++ packages/cvxpy-base/test_cvxpy.py
@@ -1,7 +1,7 @@
 from pytest_pyodide import run_in_pyodide
 
 
-@run_in_pyodide(packages=["cvxpy"])
+@run_in_pyodide(packages=["cvxpy-base"])
 def test_cvxpy_clarabel(selenium):
     import cvxpy as cp
 
