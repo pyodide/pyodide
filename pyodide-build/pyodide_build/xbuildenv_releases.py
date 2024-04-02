b21dc58c924645d12a28bd16167d1f18a4138edf
Gyeongjae Choi <def6488@gmail.com>
2024-04-02 09:37:20 +0000

    wip

--- pyodide-build/pyodide_build/xbuildenv_releases.py
+++ pyodide-build/pyodide_build/xbuildenv_releases.py
@@ -1,14 +1,10 @@
+from typing import Annotated
 
+from packaging.version import Version
 import pydantic
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
+from pydantic.functional_validators import AfterValidator
 
-class CrossBuildEnvMetaSpec(BaseModel):
-    releases: dict[str, CrossBuildEnvReleasesSpec]
-
-    class Config:
-        extra = pydantic.Extra.forbid
-        title = "CrossBuildEnvMetaSpec"
-        description = "The specification for the cross-build environment metadata"
 
 
 class CrossBuildEnvReleasesSpec(BaseModel):
@@ -31,10 +27,46 @@ class CrossBuildEnvReleasesSpec(BaseModel):
         title = "CrossBuildEnvReleasesSpec"
         description = "The specification for a release of the cross-build environment"
 
-    def check_compatibility(self, python_version: str | None, emscripten_version: str | None, pyodide_build_version: str | None) -> bool:
+    @property
+    def python_version_tuple(self) -> tuple[int, int, int]:
+        v = Version(self.python_version)
+        return (v.major, v.minor, v.micro)
+
+    @property
+    def emscripten_version_tuple(self) -> tuple[int, int, int]:
+        v = Version(self.emscripten_version)
+        return (v.major, v.minor, v.micro)
+
+
+    def check_compatibility(
+            self,
+            python_version: str | None,
+            emscripten_version: str | None,
+            pyodide_build_version: str | None
+    ) -> bool:
+        """
+        Check if the release is compatible with the given params
+
+        Parameters
+        ----------
+        python_version : str | None
+            The version of the Python interpreter. If None, it is not checked
+        emscripten_version : str | None
+            The version of the Emscripten SDK. If None, it is not checked
+        pyodide_build_version : str | None
+            The version of the pyodide-build. If None, it is not checked
+        """
         if python_version is not None:
-            
             return False
         if emscripten_version is not None and self.emscripten_version != emscripten_version:
             return False
         return True
+
+
+class CrossBuildEnvMetaSpec(BaseModel):
+    releases: dict[str, CrossBuildEnvReleasesSpec]
+
+    class Config:
+        extra = pydantic.Extra.forbid
+        title = "CrossBuildEnvMetaSpec"
+        description = "The specification for the cross-build environment metadata"
