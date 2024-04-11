1e011b51ad2af428320131cf8099786b26ff6a08
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-04-11 06:28:46 -0400

    add test from previous pull request

--- packages/duckdb/test_duckdb.py
+++ packages/duckdb/test_duckdb.py
@@ -5,10 +5,11 @@ from pytest_pyodide import run_in_pyodide
 def test_duckdb(selenium):
     import duckdb
 
-    con = duckdb.connect()
-    (platform,) = con.execute("PRAGMA platform").fetchone()
-    assert platform == "wasm_eh_pyodide"
+    with duckdb.connect() as con:
+        (platform,) = con.execute("PRAGMA platform").fetchone()
+        con.execute("CREATE TEMP TABLE t (id INT, content STRING)")
+        con.execute("INSERT INTO t VALUES (42, 'hello'), (43, 'world')")
+        query_result = con.execute("SELECT rowid, * FROM t").fetchall()
 
-    (a, b) = con.execute("SELECT 1 AS a, 'a' AS b").fetchone()
-    assert a == 1
-    assert b == "a"
+    assert platform == "wasm_eh_pyodide"
+    assert query_result == [(0, 42, "hello"), (1, 43, "world")]
