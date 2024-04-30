51cdb38f6dff86f73e6a54e09e72318f2a96aa0d
Hood Chatham <roberthoodchatham@gmail.com>
2024-04-30 13:40:17 +0200

    Cleanup

--- src/templates/makesnap.mjs
+++ src/templates/makesnap.mjs
@@ -4,8 +4,5 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const py = await loadPyodide({ _makeSnapshot: true });
-py.makeMemorySnapshot();
-py.runPython("from js import Response");
 writeFileSync(__dirname + "/snapshot.bin", py.makeMemorySnapshot());
