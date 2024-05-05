46b6a768243632f0bcc8599f2956fb64e41472f5
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-05-05 02:54:38 +0000

    Fix not to use Promise chains

--- src/js/load-package.ts
+++ src/js/load-package.ts
@@ -258,12 +258,15 @@ async function downloadPackage(
   if (IN_NODE) {
     installBaseUrl = API.config.packageCacheDir;
     // Ensure that the directory exists before trying to download files into it.
-    // Call mkdir() only when the dir doesn't exist to avoid an error on read-only file systems. See https://github.com/pyodide/pyodide/issues/4736
-    await nodeFsPromisesMod.access(API.config.packageCacheDir).catch(() =>
-      nodeFsPromisesMod.mkdir(API.config.packageCacheDir, {
+    try {
+      // Check if the packageCacheDir exists
+      await nodeFsPromisesMod.access(API.config.packageCacheDir);
+    } catch {
+      // If it doesn't exist, make it. Call mkdir() here only when necessary after checking the existence to avoid an error on read-only file systems. See https://github.com/pyodide/pyodide/issues/4736
+      await nodeFsPromisesMod.mkdir(API.config.packageCacheDir, {
         recursive: true,
-      }),
-    );
+      });
+    }
   } else {
     installBaseUrl = API.config.indexURL;
   }
