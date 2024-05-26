c896bdcb30980cba72cef972f6d93c07dbca6f75
Hood Chatham <roberthoodchatham@gmail.com>
2024-05-26 13:37:10 -0700

    Add test

---
+++ pyodide-build/pyodide_build/tests/test_oot_build.py
@@ -0,0 +1,35 @@
+
+from pyodide_build.out_of_tree import build
+
+def test_non_platformed_build(tmp_path):
+    """Check that we don't accidentally attach Pyodide platform to non
+    platformed wheels.
+    """
+
+    (tmp_path / "pyproject.toml").write_text(
+        """\
+[build-system]
+build-backend = "hatchling.build"
+requires = ["hatchling<1.22"]
+
+[project]
+requires-python = ">=3.10"
+name = "fake-pkg"
+version = "1.0"
+
+[tool.hatch.build.targets.wheel]
+packages = ["fake_pkg"]
+        """
+    )
+    (tmp_path / "fake_pkg.py").write_text("print('hi from fake_pkg!')")
+    src = tmp_path
+    dst = tmp_path / "dist"
+    exports = "pyinit"
+    config_settings = {}
+    build.run(src, dst, exports, config_settings)
+
+    wheels = list(dst.glob("*.whl"))
+    assert len(wheels) == 1
+    assert wheels[0].name == "fake_pkg-1.0-py3-none-any.whl"
+
+
