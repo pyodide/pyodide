501fb3438c6819aca61fce55ee9183dbb20ffb39
Hood Chatham <roberthoodchatham@gmail.com>
2024-06-07 10:53:45 -0700

    Add test

---
+++ packages/cpp-exceptions-test2/src/cpp_exceptions_test2.cpp
@@ -0,0 +1,12 @@
+#include <stdexcept>
+#include "Python.h"
+
+extern "C" __attribute__((visibility("default"))) 
+PyObject *PyInit_cpp_exceptions_test2() { 
+    try { 
+        throw std::runtime_error("something bad?");
+    } catch (const std::exception &e) { 
+        PyErr_SetString(PyExc_ImportError, "oops");
+    } 
+    return nullptr; 
+} 
