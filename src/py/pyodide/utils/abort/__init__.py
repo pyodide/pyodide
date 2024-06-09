81fca461cd8e7e52e2eb35d1a046487d4d9e4bd7
Muspi Merol <me@promplate.dev>
2024-06-09 14:22:39 +0000

    Lazy import `AbortSignal` and `run_js`

--- src/py/pyodide/utils/abort/__init__.py
+++ src/py/pyodide/utils/abort/__init__.py
@@ -3,25 +3,29 @@ from __future__ import annotations
 from pathlib import Path
 from typing import TYPE_CHECKING
 
-from js import AbortSignal
-
-from ...code import run_js
-
 if TYPE_CHECKING:
     from collections.abc import Callable, Iterable
 
+    from js import AbortSignal
 
-_abort_signal_any: Callable[[Iterable[AbortSignal]], AbortSignal] | None = (
-    AbortSignal.any
-)
+
+_abort_signal_any: Callable[[Iterable[AbortSignal]], AbortSignal] | None = None
 
 
 def get_abort_signal_any() -> Callable[[Iterable[AbortSignal]], AbortSignal]:
+    from js import AbortSignal
+
     global _abort_signal_any
-    path = Path(__file__).parent / "polyfill.js"
-    source = path.read_text() + ";polyfillAbortSignal()"
-    _abort_signal_any = run_js(source)
-    return _abort_signal_any  # type:ignore[return-value]
+
+    if AbortSignal.any is None:
+        from ...code import run_js  # type:ignore[unreachable]
+
+        path = Path(__file__).parent / "polyfill.js"
+        source = path.read_text() + ";polyfillAbortSignal()"
+        _abort_signal_any = run_js(source)
+    else:
+        _abort_signal_any = AbortSignal.any
+    return _abort_signal_any
 
 
 def abort_signal_any(signals: Iterable[AbortSignal]) -> AbortSignal:
