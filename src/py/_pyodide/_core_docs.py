6672607d410bd37fbf20c8535c3a3867c5e67fd5
Hood Chatham <roberthoodchatham@gmail.com>
2024-07-06 20:38:18 +0200

    Add can_run_sync to test whether run_sync will work (#4913)

--- src/py/_pyodide/_core_docs.py
+++ src/py/_pyodide/_core_docs.py
@@ -1513,6 +1513,17 @@ def run_sync(x: Awaitable[T]) -> T:
     raise NotImplementedError
 
 
+def can_run_sync() -> bool:
+    """Returns ``True`` if :py:func:`run_sync` is available and ``False`` if not.
+
+    .. admonition:: Experimental
+        :class: warning
+
+        This feature is not yet stable.
+    """
+    raise NotImplementedError
+
+
 __name__ = _save_name
 del _save_name
 
@@ -1538,6 +1549,7 @@ __all__ = [
     "JsCallable",
     "JsTypedArray",
     "run_sync",
+    "can_run_sync",
     "create_once_callable",
     "create_proxy",
     "destroy_proxies",
