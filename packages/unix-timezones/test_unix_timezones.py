cedf67793f1931cd1d1e1a94da096f89346bc9d5
Joe Marshall <joe.marshall@nottingham.ac.uk>
2024-07-13 22:09:56 +0100

    moved unix-timezones to pypi

--- packages/unix-timezones/test_unix_timezones.py
+++
@@ -1,8 +0,0 @@
-from pytest_pyodide import run_in_pyodide
-
-
-@run_in_pyodide(packages=["unix-timezones"])
-def test_install_unix_timezones(selenium):
-    from pathlib import Path
-
-    assert Path("/usr/share/zoneinfo/UTC").exists()
