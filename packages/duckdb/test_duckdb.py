5975343ed366497be45010391d4477686266cc07
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-07-16 12:35:46 -0400

    chore: move tests to run using the selenium fixture

--- packages/duckdb/test_duckdb.py
+++ packages/duckdb/test_duckdb.py
@@ -1,27 +1,46 @@
+import pytest
+
+from conftest import package_is_built
 from pytest_pyodide import run_in_pyodide
 
 
 @run_in_pyodide(packages=["duckdb"])
 def test_duckdb(selenium):
-    import duckdb
+    if not package_is_built("duckdb"):
+        pytest.skip("duckdb not built")
 
-    with duckdb.connect() as con:
-        (platform,) = con.execute("PRAGMA platform").fetchone()
-        con.execute("CREATE TEMP TABLE t (id INT, content STRING)")
-        con.execute("INSERT INTO t VALUES (42, 'hello'), (43, 'world')")
-        query_result = con.execute("SELECT rowid, * FROM t").fetchall()
+    selenium.load_package(["duckdb"])
+    selenium.run(
+        """
+        import duckdb
 
-    assert platform == "wasm_eh_pyodide"
-    assert query_result == [(0, 42, "hello"), (1, 43, "world")]
+        with duckdb.connect() as con:
+            (platform,) = con.execute("PRAGMA platform").fetchone()
+            con.execute("CREATE TEMP TABLE t (id INT, content STRING)")
+            con.execute("INSERT INTO t VALUES (42, 'hello'), (43, 'world')")
+            query_result = con.execute("SELECT rowid, * FROM t").fetchall()
+
+        assert platform == "wasm_eh_pyodide"
+        assert query_result == [(0, 42, "hello"), (1, 43, "world")]
+        """
+    )
 
 
 @run_in_pyodide(packages=["duckdb", "pandas"])
 def test_duckdb_with_pandas(selenium):
-    import duckdb
-    import pandas as pd
+    if not package_is_built("pandas"):
+        pytest.skip("pandas not built")
 
-    with duckdb.connect() as con:
-        df = con.sql("SELECT UNNEST(RANGE(5)) as x").df()
+    selenium.load_package(["duckdb", "pandas"])
+    selenium.run(
+        """
+        import duckdb
+        import pandas as pd
 
-    expected = pd.DataFrame({"x": range(5)})
-    assert df.equals(expected)
+        with duckdb.connect() as con:
+            df = con.sql("SELECT UNNEST(RANGE(5)) as x").df()
+
+        expected = pd.DataFrame({"x": range(5)})
+        assert df.equals(expected)
+        """
+    )
