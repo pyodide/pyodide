351383ef98f29d149aaa86845eb3d605e48764b1
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-07-16 12:47:18 -0400

    chore: sort for reproducibility

--- packages/duckdb/test_duckdb.py
+++ packages/duckdb/test_duckdb.py
@@ -16,7 +16,7 @@ def test_duckdb(selenium):
             (platform,) = con.execute("PRAGMA platform").fetchone()
             con.execute("CREATE TEMP TABLE t (id INT, content STRING)")
             con.execute("INSERT INTO t VALUES (42, 'hello'), (43, 'world')")
-            query_result = con.execute("SELECT rowid, * FROM t").fetchall()
+            query_result = con.execute("SELECT rowid, * FROM t ORDER BY rowid").fetchall()
 
         assert platform == "wasm_eh_pyodide"
         assert query_result == [(0, 42, "hello"), (1, 43, "world")]
@@ -25,17 +25,20 @@ def test_duckdb(selenium):
 
 
 def test_duckdb_with_pandas(selenium):
-    if not package_is_built("pandas"):
-        pytest.skip("pandas not built")
+    packages = ["duckdb", "pandas"]
 
-    selenium.load_package(["duckdb", "pandas"])
+    for package in packages:
+        if not package_is_built(package):
+            pytest.skip(f"{package} not built")
+
+    selenium.load_package(packages)
     selenium.run(
         """
         import duckdb
         import pandas as pd
 
         with duckdb.connect() as con:
-            df = con.sql("SELECT UNNEST(RANGE(5)) as x").df()
+            df = con.sql("SELECT UNNEST(RANGE(5)) as x ORDER BY x").df()
 
         expected = pd.DataFrame({"x": range(5)})
         assert df.equals(expected)
