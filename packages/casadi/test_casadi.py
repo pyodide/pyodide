fe64d034fef854fa79c536ea46710cb53a41ff69
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-07-18 02:42:05 +0530

    Mention size reason for IPOPT
    Co-Authored-By: Hood Chatham <roberthoodchatham@gmail.com>

--- packages/casadi/test_casadi.py
+++ packages/casadi/test_casadi.py
@@ -26,8 +26,9 @@ def test_basic_casadi_function_eval(selenium):
 
 
 # IPOPT not available because compiled with -DWITH_IPOPT=OFF
-# by default. This can be changed by setting -DWITH_IPOPT=ON
-# in the future
+# by default for size reasons. This can be changed by setting
+# -DWITH_IPOPT=ON in the future, but it requires IPOPT to be
+# built beforehand.
 @pytest.mark.skip(reason="IPOPT not available for now")
 @run_in_pyodide(packages=["casadi", "numpy"])
 def test_simple_optimization(selenium):
