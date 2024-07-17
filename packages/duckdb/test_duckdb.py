4740072a0379e7fb280d651fe1a1249e32b3d7c5
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-07-17 07:07:05 -0400

    test: refactor test according to review

--- packages/duckdb/test_duckdb.py
+++ packages/duckdb/test_duckdb.py
@@ -1,40 +1,50 @@
 import pytest
+from pytest_pyodide import run_in_pyodide
 
 from conftest import package_is_built
 
 
-def test_duckdb(selenium):
-    if not package_is_built("duckdb"):
-        pytest.skip("duckdb not built")
+def skip_if_not_installed(packages):
+    for package in packages:
+        if not package_is_built(package):
+            pytest.skip(f"{package} not built")
 
-    selenium.load_package(["duckdb"])
-    selenium.run(
-        """
+
+def test_duckdb(selenium):
+    packages = ["duckdb"]
+
+    skip_if_not_installed(packages)
+
+    selenium.load_package(packages)
+
+    @run_in_pyodide
+    def do_test(selenium):
         import duckdb
 
         with duckdb.connect() as con:
             (platform,) = con.execute("PRAGMA platform").fetchone()
             con.execute("CREATE TEMP TABLE t (id INT, content STRING)")
             con.execute("INSERT INTO t VALUES (42, 'hello'), (43, 'world')")
-            query_result = con.execute("SELECT rowid, * FROM t ORDER BY rowid").fetchall()
+            query_result = con.execute(
+                "SELECT rowid, * FROM t ORDER BY rowid"
+            ).fetchall()
 
         assert "pyodide" in platform
         assert "wasm" in platform
         assert query_result == [(0, 42, "hello"), (1, 43, "world")]
-        """
-    )
+
+    do_test(selenium)
 
 
 def test_duckdb_with_pandas(selenium):
     packages = ["duckdb", "pandas"]
 
-    for package in packages:
-        if not package_is_built(package):
-            pytest.skip(f"{package} not built")
+    skip_if_not_installed(packages)
 
     selenium.load_package(packages)
-    selenium.run(
-        """
+
+    @run_in_pyodide
+    def do_test(selenium):
         import duckdb
         import pandas as pd
 
@@ -43,5 +53,5 @@ def test_duckdb_with_pandas(selenium):
 
         expected = pd.DataFrame({"x": range(5)})
         assert df.equals(expected)
-        """
-    )
+
+    do_test(selenium)
