75f3fd8c3faa4ed7e39d2b9ab9f23f4a99511b0a
kjmalek <kellen.malek@ubc.ca>
2024-08-06 15:34:03 -0700

    fix affine test

--- packages/rasterio/test_rasterio.py
+++ packages/rasterio/test_rasterio.py
@@ -56,10 +56,6 @@ def test_affine(selenium):
 
     x = np.linspace(-4.0, 4.0, 240)
     y = np.linspace(-3.0, 3.0, 180)
-    X, Y = np.meshgrid(x, y)
-    Z1 = np.exp(-2 * np.log(2) * ((X - 0.5) ** 2 + (Y - 0.5) ** 2) / 1**2)
-    Z2 = np.exp(-3 * np.log(2) * ((X + 0.5) ** 2 + (Y + 0.5) ** 2) / 2.5**2)
-    Z = 10.0 * (Z2 - Z1)
 
     res = (x[-1] - x[0]) / 240.0
     affine = Affine(
