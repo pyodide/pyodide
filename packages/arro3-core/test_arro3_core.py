c02e9ea2a8e7a70478da4a3b228924a560c16cee
Kyle Barron <kylebarron2@gmail.com>
2024-08-20 15:02:29 -0400

    Update packages/arro3-core/test_arro3_core.py

--- packages/arro3-core/test_arro3_core.py
+++ packages/arro3-core/test_arro3_core.py
@@ -5,4 +5,4 @@ from pytest_pyodide import run_in_pyodide
 def test_read_write_parquet(selenium):
     from arro3.core import Array, DataType
 
-    arr = Array([1, 2, 3], DataType.int64())
+    Array([1, 2, 3], DataType.int64())
