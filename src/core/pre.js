22b667019a2dce02a1f458f43af11c15270c59f6
Hood Chatham <roberthoodchatham@gmail.com>
2024-08-21 16:13:33 +0200

    Version 0.27.0a2

--- src/core/pre.js
+++ src/core/pre.js
@@ -2,7 +2,7 @@ const API = Module.API;
 const Hiwire = {};
 const Tests = {};
 API.tests = Tests;
-API.version = "0.27.0.dev0";
+API.version = "0.27.0a2";
 Module.hiwire = Hiwire;
 
 function getTypeTag(x) {
