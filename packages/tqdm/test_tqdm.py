52d030348c260074d906dd32e000d9827b0bb570
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2024-08-22 20:05:47 +0200

    updated test

--- packages/tqdm/test_tqdm.py
+++ packages/tqdm/test_tqdm.py
@@ -2,6 +2,10 @@ from pytest_pyodide import run_in_pyodide
 
 
 @run_in_pyodide(packages=["tqdm"], pytest_assert_rewrites=False)
-def test_test(selenium):
-    # this is tested in test_core_python.py instead
-    pass
+def test_tqdm(selenium):
+    import warnings
+    with warnings.catch_warnings(record=True) as w:
+        try:
+            for i in tqdm(range(100), desc="Processing"):
+                time.sleep(0.1)
+            assert len(w)==0
