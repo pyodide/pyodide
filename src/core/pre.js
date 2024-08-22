d2d426801c08bbfa7d183379264bdae0f076b748
Hood Chatham <roberthoodchatham@gmail.com>
2024-08-22 11:15:45 +0200

    Revert "Version 0.27.0a2"
    This reverts commit 22b667019a2dce02a1f458f43af11c15270c59f6.

--- src/core/pre.js
+++ src/core/pre.js
@@ -2,7 +2,7 @@ const API = Module.API;
 const Hiwire = {};
 const Tests = {};
 API.tests = Tests;
-API.version = "0.27.0a2";
+API.version = "0.27.0.dev0";
 Module.hiwire = Hiwire;
 
 function getTypeTag(x) {
