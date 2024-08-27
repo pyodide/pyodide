1135de560ba2421b7a9d7e533d70563d880b29ac
Kyle Barron <kyle@developmentseed.org>
2024-08-27 15:53:37 -0400

    Fix package

--- packages/arro3-core/test_arro3_core.py
+++ packages/arro3-core/test_arro3_core.py
@@ -1,7 +1,7 @@
 from pytest_pyodide import run_in_pyodide
 
 
-@run_in_pyodide(packages=["pyarrow", "numpy", "pandas"])
+@run_in_pyodide(packages=["arro3-core"])
 def test_read_write_parquet(selenium):
     from arro3.core import Array, DataType
 
