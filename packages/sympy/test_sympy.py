2cb346cdaf61d755a7ba541e8cb7b2b07383b49d
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-10-05 00:21:05 +0100

    sympy: add test for sympy+python-flint

--- packages/sympy/test_sympy.py
+++ packages/sympy/test_sympy.py
@@ -9,3 +9,15 @@ def test_sympy(selenium):
     c = sympy.sqrt(a**2 + b**2)
 
     assert c.subs({a: 3, b: 4}) == 5
+
+
+@run_in_pyodide(packages=["sympy", "python-flint"])
+def test_sympy_and_python_flint(selenium):
+    import sympy
+
+    from sympy.external.gmpy import GROUND_TYPES
+    assert GROUND_TYPES == 'flint'
+
+    # Use python-flint for factorisation:
+    x = sympy.symbols('x')
+    assert (x**2 - 1).factor() == (x + 1) * (x - 1)
