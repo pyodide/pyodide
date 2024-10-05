b15e8cce2dd8bbef2b5fc4f00dd256595ba5f0ec
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-10-05 11:11:02 +0100

    sympy: skip sympy+python-flint test if not built

--- packages/sympy/test_sympy.py
+++ packages/sympy/test_sympy.py
@@ -1,4 +1,11 @@
+from conftest import package_is_built
 from pytest_pyodide import run_in_pyodide
+
+
+def skip_if_not_installed(packages):
+    for package in packages:
+        if not package_is_built(package):
+            pytest.skip(f"{package} not built")
 
 
 @run_in_pyodide(packages=["sympy"])
@@ -11,13 +18,18 @@ def test_sympy(selenium):
     assert c.subs({a: 3, b: 4}) == 5
 
 
-@run_in_pyodide(packages=["sympy", "python-flint"])
 def test_sympy_and_python_flint(selenium):
-    import sympy
-    from sympy.external.gmpy import GROUND_TYPES
+    skip_if_not_installed(["sympy", "python-flint"])
 
-    assert GROUND_TYPES == "flint"
+    @run_in_pyodide(packages=["sympy", "python-flint"])
+    def run(selenium):
+        import sympy
+        from sympy.external.gmpy import GROUND_TYPES
 
-    # Use python-flint for factorisation:
-    x = sympy.symbols("x")
-    assert (x**2 - 1).factor() == (x + 1) * (x - 1)
+        assert GROUND_TYPES == "flint"
+
+        # Use python-flint for factorisation:
+        x = sympy.symbols("x")
+        assert (x**2 - 1).factor() == (x + 1) * (x - 1)
+
+    run()
