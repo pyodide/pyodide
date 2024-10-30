53bba206721d06ed0df4817bddc711da9f803cd4
Nick Altmann <nick@nickaltmann.net>
2024-10-30 23:07:06 +0000

    Add test for rateslib

---
+++ packages/rateslib/test_rateslib.py
@@ -0,0 +1,15 @@
+import pytest
+from pytest_pyodide import run_in_pyodide
+
+
+@run_in_pyodide(packages=["rateslib"])
+def test_add_tenor(selenium):
+    import rateslib as rl
+    import datetime
+
+    start_date = datetime.datetime(2024, 10, 29)
+    end_date = datetime.datetime(2024, 10, 31)
+
+    test_date = rl.add_tenor(start_date, "2b", "F", "nyc")
+
+    assert end_date == test_date
