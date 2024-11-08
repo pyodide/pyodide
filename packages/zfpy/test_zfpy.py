803796f3e59595fbffc92c6e516c562dec542c29
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-11-08 17:39:55 +0530

    Move away from random seed fixture

--- packages/zfpy/test_zfpy.py
+++ packages/zfpy/test_zfpy.py
@@ -2,13 +2,6 @@ import pytest
 from pytest_pyodide import run_in_pyodide
 
 
-@pytest.fixture(scope="module")
-def random_seed() -> None:
-    import numpy as np
-
-    np.random.RandomState(42)
-
-
 @run_in_pyodide(packages=["zfpy", "numpy"])
 def test_compression(selenium):
     import numpy as np
@@ -27,9 +20,7 @@ def test_compression_with_tolerance(selenium):
 
     my_array = np.linspace(0, 1, 1000)
     compressed_data = zfpy.compress_numpy(my_array, tolerance=1e-3)
-    print(compressed_data)
     decompressed_array = zfpy.decompress_numpy(compressed_data)
-    print(decompressed_array)
     np.testing.assert_allclose(my_array, decompressed_array, atol=1e-3)
 
 
@@ -38,24 +29,22 @@ def test_different_dimensions(selenium) -> None:
     import numpy as np
     import zfpy
 
+    np.random.seed(42)
+
     # Test arrays; from 1D to 4D
     for dimensions in range(1, 5):
         # 1. test with uniform dimensions
         shape1 = tuple([5] * dimensions)
         array = np.random.rand(*shape1).astype(np.float64)
         compressed1 = zfpy.compress_numpy(array, write_header=True)
-        print(compressed1)
         decompressed1 = zfpy.decompress_numpy(compressed1)
-        print(decompressed1)
         np.testing.assert_array_equal(decompressed1, array)
 
         # 2. test with increasing dimensions
         shape2 = tuple(range(2, 2 + dimensions))
         array = np.random.rand(*shape2).astype(np.float64)
         compressed2 = zfpy.compress_numpy(array, write_header=True)
-        print(compressed2)
         decompressed2 = zfpy.decompress_numpy(compressed2)
-        print(decompressed2)
         np.testing.assert_array_equal(decompressed2, array)
 
 
@@ -65,6 +54,8 @@ def test_different_dtypes(selenium) -> None:
     import numpy as np
     import zfpy
 
+    np.random.seed(42)
+
     shape = (5, 5)
     num_elements = np.prod(shape)
 
@@ -73,16 +64,12 @@ def test_different_dtypes(selenium) -> None:
         elements = np.random.random_sample(num_elements)
         array = np.reshape(elements, shape).astype(dtype)
         compressed1 = zfpy.compress_numpy(array, write_header=True)
-        print(compressed1)
         decompressed1 = zfpy.decompress_numpy(compressed1)
-        print(decompressed1)
         np.testing.assert_array_equal(decompressed1, array)
 
     # Test integer types
     for dtype in [np.int32, np.int64]:
         array = np.random.randint(low=-(2**30), high=2**30, size=shape, dtype=dtype)
         compressed2 = zfpy.compress_numpy(array, write_header=True)
-        print(compressed2)
         decompressed2 = zfpy.decompress_numpy(compressed2)
-        print(decompressed2)
         np.testing.assert_array_equal(decompressed2, array)
