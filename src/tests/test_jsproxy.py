f14614bc89be20ce96d5a3ede1424b10c2a2c02d
Christian Clauss <cclauss@me.com>
2024-11-08 12:37:28 +0100

    Fix for improved error messages

--- src/tests/test_jsproxy.py
+++ src/tests/test_jsproxy.py
@@ -1520,8 +1520,7 @@ def test_array_slice_assign_2(selenium):
 
     assert exc_info_1a.value.args == exc_info_1b.value.args
     assert exc_info_2a.value.args == exc_info_2b.value.args
-    assert exc_info_3a.value.args == ("must assign iterable to extended slice",)
-    assert exc_info_3b.value.args == ("can only assign an iterable",)
+    assert exc_info_3a.value.args == exc_info_3b.value.args
 
 
 @std_hypothesis_settings
