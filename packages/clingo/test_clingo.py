db362125f384336535f0fc7bf09c703fe63099d0
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2024-11-10 18:22:34 +0100

    chore: add `clingo` package

---
+++ packages/clingo/test_clingo.py
@@ -0,0 +1,27 @@
+from pytest_pyodide import run_in_pyodide
+
+
+@run_in_pyodide(packages=["clingo"])
+def test_clingo(selenium):
+    from clingo.control import Control
+    from clingo.symbol import Number
+
+    class Context:
+        def inc(self, x):
+            return Number(x.number + 1)
+
+        def seq(self, x, y):
+            return [x, y]
+
+    def on_model(m):
+        print(m)
+
+    ctl = Control()
+    ctl.add("base", [], """
+    p(@inc(10)).
+    q(@seq(1,2)).
+    """)
+
+    ctl.ground([("base", [])], context=Context())
+    solution = ctl.solve(on_model=on_model)
+    assert solution.satisfiable
