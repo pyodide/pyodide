8b7b40526a98b535ffb408b984067e95338379e8
airen1986 <45193110+airen1986@users.noreply.github.com>
2024-12-11 11:52:28 +0530

    added highspy package

---
+++ packages/highspy/test_highspy.py
@@ -0,0 +1,21 @@
+from pytest_pyodide import run_in_pyodide
+
+SAMPLE_LP = pathlib.Path(__file__).parent / "test_data" / "WhiskasModel.lp"
+
+@run_in_pyodide(packages=["apsw"])
+async def highspy_test_helper(selenium, lp_file):
+    import highspy
+
+    h = highspy.Highs()
+    h.readModel(lp_file)
+    h.run()
+
+    model_status = h.modelStatusToString(h.getModelStatus())
+    optimal_value = round(h.getInfo().objective_function_value,2)
+
+    assert model_status == "Optimal"
+    assert optimal_value == 0.97
+
+
+def test_highspy(selenium):
+    highspy_test_helper(selenium, SAMPLE_LP)
