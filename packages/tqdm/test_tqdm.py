4064f1ef4708b36c6f7ddeb103b9a3d310e61d16
Gyeongjae Choi <def6488@gmail.com>
2024-12-20 10:25:56 +0900

    Update test_tqdm.py

--- packages/tqdm/test_tqdm.py
+++ packages/tqdm/test_tqdm.py
@@ -9,6 +9,7 @@ def test_tqdm(selenium):
     import tqdm
 
     with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("error")
         for _ in tqdm.tqdm(range(10), desc="Processing"):
             time.sleep(0.1)
     assert len(w) == 0
