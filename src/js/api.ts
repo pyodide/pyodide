b266ba3745bc3e1fc1e81806192cfed4a5e0b2c5
Hood Chatham <roberthoodchatham@gmail.com>
2024-12-24 11:14:16 +0100

    Fix pyodide.d.ts bundle

--- src/js/api.ts
+++ src/js/api.ts
@@ -7,7 +7,7 @@ import { loadBinaryFile, nodeFSMod } from "./compat";
 import { version } from "./version";
 import { setStdin, setStdout, setStderr } from "./streams";
 import { scheduleCallback } from "./scheduler";
-import { TypedArray, PackageData } from "./types";
+import { TypedArray, PackageData, FS } from "./types";
 import { IN_NODE, detectEnvironment } from "./environments";
 // @ts-ignore
 import LiteralMap from "./common/literal-map";
@@ -149,7 +149,7 @@ export class PyodideAPI {
    * are available as members of ``FS.filesystems``:
    * ``IDBFS``, ``NODEFS``, ``PROXYFS``, ``WORKERFS``.
    */
-  static FS = {} as typeof Module.FS;
+  static FS = {} as FS;
   /**
    * An alias to the `Emscripten Path API
    * <https://github.com/emscripten-core/emscripten/blob/main/src/library_path.js>`_.
