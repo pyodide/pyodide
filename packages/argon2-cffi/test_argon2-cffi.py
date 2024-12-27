26b9e23a6b26817462654e6a60ea29762a2f57b1
giuseppe <capassog97@gmail.com>
2024-12-27 17:12:05 +0100

    Precommit

--- packages/argon2-cffi/test_argon2-cffi.py
+++ packages/argon2-cffi/test_argon2-cffi.py
@@ -1,8 +1,10 @@
 from pytest_pyodide import run_in_pyodide
 
-@run_in_pyodide(packages=["argon2-cffi"])
+
+@run_in_pyodide(packages=["argon2_cffi"])
 def test_argon2_cffi(selenium):
     import argon2
+
     ph = argon2.PasswordHasher(parallelism=1)
     hash = ph.hash("test")
 
