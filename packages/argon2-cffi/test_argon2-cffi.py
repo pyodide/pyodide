9f5af2726e9a38cccdb5d2474b33f071aee5b7e0
giuseppe <capassog97@gmail.com>
2024-12-27 16:54:25 +0100

    Added argon2-cff packages

---
+++ packages/argon2-cffi/test_argon2-cffi.py
@@ -0,0 +1,9 @@
+from pytest_pyodide import run_in_pyodide
+
+@run_in_pyodide(packages=["argon2-cffi"])
+def test_argon2_cffi(selenium):
+    import argon2
+    ph = argon2.PasswordHasher(parallelism=1)
+    hash = ph.hash("test")
+
+    assert ph.verify(hash, "test") is True
