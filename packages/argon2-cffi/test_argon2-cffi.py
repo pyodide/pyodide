bd65d6e92d4c0d22d99d35531b66fe474ca594f6
giuseppe <capassog97@gmail.com>
2024-12-27 17:41:30 +0100

    Updated test

--- packages/argon2-cffi/test_argon2-cffi.py
+++ packages/argon2-cffi/test_argon2-cffi.py
@@ -1,11 +1,18 @@
+import pytest
 from pytest_pyodide import run_in_pyodide
 
 
-@run_in_pyodide(packages=["argon2_cffi"])
-def test_argon2_cffi(selenium):
+@run_in_pyodide(packages=["argon2-cffi", "argon2-cffi-bindings"])
+async def argon2_cffi_helper(selenium):
     import argon2
 
     ph = argon2.PasswordHasher(parallelism=1)
     hash = ph.hash("test")
 
     assert ph.verify(hash, "test") is True
+
+    with pytest.raises(argon2.exceptions.Argon2Error):
+        argon2.PasswordHasher(parallelism=2).hash("test")
+
+def test_argon2(selenium):
+    argon2_cffi_helper(selenium)
