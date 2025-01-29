92a4c4b50d44223074494a7ee13f8d4c1278e036
Hood Chatham <roberthoodchatham@gmail.com>
2025-01-29 11:13:23 +0100

    Fix early exit

--- src/js/pyodide.ts
+++ src/js/pyodide.ts
@@ -262,8 +262,8 @@ export async function loadPyodide(
   // `-s EXPORT_NAME="'_createPyodideModule'"`
   const Module = await _createPyodideModule(emscriptenSettings);
   // Handle early exit
-  if (emscriptenSettings.exited) {
-    throw emscriptenSettings.exited.toThrow;
+  if (Module.exitCode !== undefined) {
+    throw new Module.ExitStatus(Module.exitCode);
   }
   if (options.pyproxyToStringRepr) {
     API.setPyProxyToStringMethod(true);
