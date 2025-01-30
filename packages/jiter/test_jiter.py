d1748dfaf7694181da805025ed1179c528cab526
Samuel Colvin <s@muelcolvin.com>
2025-01-30 09:34:52 +0000

    add test for partial parsing

--- packages/jiter/test_jiter.py
+++ packages/jiter/test_jiter.py
@@ -9,3 +9,9 @@ def test_orjson(selenium):
     json_data = jiter.from_json(json_string)
 
     assert json_data == {"name": "Alice", "age": 30, "city": "New York"}
+
+    json_string = b'{"name": "Alice", "age": 30, "city": "New'
+
+    json_data = jiter.from_json(json_string, partial_mode='trailing-strings')
+
+    assert json_data == {"name": "Alice", "age": 30, "city": "New"}
