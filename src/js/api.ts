87d2adb4df53b53a3adcdd21cd47ba1d847eb2fb
Gyeongjae Choi <def6488@gmail.com>
2025-02-04 10:18:56 +0000

    Expose lockfile api

--- src/js/api.ts
+++ src/js/api.ts
@@ -683,6 +683,34 @@ export class PyodideAPI {
     }
     return API.makeSnapshot(serializer);
   }
+
+  /**
+   * Returns the `info` section of the pyodide lockfile used to load the current Pyodide instance.
+   * The format of the lockfile is defined in the `pyodide/pyodide-lock
+   * <https://github.com/pyodide/pyodide-lock`_ repository.
+   * 
+   * .. admonition:: Pyodide Lockfile
+   *    :class: warning
+   *
+   *    The content of the lockfile is not guaranteed to be stable across Pyodide versions.
+   */
+  static getLockfileInfo() {
+    return API.lockfile_info;
+  }
+
+  /**
+   * Returns the `packages` section of the pyodide lockfile used to load the current Pyodide instance.
+   * The format of the lockfile is defined in the `pyodide/pyodide-lock
+   * <https://github.com/pyodide/pyodide-lock`_ repository.
+   * 
+   * .. admonition:: Pyodide Lockfile
+   *    :class: warning
+   *
+   *    The content of the lockfile is not guaranteed to be stable across Pyodide versions.
+   */
+  static getLockfilePackages() {
+    return API.lockfile_packages;
+  }
 }
 
 /** @hidden */
