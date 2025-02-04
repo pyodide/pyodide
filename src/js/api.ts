0e906b5effbcc033b1e3c81bad19c0f370387eb8
Gyeongjae Choi <def6488@gmail.com>
2025-02-04 10:50:20 +0000

    Expose lockfile

--- src/js/api.ts
+++ src/js/api.ts
@@ -688,7 +688,7 @@ export class PyodideAPI {
    * Returns the `info` section of the pyodide lockfile used to load the current Pyodide instance.
    * The format of the lockfile is defined in the `pyodide/pyodide-lock
    * <https://github.com/pyodide/pyodide-lock`_ repository.
-   * 
+   *
    * .. admonition:: Pyodide Lockfile
    *    :class: warning
    *
@@ -702,7 +702,7 @@ export class PyodideAPI {
    * Returns the `packages` section of the pyodide lockfile used to load the current Pyodide instance.
    * The format of the lockfile is defined in the `pyodide/pyodide-lock
    * <https://github.com/pyodide/pyodide-lock`_ repository.
-   * 
+   *
    * .. admonition:: Pyodide Lockfile
    *    :class: warning
    *
