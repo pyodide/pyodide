24d3e2211a7da79a3b752ce94424c6b1ac60e927
Hood Chatham <roberthoodchatham@gmail.com>
2025-02-22 13:03:27 +0100

    Instead of ??? put <unknown JS frame/file> in JS frames for Python error
    This is much clearer.

--- src/core/error_handling.ts
+++ src/core/error_handling.ts
@@ -284,8 +284,8 @@ Module.handle_js_error = function (e: any) {
     if (isPyodideFrame(frame)) {
       break;
     }
-    const funcnameAddr = stringToNewUTF8(frame.functionName || "???");
-    const fileNameAddr = stringToNewUTF8(frame.fileName || "???.js");
+    const funcnameAddr = stringToNewUTF8(frame.functionName || "<unknown JS frame>");
+    const fileNameAddr = stringToNewUTF8(frame.fileName || "<unknown JS file>.js");
     __PyTraceback_Add(funcnameAddr, fileNameAddr, frame.lineNumber);
     _free(funcnameAddr);
     _free(fileNameAddr);
