b86513f5a341e9457e2a2e6e1f713611e0d3be32
Hood Chatham <roberthoodchatham@gmail.com>
2025-03-11 18:47:29 +0100

    Fix browser test_console tests

--- src/tests/test_console.py
+++ src/tests/test_console.py
@@ -430,7 +430,7 @@ def test_console_html(selenium):
             [[;;;terminal-error]  File \"<console>\", line 1
                 1+
                  ^
-            SyntaxError: incomplete input]
+            _IncompleteInputError: incomplete input]
             """
         ).strip()
     )
@@ -472,7 +472,6 @@ def test_console_html(selenium):
             [[;;;terminal-error]Traceback (most recent call last):
               File \"/lib/pythonxxx/pyodide/console.py\", line xxx, in repr_shorten
                 text = repr(value)
-                       ^^^^^^^^^^^
               File \"<console>\", line 3, in __repr__
             TypeError: hi]
             """
