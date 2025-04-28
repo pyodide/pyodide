96d2ac0828c1df2bfd6f697a0801e202ab55859b
Lukas <33507994+Lukasdoe@users.noreply.github.com>
2025-04-29 00:43:59 +0200

    suppress errors generated by `which` test in pyodide_env.sh (#5612)

--- pyodide_env.sh
+++ pyodide_env.sh
@@ -9,7 +9,7 @@ ROOT=$(cd -- "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 ; pwd -P)
 # shellcheck source=/dev/null
 source "$ROOT/emsdk/emsdk/emsdk_env.sh" 2> /dev/null || true
 export PATH="$ROOT/node_modules/.bin/:$ROOT/emsdk/emsdk/ccache/git-emscripten_64bit/bin:$PATH:$ROOT/packages/.artifacts/bin/"
-EMCC_PATH=$(which emcc.py || echo ".")
+EMCC_PATH=$(which emcc.py 2>/dev/null || echo ".")
 EM_DIR=$(dirname "$EMCC_PATH")
 export EM_DIR
 
