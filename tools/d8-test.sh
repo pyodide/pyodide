8569114f6db407de9db6b7762116aefdf2604a96
Hood Chatham <roberthoodchatham@gmail.com>
2025-05-02 14:37:31 -0400

    Specify OS in jsvu invocation

--- tools/d8-test.sh
+++ tools/d8-test.sh
@@ -5,7 +5,7 @@
 set -x
 TOOLS=$(dirname "${BASH_SOURCE[0]}")
 
-npx jsvu --engines=v8
+npx jsvu --engines=v8 --os=linux64
 echo $?
 
 "${HOME}"/.jsvu/bin/v8 --enable-os-system --module "${TOOLS}"/d8-test.mjs
