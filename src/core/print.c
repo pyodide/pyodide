ad2fedabd49996a46cef0c4955fdd985cb77882d
Hood Chatham <roberthoodchatham@gmail.com>
2025-05-30 19:20:25 -0400

    Update src/core/print.c

--- src/core/print.c
+++ src/core/print.c
@@ -1,11 +1,6 @@
 #include <emscripten.h>
 #include <stdio.h>
 
-// Note: We cannot use stdout / stderr const from stdio.h because we override
-// the stream when initializing the Pyodide runtime. Also users can override the
-// streams using `setStderr` and `setStdout`. Therefore, we always need to open
-// the stream on each call.
-
 EMSCRIPTEN_KEEPALIVE int
 print_stdout(const char* msg)
 {
