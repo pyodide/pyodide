3c8d6c6633261e18fcf03dd686e7042f10dc829f
ryanking13 <def6488@gmail.com>
2025-06-03 11:14:41 +0900

    Check exception

--- src/tests/test_package_loading.py
+++ src/tests/test_package_loading.py
@@ -369,15 +369,15 @@ def test_install_archive(selenium):
 @pytest.mark.requires_dynamic_linking
 def test_load_bad_so_file(selenium):
     # If we load a bad so file, it will raise with a message
-    try:
+    with pytest.raises(
+        selenium.JavascriptException, match="Failed to load dynamic library /a.so"
+    ):
         selenium.run_js(
             """
             pyodide.FS.writeFile("/a.so", new Uint8Array(4))
             await pyodide._api.loadDynlib("/a.so");
             """
         )
-    except selenium.JavascriptException as e:
-        assert "Failed to load dynamic library /a.so" in str(e)
 
 
 def test_should_load_dynlib():
