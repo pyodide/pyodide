8250fdc3dc7bfd587fd8f4cc267e173095c58445
Hood Chatham <roberthoodchatham@gmail.com>
2025-06-04 15:29:10 -0700

    Pyodide v0.27.7

--- src/core/pre.js
+++ src/core/pre.js
@@ -2,7 +2,7 @@ const API = Module.API;
 const Hiwire = {};
 const Tests = {};
 API.tests = Tests;
-API.version = "0.27.6";
+API.version = "0.27.7";
 Module.hiwire = Hiwire;
 
 function getTypeTag(x) {
--- src/js/version.ts
+++ src/js/version.ts
@@ -6,4 +6,4 @@
  * from the version in ``package.json`` which follows the node package manager
  * version convention.
  */
-export const version: string = "0.27.6";
+export const version: string = "0.27.7";
