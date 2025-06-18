d29ab1f9edf1d3f33044f2f612a99dd0f758e33e
Gyeongjae Choi <def6488@gmail.com>
2025-06-18 19:02:47 +0900

    Unvendor non-core packages (#5699)

--- packages/idna/test_idna.py
+++
@@ -1,8 +0,0 @@
-from pytest_pyodide import run_in_pyodide
-
-
-@run_in_pyodide(packages=["idna"])
-def test_idna(selenium_standalone):
-    import idna
-
-    assert idna.encode("ドメイン.テスト") == b"xn--eckwd4c7c.xn--zckzah"
