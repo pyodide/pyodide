c75cfc509d03e6a137e3ccae6c1967a65c7c935e
Hood Chatham <roberthoodchatham@gmail.com>
2025-06-28 09:19:09 +0100

    Better iOS detection

--- src/core/sentinel.ts
+++ src/core/sentinel.ts
@@ -1,12 +1,22 @@
-// @ts-ignore
+// @ts-ignore Can't find sentinel.wasm or it's corresponding type declarations
 import sentinelWasm from "./sentinel.wasm";
 
 declare const sentinelWasm: Uint8Array;
 
 const sentinelInstancePromise: Promise<WebAssembly.Instance | undefined> =
   (async function () {
-    const isIOS =
-      globalThis.navigator && /iPad|iPhone|iPod/.test(navigator.platform);
+    // Starting with iOS 18.3.1, WebKit on iOS has an issue with the garbage
+    // collector that breaks the call trampoline. See #130418 and
+    // https://bugs.webkit.org/show_bug.cgi?id=293113 for details.
+    let isIOS =
+      globalThis.navigator &&
+      (/iPad|iPhone|iPod/.test(navigator.userAgent) ||
+        // Starting with iPadOS 13, iPads might send a platform string that looks like a desktop Mac.
+        // To differentiate, we check if the platform is 'MacIntel' (common for Macs and newer iPads)
+        // AND if the device has multi-touch capabilities (navigator.maxTouchPoints > 1)
+        (navigator.platform === "MacIntel" &&
+          typeof navigator.maxTouchPoints !== "undefined" &&
+          navigator.maxTouchPoints > 1));
     if (isIOS) {
       return undefined;
     }
@@ -22,7 +32,6 @@ const sentinelInstancePromise: Promise<WebAssembly.Instance | undefined> =
   })();
 
 export async function getSentinelImport() {
-  await sentinelInstancePromise;
   const sentinelInstance = await sentinelInstancePromise;
   if (sentinelInstance) {
     return sentinelInstance.exports;
