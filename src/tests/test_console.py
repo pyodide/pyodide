2b75443e4debbe5e427774e6fffd7a1ab79d85a6
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-07-29 19:14:22 +0530

    Oops, add back truncation in traceback

--- src/tests/test_console.py
+++ src/tests/test_console.py
@@ -456,7 +456,7 @@ def test_console_html(selenium):
         "... \n"
         ">>> Test()\n"
         '[[;;;terminal-error;Traceback (most recent call last):\\n  File "/lib/pythonxxx/pyodide/console.py", line xxx, in repr_shorten\\n    text = repr(value)\\n  File "<console>", line 3, in __repr__\\nTypeError: hi]Traceback (most recent call last):]\n'
-        '[[;;;terminal-error;Traceback (most recent call last):\\n  File "/lib/pythonxxx/pyodide/console.py", line xxx, in repr_shorten\\n    text = repr(value)\\n  File "<console>", line 3, in __repr__\\nTypeError: hi]  File "/lib/pythonxxx/pyodide/console.py", line xxx, in repr_shorten]\n'
+        '[[;;;terminal-error;Traceback (most recent call last):\\n  File "/lib/pythonxxx/pyodide/console.py", line xxx, in repr_shorten\\n    text = repr(value)\\n  File "<console>", line 3, in __repr__\\nTypeError: hi]  File "/lib/pythonxxx/pyodide/console.py", line xxx, in repr_sho]\n'
         '[[;;;terminal-error;Traceback (most recent call last):\\n  File "/lib/pythonxxx/pyodide/console.py", line xxx, in repr_shorten\\n    text = repr(value)\\n  File "<console>", line 3, in __repr__\\nTypeError: hi]rten]\n'
         '[[;;;terminal-error;Traceback (most recent call last):\\n  File "/lib/pythonxxx/pyodide/console.py", line xxx, in repr_shorten\\n    text = repr(value)\\n  File "<console>", line 3, in __repr__\\nTypeError: hi]    text = repr(value)]\n'
         '[[;;;terminal-error;Traceback (most recent call last):\\n  File "/lib/pythonxxx/pyodide/console.py", line xxx, in repr_shorten\\n    text = repr(value)\\n  File "<console>", line 3, in __repr__\\nTypeError: hi]  File "<console>", line 3, in __repr__]\n'
