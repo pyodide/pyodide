92591f766c9cf9678a5e7619ef2ac139959486ed
zsaladin <zsaladinz@gmail.com>
2025-08-18 21:02:24 +0900

    Use `selenium_standalone_noload` instead of `run_in_pyodie`
    - https://github.com/pyodide/pyodide/pull/5830/files#r2264348671
    - https://github.com/pyodide/pyodide/pull/5830/files#r2264359327

--- src/tests/test_filesystem_mounting.py
+++ src/tests/test_filesystem_mounting.py
@@ -1,178 +1,133 @@
-from pytest_pyodide import run_in_pyodide
+from inspect import getsource
 
 
-@run_in_pyodide
-def test_filesystem_mounting(selenium):
-    import asyncio
-    from string import Template
+def test_filesystem_mounting(selenium_standalone_noload):
+    selenium = selenium_standalone_noload
+    selenium.run_js(
+        f"""
+        let pyodide1 = await loadPyodide();
+        let pyodide2 = await loadPyodide();
 
-    from pyodide.code import run_js
+        {js_set_test_environment_source()}
 
-    PYODIDE1_FLAG = "pyodide1"
-    PYODIDE2_FLAG = "pyodide2"
+        pyodide1.runPython(`{py_write_file_source(1)}`);
+        pyodide2.runPython(`{py_write_file_source(2)}`);
 
-    # setup the pyodide objects in js and return them to the python code
-    setup_creation_in_js = Template(
-        """
-        async function set_pyodide_objects() {
-            let pyodide1 = await loadPyodide();
-            let pyodide2 = await loadPyodide();
+        pyodide1.runPython(`{py_assert_created_files_source(1)}`);
+        pyodide2.runPython(`{py_assert_created_files_source(2)}`);
 
-            await pyodide1.FS.mkdir("${PYODIDE1_DIR}");
-            await pyodide2.FS.mkdir("${PYODIDE2_DIR}");
+        {js_remove_files_source()}
 
-            await pyodide2.FS.mount(
-                pyodide2.FS.filesystems.PROXYFS,
-                {
-                    root: "${PYODIDE1_DIR}",
-                    fs: pyodide1.FS,
-                },
-                "${PYODIDE2_DIR}"
-            );
-            self.pyodide1 = pyodide1;
-            self.pyodide2 = pyodide2;
-            return [pyodide1, pyodide2];
-        }
-        set_pyodide_objects();
+        pyodide1.runPython(`{py_assert_removed_files_source(1)}`);
+        pyodide2.runPython(`{py_assert_removed_files_source(2)}`);
         """
     )
 
-    # setup the python code to write to the file in shared directory mounted in both pyodide objects
-    # this code is executed in both pyodide objects
-    setup_creation_in_python = Template(
-        """
+
+def js_set_test_environment_source():
+    return """
+        await pyodide1.FS.mkdir("/pyodide1");
+        await pyodide2.FS.mkdir("/pyodide2");
+
+        await pyodide2.FS.mount(
+            pyodide2.FS.filesystems.PROXYFS,
+            {
+                root: "/pyodide1",
+                fs: pyodide1.FS,
+            },
+            "/pyodide2"
+        );
+    """
+
+
+def js_remove_files_source():
+    return """
+        await pyodide2.FS.unmount("/pyodide2");
+        await pyodide2.FS.rmdir("/pyodide2");
+        await pyodide1.FS.unlink("/pyodide1/file_from_1.txt");
+        await pyodide1.FS.unlink("/pyodide1/file_from_2.txt");
+        await pyodide1.FS.rmdir("/pyodide1");
+    """
+
+
+def py_write_file_source(index: int) -> str:
+    def write_file():
+        with open("/pyodide{index}/file_from_{index}.txt", "w", encoding="utf-8") as f:
+            f.write("pyodide{index}")
+
+    return (
+        getsource(write_file).format(index=index).lstrip() + f"{write_file.__name__}()"
+    )
+
+
+def py_assert_created_files_source(index: int) -> str:
+    def assert_created_files():
         import os
 
-        with open("${MOUNT_DIR}/file_from_${FILE_SUFFIX}.txt", "w", encoding="utf-8") as f:
-            f.write("${CONTENT}")
-        """
-    )
+        assert os.path.exists("/pyodide{index}/file_from_1.txt")
+        assert os.path.exists("/pyodide{index}/file_from_2.txt")
+        assert not os.path.exists("/pyodide{index}/file_from_nowhere.txt")
 
-    # test the python code to read the file in shared directory mounted in both pyodide objects
-    # this code is executed in both pyodide objects
-    test_creation_in_python = Template(
-        """
-        # Test os.exists
-        assert os.path.exists("${MOUNT_DIR}/file_from_${FILE_SUFFIX_1}.txt")
-        assert os.path.exists("${MOUNT_DIR}/file_from_${FILE_SUFFIX_2}.txt")
-        assert not os.path.exists("${MOUNT_DIR}/file_from_nowhere.txt")
+        assert os.path.isfile("/pyodide{index}/file_from_1.txt")
+        assert os.path.isfile("/pyodide{index}/file_from_2.txt")
+        assert not os.path.isfile("/pyodide{index}/file_from_nowhere.txt")
 
-        # Test os.path.isfile and os.path.isdir
-        assert os.path.isfile("${MOUNT_DIR}/file_from_${FILE_SUFFIX_1}.txt")
-        assert os.path.isfile("${MOUNT_DIR}/file_from_${FILE_SUFFIX_2}.txt")
-        assert not os.path.isfile("${MOUNT_DIR}/file_from_nowhere.txt")
-        assert not os.path.isdir("${MOUNT_DIR}/file_from_${FILE_SUFFIX_1}.txt")
-        assert not os.path.isdir("${MOUNT_DIR}/file_from_${FILE_SUFFIX_2}.txt")
-        assert not os.path.isdir("${MOUNT_DIR}/file_from_nowhere.txt")
-        assert os.path.isdir("${MOUNT_DIR}")
-        assert not os.path.isfile("${MOUNT_DIR}")
+        assert not os.path.isdir("/pyodide{index}/file_from_1.txt")
+        assert not os.path.isdir("/pyodide{index}/file_from_2.txt")
+        assert not os.path.isdir("/pyodide{index}/file_from_nowhere.txt")
+
+        assert os.path.isdir("/pyodide{index}")
+        assert not os.path.isfile("/pyodide{index}")
 
         # Test os.listdir
-        assert set(os.listdir("${MOUNT_DIR}")) == { "file_from_${FILE_SUFFIX_1}.txt", "file_from_${FILE_SUFFIX_2}.txt" }
+        assert sorted(os.listdir("/pyodide{index}")) == [
+            "file_from_1.txt",
+            "file_from_2.txt",
+        ]
 
         # Test file reading
-        with open("${MOUNT_DIR}/file_from_${FILE_SUFFIX_1}.txt", "r", encoding="utf-8") as f:
+        with open("/pyodide{index}/file_from_1.txt", encoding="utf-8") as f:
             content = f.read()
-        assert content == "${CONTENT_1}"
+        assert content == "pyodide1"
 
-        with open("${MOUNT_DIR}/file_from_${FILE_SUFFIX_2}.txt", "r", encoding="utf-8") as f:
+        with open("/pyodide{index}/file_from_2.txt", encoding="utf-8") as f:
             content = f.read()
-        assert content == "${CONTENT_2}"
+        assert content == "pyodide2"
 
         # Test os.path.getsize
-        file1_size = os.path.getsize("${MOUNT_DIR}/file_from_${FILE_SUFFIX_1}.txt")
-        file2_size = os.path.getsize("${MOUNT_DIR}/file_from_${FILE_SUFFIX_2}.txt")
-        assert file1_size == len("${CONTENT_1}".encode("utf-8"))
-        assert file2_size == len("${CONTENT_2}".encode("utf-8"))
+        file1_size = os.path.getsize("/pyodide{index}/file_from_1.txt")
+        file2_size = os.path.getsize("/pyodide{index}/file_from_2.txt")
+        assert file1_size == len(b"pyodide1")
+        assert file2_size == len(b"pyodide2")
 
         # Test os.stat
-        stat_info1 = os.stat("${MOUNT_DIR}/file_from_${FILE_SUFFIX_1}.txt")
-        stat_info2 = os.stat("${MOUNT_DIR}/file_from_${FILE_SUFFIX_2}.txt")
-        assert stat_info1.st_size == len("${CONTENT_1}".encode("utf-8"))
-        assert stat_info2.st_size == len("${CONTENT_2}".encode("utf-8"))
-        """
+        stat_info1 = os.stat("/pyodide{index}/file_from_1.txt")
+        stat_info2 = os.stat("/pyodide{index}/file_from_2.txt")
+        assert stat_info1.st_size == len(b"pyodide1")
+        assert stat_info2.st_size == len(b"pyodide2")
+
+    return (
+        getsource(assert_created_files).format(index=index).lstrip()
+        + f"{assert_created_files.__name__}()"
     )
 
-    setup_removal_in_js = Template(
-        """
-        async function remove_directory() {
-            await self.pyodide2.FS.unmount("${PYODIDE2_DIR}");
-            await self.pyodide2.FS.rmdir("${PYODIDE2_DIR}");
-            await self.pyodide1.FS.unlink("${PYODIDE1_DIR}/file_from_${FILE_SUFFIX_1}.txt");
-            await self.pyodide1.FS.unlink("${PYODIDE1_DIR}/file_from_${FILE_SUFFIX_2}.txt");
-            await self.pyodide1.FS.rmdir("${PYODIDE1_DIR}");
-        }
-        remove_directory();
-        """
+
+def py_assert_removed_files_source(index: int) -> str:
+    def assert_removed_files():
+        import os
+
+        assert not os.path.exists("/pyodide{index}")
+        assert not os.path.isfile("/pyodide{index}")
+        assert not os.path.isdir("/pyodide{index}")
+
+        assert not os.path.exists("/pyodide{index}/file_from_1.txt")
+        assert not os.path.exists("/pyodide{index}/file_from_2.txt")
+        assert not os.path.isfile("/pyodide{index}/file_from_1.txt")
+        assert not os.path.isfile("/pyodide{index}/file_from_2.txt")
+        assert not os.path.isdir("/pyodide{index}/file_from_1.txt")
+        assert not os.path.isdir("/pyodide{index}/file_from_2.txt")
+
+    return (
+        getsource(assert_removed_files).format(index=index).lstrip()
+        + f"{assert_removed_files.__name__}()"
     )
-
-    test_removal_in_python = Template(
-        """
-        assert not os.path.exists("${MOUNT_DIR}")
-        assert not os.path.isfile("${MOUNT_DIR}")
-        assert not os.path.isdir("${MOUNT_DIR}")
-
-        assert not os.path.exists("${MOUNT_DIR}/file_from_${FILE_SUFFIX_1}.txt")
-        assert not os.path.exists("${MOUNT_DIR}/file_from_${FILE_SUFFIX_2}.txt")
-        assert not os.path.isfile("${MOUNT_DIR}/file_from_${FILE_SUFFIX_1}.txt")
-        assert not os.path.isfile("${MOUNT_DIR}/file_from_${FILE_SUFFIX_2}.txt")
-        assert not os.path.isdir("${MOUNT_DIR}/file_from_${FILE_SUFFIX_1}.txt")
-        assert not os.path.isdir("${MOUNT_DIR}/file_from_${FILE_SUFFIX_2}.txt")
-        """
-    )
-
-    set_pyodide_objects = run_js(
-        setup_creation_in_js.substitute(
-            PYODIDE1_DIR=f"/{PYODIDE1_FLAG}", PYODIDE2_DIR=f"/{PYODIDE2_FLAG}"
-        )
-    )
-    asyncio.run(set_pyodide_objects)
-
-    pyodides = set_pyodide_objects.result()
-    pyodide1 = pyodides[0]
-    pyodide2 = pyodides[1]
-
-    for pyodide, pyodide_flag in zip(
-        [pyodide1, pyodide2], [PYODIDE1_FLAG, PYODIDE2_FLAG], strict=True
-    ):
-        pyodide.runPython(
-            setup_creation_in_python.substitute(
-                MOUNT_DIR=f"/{pyodide_flag}",
-                FILE_SUFFIX=pyodide_flag,
-                CONTENT=pyodide_flag,
-            )
-        )
-
-    for pyodide, pyodide_flag in zip(
-        [pyodide1, pyodide2], [PYODIDE1_FLAG, PYODIDE2_FLAG], strict=True
-    ):
-        pyodide.runPython(
-            test_creation_in_python.substitute(
-                MOUNT_DIR=f"/{pyodide_flag}",
-                FILE_SUFFIX_1=PYODIDE1_FLAG,
-                FILE_SUFFIX_2=PYODIDE2_FLAG,
-                CONTENT_1=PYODIDE1_FLAG,
-                CONTENT_2=PYODIDE2_FLAG,
-            )
-        )
-
-    remove_directory = run_js(
-        setup_removal_in_js.substitute(
-            PYODIDE1_DIR=f"/{PYODIDE1_FLAG}",
-            PYODIDE2_DIR=f"/{PYODIDE2_FLAG}",
-            FILE_SUFFIX_1=PYODIDE1_FLAG,
-            FILE_SUFFIX_2=PYODIDE2_FLAG,
-        )
-    )
-    asyncio.run(remove_directory)
-
-    for pyodide, pyodide_flag in zip(
-        [pyodide1, pyodide2], [PYODIDE1_FLAG, PYODIDE2_FLAG], strict=True
-    ):
-        pyodide.runPython(
-            test_removal_in_python.substitute(
-                MOUNT_DIR=f"/{pyodide_flag}",
-                FILE_SUFFIX_1=PYODIDE1_FLAG,
-                FILE_SUFFIX_2=PYODIDE1_FLAG,
-            )
-        )
