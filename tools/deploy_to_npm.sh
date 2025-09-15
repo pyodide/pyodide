aba123cdc7ecaac0b2ea8307e5ada15694f46d0a
zsaladin <zsaladinz@gmail.com>
2025-09-15 17:30:05 +0900

    Fix SemVer error

--- tools/deploy_to_npm.sh
+++ tools/deploy_to_npm.sh
@@ -16,8 +16,8 @@ cd dist/
 PACKAGE_NAME=$(node -p "require('./package.json').name")
 JS_VERSION=$(node -p "require('./package.json').version")
 if [[ -n "${DRY_RUN}" ]]; then
-    echo "Dry run: npm publish --tag ${JS_VERSION}"
-    npm publish --dry-run --tag "${JS_VERSION}"
+    echo "Dry run: npm publish --tag dev"
+    npm publish --dry-run --tag dev
 elif [[ ${JS_VERSION} =~ [alpha|beta|rc|dev] ]]; then
     echo "Publishing an unstable release"
     npm publish --tag next
