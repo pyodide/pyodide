be80d9cf81dfe76476475cee79de0095869e8341
zsaladin <zsaladinz@gmail.com>
2025-09-25 19:47:58 +0900

    Leave only one browser type

--- src/js/playwright.config.ts
+++ src/js/playwright.config.ts
@@ -17,13 +17,5 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
   ],
 });
