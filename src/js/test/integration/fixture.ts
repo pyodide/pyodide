66b3c281ffe861d1e3b5011b8a17e769c195855d
zsaladin <zsaladinz@gmail.com>
2025-10-07 22:37:58 +0900

    Make integration test for node work with playwright

--- src/js/test/integration/fixture.ts
+++ src/js/test/integration/fixture.ts
@@ -1,16 +1,33 @@
 import { test as base } from "@playwright/test";
+import path from "node:path";
+const NODE = process.env.TEST_NODE;
 
+async function page_in_node({}, use) {
+  const root = path.resolve(__dirname, "../../../../", "dist");
+  const { loadPyodide } = require(path.resolve(root, "pyodide"));
+  const page = {
+    title: () => Promise.resolve("pyodide"),
+    goto: () => Promise.resolve(null),
+    evaluate: (fn, ...args) => fn(...args),
+  };
+  await page.evaluate(async () => {
+    globalThis.pyodide = await loadPyodide({ root });
+  });
+  await use(page);
+}
+
+async function page_in_browser({ page }, use) {
+  await page.goto("/test.html");
+  await page.addScriptTag({
+    url: "/pyodide.js",
+  });
+  await page.evaluate(async () => {
+    globalThis.pyodide = await loadPyodide();
+  });
+  await use(page);
+}
 export const test = base.extend({
-  page: async ({ page }, use) => {
-    await page.goto("/test.html");
-    await page.addScriptTag({
-      url: "/pyodide.js",
-    });
-    await page.evaluate(async () => {
-      globalThis.pyodide = await loadPyodide();
-    });
-    await use(page);
-  },
+  page: NODE ? page_in_node : page_in_browser,
 });
 
 export { expect } from "@playwright/test";
