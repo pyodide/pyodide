ef9f4306794f6d10bb93daed41b1adae484896f1
Chanho Lee <ibear6954@gmail.com>
2025-10-25 13:39:52 +0900

    [scipy] Delete OpenBLAS benchmark

--- packages/test-openblas-simd/src/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="test-openblas-simd",
-    version="0.1",
-    packages=["test_openblas_simd"],
-)
--- packages/test-openblas-simd/src/test_openblas_simd/__init__.py
+++
@@ -1,1 +0,0 @@
-# package marker for test-openblas-simd
