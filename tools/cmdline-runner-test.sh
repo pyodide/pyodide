6a1eb9a61b671c42108708c91f4c030a82f3b229
ryanking13 <def6488@gmail.com>
2025-12-11 17:37:25 +0900

    Fix path and plugin

--- tools/cmdline-runner-test.sh
+++ tools/cmdline-runner-test.sh
@@ -21,4 +21,6 @@ source .venv-pyodide/bin/activate
 git clone https://github.com/python-attrs/attrs --depth 1 --branch 25.3.0
 cd attrs || exit
 pip install ".[tests]"
+# mypy_plugins uses pty and stuff that isn't supported on Emscripten.
+../.venv-pyodide/bin/pip uninstall pytest-mypy-plugins -y
 python -m pytest -k 'not mypy'
