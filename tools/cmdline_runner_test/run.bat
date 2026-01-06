ca6675014376d425bd11634332b29e551f72342d
ryanking13 <def6488@gmail.com>
2026-01-06 23:21:18 +0900

    Add calls

--- tools/cmdline_runner_test/run.bat
+++ tools/cmdline_runner_test/run.bat
@@ -5,10 +5,6 @@ REM Get the current directory as PYODIDE_ROOT
 set PYODIDE_ROOT=%CD%
 echo %PYODIDE_ROOT%
 
-REM Build pyodide_build
-pip install -e ./pyodide-build
-if errorlevel 1 exit /b 1
-
 REM Clean up and create test directory
 if exist test-cmdline-runner rmdir /s /q test-cmdline-runner
 mkdir test-cmdline-runner
@@ -23,8 +19,12 @@ REM Activate host virtual environment
 call .venv-host\Scripts\activate.bat
 if errorlevel 1 exit /b 1
 
+REM Build pyodide_build
+call pip install -e ../pyodide-build
+if errorlevel 1 exit /b 1
+
 REM Create pyodide virtual environment
-pyodide venv .venv-pyodide
+call pyodide venv .venv-pyodide
 if errorlevel 1 exit /b 1
 
 REM Activate pyodide virtual environment
