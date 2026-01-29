ad971cda347c4899567b1179d88869a40ebe1489
Gyeongjae Choi <def6488@gmail.com>
2026-01-29 12:09:08 +0900

    Try to fix flaky windows_to_linux_path test in firefox (#6091)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/tests/test_pyodide.py
+++ src/tests/test_pyodide.py
@@ -1632,6 +1632,7 @@ def test_windows_to_linux_path_finder_edge_cases(selenium):
 @run_in_pyodide
 def test_windows_to_linux_path_import(selenium_standalone):
     import sys
+    from importlib import invalidate_caches
     from pathlib import Path
 
     tmp_dir = Path("/tmp/my/temporary/directory/for/testing/import")
@@ -1639,6 +1640,7 @@ def test_windows_to_linux_path_import(selenium_standalone):
     module_file = tmp_dir / "test_module.py"
 
     sys.path.append("C:\\tmp\\my\\temporary\\directory\\for\\testing\\import")
+    invalidate_caches()
 
     try:
         import test_module
