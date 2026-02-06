37e57732102da6a0719ea5af3b9044c969c4eeae
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-02-06 12:29:30 +0900

    Cleanup debug

--- src/core/stack_switching/suspenders.c
+++ src/core/stack_switching/suspenders.c
@@ -14,21 +14,9 @@ EM_JS(JsVal, syncifyHandler, (JsVal x, JsVal y), {
   return Module.error;
 }
 
-// For the new JSPI API, use a regular function (not async) that returns a Promise.
-// Using an async function creates JS frames that JSPI cannot suspend through.
-function innerSync(x, y) {
-  const promise = x ?? y;
-  return Promise.resolve(promise).catch(e => {
-    if (e && e.pyodide_fatal_error) {
-      throw e;
-    }
-    Module.syncify_error = e;
-    return Module.error;
-  });
-}
-
-// For the old JSPI API, keep the async function as it works differently.
-async function innerAsync(x, y) {
+async function inner(x, y) {
+  // In the old JSPI API, we get the promise as first argument.
+  // In the new JSPI API we get it as the second argument.
   try {
     return await (x ?? y);
   } catch (e) {
@@ -39,13 +27,12 @@ async function innerAsync(x, y) {
     return Module.error;
   }
 }
-
 if (newJspiSupported) {
-  syncifyHandler = new WebAssembly.Suspending(innerSync);
+  syncifyHandler = new WebAssembly.Suspending(inner);
 } else if (oldJspiSupported) {
   syncifyHandler = new WebAssembly.Function(
     { parameters: ["externref", "externref"], results: ["externref"] },
-    innerAsync,
+    inner,
     { suspending: "first" }
   );
 }
@@ -70,23 +57,16 @@ EM_JS(void, JsvPromise_Syncify_handleError, (void), {
  * so we don't need to wrap this in a hiwire ID.
  */
 EM_JS(JsVal, saveState, (void), {
-  console.log('[DEBUG] saveState: Starting');
   if (!validSuspender.value) {
-    console.log('[DEBUG] saveState: ERROR - No valid suspender');
     return Module.error;
   }
-  console.log('[DEBUG] saveState: Creating StackState');
   const stackState = new StackState();
-  console.log('[DEBUG] saveState: Calling _captureThreadState');
   const threadState = _captureThreadState();
-  console.log('[DEBUG] saveState: threadState:', threadState);
-  const state = {
+  return {
     threadState,
     stackState,
     suspender : suspenderGlobal.value,
   };
-  console.log('[DEBUG] saveState: Returning state');
-  return state;
 });
 
 /**
@@ -95,41 +75,24 @@ EM_JS(JsVal, saveState, (void), {
  * argument is the return value from save_state.
  */
 EM_JS(void, restoreState, (JsVal state), {
-  console.log('[DEBUG] restoreState: Starting');
-  console.log('[DEBUG] restoreState: Restoring stack state');
   state.stackState.restore();
-  console.log('[DEBUG] restoreState: Calling _restoreThreadState');
   _restoreThreadState(state.threadState);
-  console.log('[DEBUG] restoreState: Setting suspender and validSuspender');
   suspenderGlobal.value = state.suspender;
   validSuspender.value = true;
-  console.log('[DEBUG] restoreState: Completed');
 });
 
 JsVal
 JsvPromise_Syncify(JsVal promise)
 {
-  printf("[DEBUG] JsvPromise_Syncify: Starting\n");
-  printf("[DEBUG] JsvPromise_Syncify: Calling saveState\n");
   JsVal state = saveState();
   if (JsvError_Check(state)) {
-    printf("[DEBUG] JsvPromise_Syncify: ERROR - saveState failed\n");
     return JS_ERROR;
   }
-  printf("[DEBUG] JsvPromise_Syncify: saveState succeeded\n");
-  printf("[DEBUG] JsvPromise_Syncify: Getting suspender\n");
   JsVal suspender = get_suspender();
-  printf("[DEBUG] JsvPromise_Syncify: Got suspender\n");
-  printf("[DEBUG] JsvPromise_Syncify: Calling syncifyHandler\n");
   JsVal result = syncifyHandler(suspender, promise);
-  printf("[DEBUG] JsvPromise_Syncify: syncifyHandler returned\n");
-  printf("[DEBUG] JsvPromise_Syncify: Calling restoreState\n");
   restoreState(state);
-  printf("[DEBUG] JsvPromise_Syncify: restoreState completed\n");
   if (JsvError_Check(result)) {
-    printf("[DEBUG] JsvPromise_Syncify: ERROR - result is error, handling\n");
     JsvPromise_Syncify_handleError();
   }
-  printf("[DEBUG] JsvPromise_Syncify: Returning result\n");
   return result;
 }
