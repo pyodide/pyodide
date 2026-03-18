14cd533d842fb5d7495976f655fbb890ac35c400
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-03-18 22:28:06 +0900

    Add integration tests for node sockets

--- conftest.py
+++ conftest.py
@@ -207,6 +207,15 @@ def pytest_collection_modifyitems(config, items):
                 )
                 continue
 
+        if item.get_closest_marker("db"):
+            # Skip db tests if mark not explicitly included
+            markexpr = config.getoption("-m", default="")
+            if "db" not in markexpr:
+                item.add_marker(
+                    pytest.mark.skip(reason="db test skipped (use '-m db' to run)")
+                )
+                continue
+
         maybe_skip_test(item, delayed=True)
 
 
