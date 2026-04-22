53e8382e0a85c2b9e6f5c9aaa2ce9da02ef18fc0
Hood Chatham <roberthoodchatham@gmail.com>
2026-04-22 15:47:43 -0700

    Make test_packaging version independent
    And copy patch from pyodide-recipes

--- packages/packaging/test_packaging.py
+++ packages/packaging/test_packaging.py
@@ -3,9 +3,13 @@ from pytest_pyodide.decorator import run_in_pyodide
 
 @run_in_pyodide(packages=["packaging"])
 def test_packaging(selenium):
+    import re
+
     from packaging.tags import platform_tags
 
-    assert sorted(platform_tags()) == [
-        "emscripten_5_0_3_wasm32",
-        "pyemscripten_2026_0_wasm32",
+    tags = sorted(re.sub(r"[0-9]+", "x", t) for t in platform_tags())
+    assert tags == [
+        "emscripten_x_x_x_wasmx",
+        "pyemscripten_x_x_wasmx",
+        "pyodide_x_x_wasmx",
     ]
