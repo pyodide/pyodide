6198ff461bf48be68d5af0c5ed8c8accc6410778
Hood Chatham <roberthoodchatham@gmail.com>
2026-05-01 10:13:35 -0700

    Remove assertion about lockfile_info.version

--- src/tests/test_pyodide.py
+++ src/tests/test_pyodide.py
@@ -2134,7 +2134,6 @@ def test_lockfile_api(selenium):
 
     assert lockfile_info is not None
     assert lockfile_info.abi_version is not None
-    assert lockfile_info.version is not None
     assert lockfile_info.python is not None
 
     assert lockfile_packages.micropip is not None
