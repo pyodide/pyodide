d801c8706f9fd00e2022e6560a48b42fbfac3d78
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-05-08 06:09:28 +0530

    Silence the linter for now, like elsewhere

--- src/tests/test_python_socket.py
+++ src/tests/test_python_socket.py
@@ -1049,7 +1049,7 @@ def test_tls_starttls_send_recv(selenium_nodesock, self_signed_cert):
         s.connect((host, port))
 
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)  # type: ignore[attr-defined]
-        ctx.minimum_version = ssl.TLSVersion.TLSv1_2
+        ctx.minimum_version = ssl.TLSVersion.TLSv1_2  # type: ignore[attr-defined]
         ctx.check_hostname = False
 
         ss = ctx.wrap_socket(s, server_hostname="localhost")
