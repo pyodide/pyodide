b72d54f8710a3eb6637f6062a3fce3d248c6ad75
Hood Chatham <roberthoodchatham@gmail.com>
2026-06-11 16:29:05 -0700

    Fix

--- src/core/jsproxy.c
+++ src/core/jsproxy.c
@@ -544,8 +544,8 @@ JsProxy_GetAttr_helper(PyObject* self, PyObject* attr, bool is_method)
 
   bool success = false;
   JsVal jsresult = JS_ERROR;
-  DECLARE_PY_OBJECT(get_attr_sig_res);
-  DECLARE_PY_OBJECT(attr_sig);
+  PyObject* get_attr_sig_res = NULL;
+  PyObject* attr_sig = NULL;
   // result:
   PyObject* pyresult = NULL;
 
@@ -623,6 +623,8 @@ JsProxy_GetAttr_helper(PyObject* self, PyObject* attr, bool is_method)
 success:
   success = true;
 finally:
+  Py_CLEAR(attr_sig);
+  Py_CLEAR(get_attr_sig_res);
   if (!success) {
     Py_CLEAR(pyresult);
   }
