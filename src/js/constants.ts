289e7b706bd6678a4d8ae4eda49d1824ad58ba47
Hood Chatham <roberthoodchatham@gmail.com>
2026-07-23 16:54:09 +0200

    Derive API.version and abiVersion from Makefile

--- src/js/constants.ts
+++ src/js/constants.ts
@@ -7,6 +7,10 @@ declare global {
   export const DEBUG: boolean;
   /** @private */
   export const SOURCEMAP: boolean;
+  /** The Pyodide version, injected at build time from PYODIDE_VERSION. */
+  export const API_VERSION: string;
+  /** The Pyodide ABI version, injected at build time from PYODIDE_ABI_VERSION. */
+  export const ABI_VERSION: string;
 }
 
 /** @hidden */
