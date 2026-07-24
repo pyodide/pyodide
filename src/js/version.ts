9deba1ea3f40dc234a775b0fd7277ec6487beeaf
Gyeongjae Choi <gyeongjae@cloudflare.com>
2026-07-24 14:19:19 +0900

    314.0.3

--- src/js/version.ts
+++ src/js/version.ts
@@ -6,4 +6,4 @@
  * from the version in ``package.json`` which follows the node package manager
  * version convention.
  */
-export const version: string = "314.0.2";
+export const version: string = "314.0.3";
