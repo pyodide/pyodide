5f0290f3b4318603c55d5b1e06aaf64fbd492437
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-07-29 12:58:10 +0530

    Try cases suggested by Gyeongjae
    Co-authored-by: Gyeongjae Choi <def6488@gmail.com>

--- tools/tests/test_windows_launchers.py
+++ tools/tests/test_windows_launchers.py
@@ -121,6 +121,9 @@ class TestPythonExe:
             ["-c", "print('percent 50%')"],
             ["-c", 'print("percent 50%")'],
             ["-m", "http.server", "--bind", "::1", "8000"],
+            ['-c', 'print("a & b")'],
+            ['a"&b'],
+            ['%PATH%'],
         ],
         ids=" ".join,
     )
