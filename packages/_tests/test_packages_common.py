332661f20fee5ba5bcb22dbd17bf036f56c6820a
Gyeongjae Choi <def6488@gmail.com>
2026-09-13 12:35:31 +0900

    Add psycopg-binary dependency note

--- packages/_tests/test_packages_common.py
+++ packages/_tests/test_packages_common.py
@@ -26,6 +26,7 @@ XFAIL_PACKAGES: dict[str, str] = {
     "numpy-tests": "test only package",
     "coolprop": "slow",
     "psycopg-c": "the psycopg package should be imported before psycopg_c",
+    "psycopg-binary": "the psycopg package should be imported before psycopg_binary",
 }
 
 LOCKFILE_PATH = PYODIDE_ROOT / "dist" / "pyodide-lock.json"
